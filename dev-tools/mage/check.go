a3a26d423f7076daf2613d926f289087bcff3ab2
Mariana Dima <mariana@elastic.co>
2022-01-14 13:03:36 +0100

    Remove `elastic-agent` directory (#12)
    * move directory
    
    * fix mage cmd
    
    * add missing
    
    * licence header
    
    * licence header
    
    * licence
    
    * fix check
    
    * mage check fix
    
    * fmt
    
    * mage fmt update
    
    * tests
    
    * fix tests
    
    * skip flaky test
    
    * skip tests

--- dev-tools/mage/check.go
+++ dev-tools/mage/check.go
@@ -1,19 +1,6 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
 
 package mage
 
@@ -189,24 +176,12 @@ func GoVet() error {
 // CheckLicenseHeaders checks license headers in .go files.
 func CheckLicenseHeaders() error {
 	fmt.Println(">> fmt - go-licenser: Checking for missing headers")
-
 	mg.Deps(InstallGoLicenser)
 
-	var license string
-	switch BeatLicense {
-	case "ASL2", "ASL 2.0":
-		license = "ASL2"
-	case "Elastic", "Elastic License":
-		license = "Elastic"
-	default:
-		return errors.Errorf("unknown license type %v", BeatLicense)
-	}
-
 	licenser := gotool.Licenser
-	return licenser(licenser.Check(), licenser.License(license))
+	return licenser(
+		licenser.Check(),
+		licenser.License("Elastic"),
+	)
+
 }
-
-
-
-
-
