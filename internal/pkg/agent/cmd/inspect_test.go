76d12569723d9fbc436e8c536b4c0bbfc5308c3d
michel-laterman <michel.laterman@elastic.co>
2022-03-23 14:58:09 -0700

    fix linter

--- internal/pkg/agent/cmd/inspect_test.go
+++ internal/pkg/agent/cmd/inspect_test.go
@@ -1,10 +1,14 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package cmd
 
 import (
 	"testing"
 )
 
-func TestgetFleetInput(t *testing.T) {
+func TestGetFleetInput(t *testing.T) {
 	tests := []struct {
 		name   string
 		input  map[string]interface{}
