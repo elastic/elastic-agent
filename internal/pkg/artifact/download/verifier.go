85697f8ee9e99a2403ed0d5e81f0051393ab40cc
Andrew Kroh <andrew.kroh@elastic.co>
2022-03-28 09:06:03 -0400

    Add more godocs

--- internal/pkg/artifact/download/verifier.go
+++ internal/pkg/artifact/download/verifier.go
@@ -21,6 +21,8 @@ import (
 	"github.com/elastic/elastic-agent/internal/pkg/agent/program"
 )
 
+// ChecksumMismatchError indicates the expected checksum for a file does not
+// match the computed checksum.
 type ChecksumMismatchError struct {
 	Expected string
 	Computed string
@@ -31,6 +33,7 @@ func (e *ChecksumMismatchError) Error() string {
 	return "checksum mismatch for " + e.File + ": expected " + e.Expected + ", computed " + e.Computed
 }
 
+// InvalidSignatureError indicates the file's GPG signature is invalid.
 type InvalidSignatureError struct {
 	File string
 	Err  error
@@ -40,12 +43,17 @@ func (e *InvalidSignatureError) Error() string {
 	return "invalid signature for " + e.File + ": " + e.Err.Error()
 }
 
+// Unwrap returns the cause.
 func (e *InvalidSignatureError) Unwrap() error { return e.Err }
 
 // Verifier is an interface verifying the SHA512 checksum and GPG signature and
 // of a downloaded artifact.
 type Verifier interface {
 	// Verify should verify the artifact and return an error if any checks fail.
+	// If the checksum does no match Verify returns a
+	// *download.ChecksumMismatchError. And if the GPG signature is invalid then
+	// Verify returns a *download.InvalidSignatureError. Use errors.As() to
+	// check error types.
 	Verify(spec program.Spec, version string) error
 }
 
@@ -85,7 +93,8 @@ func VerifySHA512Hash(filename string) error {
 
 // readChecksumFile reads the checksum of the file named in filename from
 // checksumFile. checksumFile is expected to contain the output from the
-// shasum family of tools (e.g. sha512sum).
+// shasum family of tools (e.g. sha512sum). If the checksum does not match then
+// a *download.ChecksumMismatchError is returned.
 func readChecksumFile(checksumFile, filename string) (string, error) {
 	f, err := os.Open(checksumFile)
 	if err != nil {
@@ -123,6 +132,10 @@ func readChecksumFile(checksumFile, filename string) (string, error) {
 	return checksum, nil
 }
 
+// VerifyGPGSignature verifies the GPG signature of a file. It accepts the path
+// to the file to verify, the ASCII armored signature, and the public key to
+// check against. If there is a problem with the signature then a
+// *download.InvalidSignatureError is returned.
 func VerifyGPGSignature(file string, asciiArmorSignature, publicKey []byte) error {
 	keyring, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(publicKey))
 	if err != nil {
