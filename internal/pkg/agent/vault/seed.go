2bff2cba7800b1eff77e399e8e50aeeb2b467a07
Aleksandr Maus <aleksandr.maus@elastic.co>
2022-05-04 19:52:26 -0400

    Remove .seed file root owner check, makes it impossible to unit test encrypted storage on any of the platforms

--- internal/pkg/agent/vault/seed.go
+++ internal/pkg/agent/vault/seed.go
@@ -18,42 +18,8 @@ import (
 
 const seedFile = ".seed"
 
-var ErrNonRootFileOwner = errors.New("non-root file owner")
-
-func isFileOwnerRoot(path string) (isOwnerRoot bool, err error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return false, err
-	}
-
-	stat, err := file.Wrap(info)
-	if err != nil {
-		return false, err
-	}
-
-	uid, _ := stat.UID()
-	gid, _ := stat.GID()
-	if uid == 0 && gid == 0 {
-		return true, nil
-	}
-
-	return false, nil
-}
-
 func getSeed(path string) ([]byte, error) {
 	fp := filepath.Join(path, seedFile)
-
-	isOwnerRoot, err := isFileOwnerRoot(fp)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return nil, err
-		}
-		isOwnerRoot = true
-	}
-
-	if !isOwnerRoot {
-		return nil, ErrNonRootFileOwner
-	}
 
 	b, err := ioutil.ReadFile(fp)
 
