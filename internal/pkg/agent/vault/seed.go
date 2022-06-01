426f33c507a6ef1fecb300a0b9aac861ea87ff08
Aleksandr Maus <aleksandr.maus@elastic.co>
2022-06-01 12:01:42 -0400

    Remove vault seed file ownership root check (applicable to linux only) (#494)
    * Remove vault seed file ownership root check
    
    * Remove unused import

--- internal/pkg/agent/vault/seed.go
+++ internal/pkg/agent/vault/seed.go
@@ -9,64 +9,15 @@ package vault
 
 import (
 	"errors"
-	"io/fs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime"
-
-	"github.com/elastic/elastic-agent-libs/file"
 )
 
 const seedFile = ".seed"
 
-var ErrNonRootFileOwner = errors.New("non-root file owner")
-
-var skipRootCheck bool
-
-// DisableRootCheck is a hook to disable root owner check for .seed file
-// This is needed for linux unit tests
-func DisableRootCheck() {
-	skipRootCheck = true
-}
-
-func isFileOwnerRoot(path string) (isOwnerRoot bool, err error) {
-	if skipRootCheck || runtime.GOOS != "linux" {
-		return true, nil
-	}
-	info, err := os.Stat(path)
-	if err != nil {
-		return false, err
-	}
-
-	stat, err := file.Wrap(info)
-	if err != nil {
-		return false, err
-	}
-
-	uid, _ := stat.UID()
-	gid, _ := stat.GID()
-	if uid == 0 && gid == 0 {
-		return true, nil
-	}
-
-	return false, nil
-}
-
 func getSeed(path string) ([]byte, error) {
 	fp := filepath.Join(path, seedFile)
-
-	isOwnerRoot, err := isFileOwnerRoot(fp)
-	if err != nil {
-		if !errors.Is(err, fs.ErrNotExist) {
-			return nil, err
-		}
-		isOwnerRoot = true
-	}
-
-	if !isOwnerRoot {
-		return nil, ErrNonRootFileOwner
-	}
 
 	b, err := ioutil.ReadFile(fp)
 	if err != nil {
