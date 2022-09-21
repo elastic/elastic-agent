600a0629105384479d9c7804d8bc7eb1a495668c
Michal Pristas <michal.pristas@gmail.com>
2022-09-21 16:27:44 +0200

    lint

--- internal/pkg/agent/operation/operator_test.go
+++ internal/pkg/agent/operation/operator_test.go
@@ -406,7 +406,7 @@ func TestConfigurableService(t *testing.T) {
 
 	// emulating a service, so we need to start the binary here in the test
 	spec := p.ProcessSpec()
-	cmd := exec.Command(spec.BinaryPath, fmt.Sprintf("%d", p.ServicePort()))
+	cmd := exec.Command(spec.BinaryPath, fmt.Sprintf("%d", p.ServicePort())) //nolint:gosec,G204 // this is fine
 	cmd.Env = append(cmd.Env, os.Environ()...)
 	cmd.Dir = filepath.Dir(spec.BinaryPath)
 	cmd.Stdout = os.Stdout
