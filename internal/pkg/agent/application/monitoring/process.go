2bf6471b7b6eb918403a7687b64d6887f629a22e
Michal Pristas <michal.pristas@gmail.com>
2022-11-24 11:28:17 +0100

    Support processes/stats for agent, apm and fleet server

--- internal/pkg/agent/application/monitoring/process.go
+++ internal/pkg/agent/application/monitoring/process.go
@@ -5,25 +5,44 @@
 package monitoring
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/coordinator"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
 )
 
-const processIDKey = "processID"
+const (
+	processIDKey = "processID"
+	timeout      = 10 * time.Second
+)
 
-func processHandler(coord *coordinator.Coordinator, statsHandler func(http.ResponseWriter, *http.Request) error) func(http.ResponseWriter, *http.Request) error {
+var redirectPathAllowlist = map[string]struct{}{
+	"stats": {},
+	"state": {},
+}
+
+var redirectableProcesses = []string{
+	"apm-server",
+	"fleet-server",
+}
+
+func processHandler(coord *coordinator.Coordinator, statsHandler func(http.ResponseWriter, *http.Request) error, operatingSystem string) func(http.ResponseWriter, *http.Request) error {
 	return func(w http.ResponseWriter, r *http.Request) error {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 		vars := mux.Vars(r)
-		id, found := vars[processIDKey]
 
+		id, found := vars[processIDKey]
 		if !found {
 			return errorfWithStatus(http.StatusNotFound, "productID not found")
 		}
@@ -31,6 +50,14 @@ func processHandler(coord *coordinator.Coordinator, statsHandler func(http.Respo
 		if id == "" || id == paths.BinaryName {
 			// proxy stats for elastic agent process
 			return statsHandler(w, r)
+		}
+
+		metricsPath := vars["metricsPath"]
+		if _, ok := redirectPathAllowlist[metricsPath]; ok {
+			if isProcessRedirectable(id) {
+				return redirectToPath(w, r, id, metricsPath, operatingSystem)
+			}
+			return errorfWithStatus(http.StatusNotFound, "endpoint not found")
 		}
 
 		state := coord.State(false)
@@ -60,4 +87,77 @@ func processHandler(coord *coordinator.Coordinator, statsHandler func(http.Respo
 
 		return errorWithStatus(http.StatusNotFound, fmt.Errorf("matching component %v not found", id))
 	}
+}
+
+func isProcessRedirectable(processName string) bool {
+	processNameLower := strings.ToLower(processName)
+	for _, prefix := range redirectableProcesses {
+		if strings.HasPrefix(processNameLower, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
+func redirectToPath(w http.ResponseWriter, r *http.Request, id, path, operatingSystem string) error {
+	endpoint := prefixedEndpoint(endpointPath(id, operatingSystem))
+	metricsBytes, statusCode, metricsErr := processMetrics(r.Context(), endpoint, path)
+	if metricsErr != nil {
+		return metricsErr
+	}
+
+	if statusCode > 0 {
+		w.WriteHeader(statusCode)
+	}
+
+	fmt.Fprint(w, string(metricsBytes))
+	return nil
+}
+
+func processMetrics(ctx context.Context, endpoint, path string) ([]byte, int, error) {
+	hostData, err := parseURL(endpoint, "http", "", "", path, "")
+	if err != nil {
+		return nil, 0, errorWithStatus(http.StatusInternalServerError, err)
+	}
+
+	dialer, err := hostData.transport.Make(timeout)
+	if err != nil {
+		return nil, 0, errorWithStatus(http.StatusInternalServerError, err)
+	}
+
+	client := http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			Dial: dialer.Dial,
+		},
+	}
+
+	req, err := http.NewRequest("GET", hostData.uri, nil)
+	if err != nil {
+		return nil, 0, errorWithStatus(
+			http.StatusInternalServerError,
+			fmt.Errorf("fetching metrics failed: %w", err),
+		)
+	}
+
+	req.Close = true
+	cctx, cancelFn := context.WithCancel(ctx)
+	defer cancelFn()
+
+	resp, err := client.Do(req.WithContext(cctx))
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		if errors.Is(err, syscall.ENOENT) {
+			statusCode = http.StatusNotFound
+		}
+		return nil, 0, errorWithStatus(statusCode, err)
+	}
+	defer resp.Body.Close()
+
+	rb, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, errorWithStatus(http.StatusInternalServerError, err)
+	}
+
+	return rb, resp.StatusCode, nil
 }
