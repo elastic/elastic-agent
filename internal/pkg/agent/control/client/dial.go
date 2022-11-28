8aa9b46eafe94650f22ede3ac5e1bc06c7596c6f
Michal Pristas <michal.pristas@gmail.com>
2022-11-28 08:27:31 +0100

    Fix diagnostics failures on max message size limit (#1777)
    Fix diagnostics failures on max message size limit (#1777)

--- internal/pkg/agent/control/client/dial.go
+++ internal/pkg/agent/control/client/dial.go
@@ -12,13 +12,21 @@ import (
 	"net"
 	"strings"
 
+	"github.com/elastic/elastic-agent/internal/pkg/agent/configuration"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/control"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func dialContext(ctx context.Context) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, strings.TrimPrefix(control.Address(), "unix://"), grpc.WithInsecure(), grpc.WithContextDialer(dialer))
+func dialContext(ctx context.Context, grpcConfig *configuration.GRPCConfig) (*grpc.ClientConn, error) {
+	return grpc.DialContext(
+		ctx,
+		strings.TrimPrefix(control.Address(), "unix://"),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(dialer),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcConfig.MaxMsgSize)),
+	)
 }
 
 func dialer(ctx context.Context, addr string) (net.Conn, error) {
