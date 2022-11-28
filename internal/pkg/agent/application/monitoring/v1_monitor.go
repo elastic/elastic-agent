ee2def81f84c1637c2431d754a2a3e16925ff576
Blake Rouse <blake.rouse@elastic.co>
2022-11-28 17:15:13 -0500

    Drop other.

--- internal/pkg/agent/application/monitoring/v1_monitor.go
+++ internal/pkg/agent/application/monitoring/v1_monitor.go
@@ -311,7 +311,7 @@ func (b *BeatsMonitor) injectLogsInput(cfg map[string]interface{}, componentIDTo
 				},
 			},
 			"processors": []interface{}{
-				// drop all events from filestream monitoring component (do it early; no need for extra processing)
+				// drop all events from monitoring components (do it early)
 				// without dropping these events the filestream gets stuck in an infinite loop
 				// if filestream hits an issue publishing the events it logs an error which then filestream monitor
 				// will read from the logs and try to also publish that new log message (thus the infinite loop)
@@ -322,6 +322,18 @@ func (b *BeatsMonitor) injectLogsInput(cfg map[string]interface{}, componentIDTo
 								map[string]interface{}{
 									"equals": map[string]interface{}{
 										"component.dataset": fmt.Sprintf("elastic_agent.filestream_%s", monitoringOutput),
+									},
+								},
+								// for consistency this monitor is also not shipped (fetch-able with diagnostics)
+								map[string]interface{}{
+									"equals": map[string]interface{}{
+										"component.dataset": fmt.Sprintf("elastic_agent.beats_metrics_%s", monitoringOutput),
+									},
+								},
+								// for consistency with this monitor is also not shipped (fetch-able with diagnostics)
+								map[string]interface{}{
+									"equals": map[string]interface{}{
+										"component.dataset": fmt.Sprintf("elastic_agent.http_metrics_%s", monitoringOutput),
 									},
 								},
 							},
