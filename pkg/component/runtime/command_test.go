7a245b624c8575f8ee6e8de3616e914d5acafed6
Michal Pristas <michal.pristas@gmail.com>
2022-12-29 08:21:25 +0100

    UT reduced wait time

--- pkg/component/runtime/command_test.go
+++ pkg/component/runtime/command_test.go
@@ -18,22 +18,22 @@ func TestAddToBucket(t *testing.T) {
 		addSleep    time.Duration
 		shouldBlock bool
 	}{
-		"no error":           {1, 0, 10 * time.Millisecond, false},
-		"error within limit": {1, 1, 10 * time.Millisecond, false},
-		"errors > than limit but across timespans":                {1, 2, 800 * time.Millisecond, false},
-		"errors > than limit within timespans, exact bucket size": {2, 2, 20 * time.Millisecond, false},
-		"errors > than limit within timespans, off by one":        {2, 3, 20 * time.Millisecond, true},
-		"errors > than limit within timespans":                    {2, 4, 20 * time.Millisecond, true},
+		"no error":           {1, 0, 1 * time.Millisecond, false},
+		"error within limit": {1, 1, 1 * time.Millisecond, false},
+		"errors > than limit but across timespans":                {1, 2, 80 * time.Millisecond, false},
+		"errors > than limit within timespans, exact bucket size": {2, 2, 2 * time.Millisecond, false},
+		"errors > than limit within timespans, off by one":        {2, 3, 2 * time.Millisecond, true},
+		"errors > than limit within timespans":                    {2, 4, 2 * time.Millisecond, true},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			dropRate := 500 * time.Millisecond
+			dropRate := 50 * time.Millisecond
 			b := newRateLimiter(dropRate, tc.bucketSize)
 
 			blocked := false
 			b.Allow()
-			<-time.After(dropRate + 200*time.Millisecond) // init ticker
+			<-time.After(dropRate + 20*time.Millisecond) // init ticker
 
 			for i := 0; i < tc.add; i++ {
 				wasBlocked := !b.Allow()
