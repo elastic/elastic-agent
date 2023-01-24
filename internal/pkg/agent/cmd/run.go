32ec28fc4a78dcf471969695764fe0066203cb16
Michal Pristas <michal.pristas@gmail.com>
2023-01-24 10:19:57 +0100

    Tag agent logs with 'service.name' (#2044)
    Tag agent logs with 'service.name' (#2044)
    
    (cherry picked from commit cd31f99219a272d4d2a806b90b75d4d2a093960c)

--- internal/pkg/agent/cmd/run.go
+++ internal/pkg/agent/cmd/run.go
@@ -109,10 +109,14 @@ func run(override cfgOverrider, modifiers ...component.PlatformModifier) error {
 	if cfg.Settings.LoggingConfig != nil {
 		logLvl = cfg.Settings.LoggingConfig.Level
 	}
-	l, err := logger.NewFromConfig("", cfg.Settings.LoggingConfig, true)
+	baseLogger, err := logger.NewFromConfig("", cfg.Settings.LoggingConfig, true)
 	if err != nil {
 		return err
 	}
+
+	l := baseLogger.With("log", map[string]interface{}{
+		"source": agentName,
+	})
 
 	cfg, err = tryDelayEnroll(ctx, l, cfg, override)
 	if err != nil {
@@ -188,7 +192,7 @@ func run(override cfgOverrider, modifiers ...component.PlatformModifier) error {
 		l.Info("APM instrumentation disabled")
 	}
 
-	coord, err := application.New(l, logLvl, agentInfo, rex, tracer, configuration.IsFleetServerBootstrap(cfg.Fleet), modifiers...)
+	coord, err := application.New(l, baseLogger, logLvl, agentInfo, rex, tracer, configuration.IsFleetServerBootstrap(cfg.Fleet), modifiers...)
 	if err != nil {
 		return err
 	}
