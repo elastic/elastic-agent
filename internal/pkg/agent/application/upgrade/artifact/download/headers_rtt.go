4707d199deb782e6130bcdbafd82234b8f62e119
Michal Pristas <michal.pristas@gmail.com>
2023-02-09 15:56:18 +0100

    missing files

---
+++ internal/pkg/agent/application/upgrade/artifact/download/headers_rtt.go
@@ -0,0 +1,35 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package download
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/elastic/elastic-agent/internal/pkg/release"
+)
+
+var Headers = map[string]string{
+	"User-Agent": fmt.Sprintf("Beat elastic-agent v%s", release.Version()),
+}
+
+func WithHeaders(rtt http.RoundTripper, headers map[string]string) http.RoundTripper {
+	if rtt == nil {
+		rtt = http.DefaultTransport
+	}
+	return &rttWithHeaders{target: rtt, headers: headers}
+}
+
+type rttWithHeaders struct {
+	target  http.RoundTripper
+	headers map[string]string
+}
+
+func (r *rttWithHeaders) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k, v := range r.headers {
+		req.Header.Add(k, v)
+	}
+	return r.target.RoundTrip(req)
+}
