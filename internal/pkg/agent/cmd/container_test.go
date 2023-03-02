b0fa96d79f6d2540e9993cd3405bdb3cf35ee836
Shaunak Kashyap <ycombinator@gmail.com>
2023-03-02 13:06:33 -0500

    Add unit test

---
+++ internal/pkg/agent/cmd/container_test.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/elastic/elastic-agent/internal/pkg/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestContainerTestPaths(t *testing.T) {
+	cases := map[string]struct {
+		config   string
+		expected containerPaths
+	}{
+		"only_state_path": {
+			config: `state_path: /foo/bar/state`,
+			expected: containerPaths{
+				StatePath:  "/foo/bar/state",
+				ConfigPath: "",
+				LogsPath:   "",
+			},
+		},
+		"only_config_path": {
+			config: `config_path: /foo/bar/config`,
+			expected: containerPaths{
+				StatePath:  "",
+				ConfigPath: "/foo/bar/config",
+				LogsPath:   "",
+			},
+		},
+		"only_logs_path": {
+			config: `logs_path: /foo/bar/logs`,
+			expected: containerPaths{
+				StatePath:  "",
+				ConfigPath: "",
+				LogsPath:   "/foo/bar/logs",
+			},
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			cfg, err := config.NewConfigFrom(c.config)
+			require.NoError(t, err)
+
+			var paths containerPaths
+			err = cfg.Unpack(&paths)
+			require.NoError(t, err)
+
+			require.Equal(t, c.expected, paths)
+		})
+	}
+}
