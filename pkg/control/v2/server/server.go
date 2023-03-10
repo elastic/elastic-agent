c21b4c6bbf13e5ec2a2f548ba9752d1cc88a5bcf
Shaunak Kashyap <ycombinator@gmail.com>
2023-03-09 20:00:26 -0500

    Ignore nil error

--- pkg/control/v2/server/server.go
+++ pkg/control/v2/server/server.go
@@ -168,6 +168,7 @@ func (s *Server) Restart(_ context.Context, _ *cproto.Empty) (*cproto.RestartRes
 func (s *Server) Upgrade(ctx context.Context, request *cproto.UpgradeRequest) (*cproto.UpgradeResponse, error) {
 	err := s.coord.Upgrade(ctx, request.Version, request.SourceURI, nil, request.SkipVerify, request.PgpBytes...)
 	if err != nil {
+		//nolint:nilerr // ignore the error, return a failure upgrade response
 		return &cproto.UpgradeResponse{
 			Status: cproto.ActionStatus_FAILURE,
 			Error:  err.Error(),
