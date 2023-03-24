7b4985c430ab4389d2b7d686be759e9543f2686c
Alex K <8418476+fearful-symmetry@users.noreply.github.com>
2023-03-24 15:54:50 -0700

    Fix timeout in coordinator shutdown loop (#2396)
    * fix timeout in coordinator shutdown loop
    
    * fix test, add changelog
    
    * add tests, check channel state
    
    * add test

--- internal/pkg/agent/application/coordinator/coordinator_test.go
+++ internal/pkg/agent/application/coordinator/coordinator_test.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	goruntime "runtime"
+	"strings"
 	"testing"
 	"time"
 
@@ -313,6 +314,114 @@ func TestCoordinator_StateSubscribe(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestCoordinatorWithErrors(t *testing.T) {
+	handlerChan, runtime, varWatcher, config := setupAndWaitCoordinatorDone()
+
+	close(runtime)
+	close(varWatcher)
+	cfgErrStr := "configWatcher error"
+	config <- errors.New(cfgErrStr)
+
+	// All the multierror stuff breaks errors.Is
+	waitAndTestError(t, func(err error) bool { return strings.Contains(err.Error(), cfgErrStr) }, handlerChan)
+
+}
+
+func TestCoordinatorShutdownClosedChannels(t *testing.T) {
+	CoordinatorShutdownTimeout = time.Second * 5
+	handlerChan, runtime, varWatcher, config := setupAndWaitCoordinatorDone()
+
+	close(runtime)
+	close(varWatcher)
+	close(config)
+
+	waitAndTestError(t, func(err error) bool { return errors.Is(err, context.Canceled) }, handlerChan)
+
+}
+
+func TestCoordinatorShutdownTimeout(t *testing.T) {
+	CoordinatorShutdownTimeout = time.Millisecond
+	handlerChan, _, _, _ := setupAndWaitCoordinatorDone()
+	waitAndTestError(t, func(err error) bool { return errors.Is(err, context.Canceled) }, handlerChan)
+}
+
+func TestCoordinatorShutdownErrorWithClose(t *testing.T) {
+	CoordinatorShutdownTimeout = time.Second * 5
+	handlerChan, runtime, varWatcher, config := setupAndWaitCoordinatorDone()
+	// return an error, then close the channel
+	cfgErrStr := "config watcher error"
+	config <- errors.New(cfgErrStr)
+	close(config)
+
+	close(runtime)
+	close(varWatcher)
+
+	waitAndTestError(t, func(err error) bool { return strings.Contains(err.Error(), cfgErrStr) }, handlerChan)
+}
+
+func TestCoordinatorShutdownErrorWithoutClose(t *testing.T) {
+	CoordinatorShutdownTimeout = time.Millisecond
+	handlerChan, _, _, config := setupAndWaitCoordinatorDone()
+
+	cfgErrStr := "config watcher error"
+	config <- errors.New(cfgErrStr)
+
+	waitAndTestError(t, func(err error) bool { return strings.Contains(err.Error(), cfgErrStr) }, handlerChan)
+}
+
+func TestCoordinatorShutdownErrorCloseTimeout(t *testing.T) {
+	CoordinatorShutdownTimeout = time.Second
+	handlerChan, _, varWatcher, config := setupAndWaitCoordinatorDone()
+	// return an error on one chanel, close another, let another time out
+	cfgErrStr := "config watcher error"
+	config <- errors.New(cfgErrStr)
+
+	close(varWatcher)
+
+	waitAndTestError(t, func(err error) bool { return strings.Contains(err.Error(), cfgErrStr) }, handlerChan)
+}
+
+func waitAndTestError(t *testing.T, check func(error) bool, handlerErr chan error) {
+	waitCtx, waitCancel := context.WithTimeout(context.Background(), time.Second*4)
+	defer waitCancel()
+	for {
+		select {
+		case <-waitCtx.Done():
+			t.Fatalf("handleCoordinatorDone timed out while waiting for shutdown")
+		case gotErr := <-handlerErr:
+			if handlerErr != nil {
+				if check(gotErr) {
+					t.Logf("got correct error")
+					return
+				} else {
+					t.Fatalf("got incorrect error: %s", gotErr)
+				}
+			}
+		}
+
+	}
+}
+
+func setupAndWaitCoordinatorDone() (chan error, chan error, chan error, chan error) {
+	runtime := make(chan error)
+	varWatcher := make(chan error)
+	config := make(chan error)
+
+	testCord := Coordinator{logger: logp.L()}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// emulate shutdown
+	cancel()
+
+	handlerChan := make(chan error)
+	go func() {
+		handlerErr := testCord.handleCoordinatorDone(ctx, varWatcher, runtime, config)
+		handlerChan <- handlerErr
+	}()
+
+	return handlerChan, runtime, varWatcher, config
+}
+
 func TestCoordinator_ReExec(t *testing.T) {
 	coordCh := make(chan error)
 	ctx, cancel := context.WithCancel(context.Background())
@@ -468,7 +577,7 @@ type fakeReExecManager struct {
 
 func (f *fakeReExecManager) ReExec(callback reexec.ShutdownCallbackFn, _ ...string) {
 	if callback != nil {
-		callback()
+		_ = callback()
 	}
 }
 
