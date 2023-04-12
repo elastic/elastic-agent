97739212eaa347c1ad50e93e2da6e549d31eb035
Shaunak Kashyap <ycombinator@gmail.com>
2023-04-12 16:31:44 -0700

    Update internal/pkg/agent/cmd/status.go
    Co-authored-by: David Kilfoyle <41695641+kilfoyle@users.noreply.github.com>

--- internal/pkg/agent/cmd/status.go
+++ internal/pkg/agent/cmd/status.go
@@ -35,7 +35,7 @@ func newStatusCommand(_ []string, streams *cli.IOStreams) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "status",
 		Short: "Show the current status of the running Elastic Agent daemon",
-		Long:  `This command will show the current status of the running Elastic Agent daemon.`,
+		Long:  `This command shows the current status of the running Elastic Agent daemon.`,
 		Run: func(c *cobra.Command, args []string) {
 			if err := statusCmd(streams, c, args); err != nil {
 				fmt.Fprintf(streams.Err, "Error: %v\n%s\n", err, troubleshootMessage())
