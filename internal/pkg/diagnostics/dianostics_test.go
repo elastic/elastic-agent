bac82993e971fa384d94ae564f07275d37f1b692
Lee E. Hinman <lee.e.hinman@elastic.co>
2023-05-01 09:47:17 -0500

    fix merge conflicts

--- internal/pkg/diagnostics/dianostics_test.go
+++ internal/pkg/diagnostics/dianostics_test.go
@@ -5,62 +5,17 @@
 package diagnostics
 
 import (
-	"archive/zip"
 	"bytes"
 	"compress/gzip"
 	"context"
 	"io"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/google/pprof/profile"
-
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 )
-
-func TestZipLogs(t *testing.T) {
-	// Setup a directory structure of: logs/httpjson/log.ndjson
-	{
-		paths.SetTop(t.TempDir())
-		dir := filepath.Join(paths.Home(), "logs/sub-dir")
-		require.NoError(t, os.MkdirAll(dir, 0o700))
-		require.NoError(t, os.WriteFile(filepath.Join(dir, "log.ndjson"), []byte(".\n"), 0o600))
-	}
-
-	// Zip the logs directory.
-	buf := new(bytes.Buffer)
-	w := zip.NewWriter(buf)
-	require.NoError(t, zipLogs(w, time.Now()))
-	require.NoError(t, w.Close())
-
-	type zippedItem struct {
-		Name  string
-		IsDir bool
-	}
-
-	// Read back the contents.
-	r, err := zip.NewReader(bytes.NewReader(buf.Bytes()), int64(buf.Len()))
-	require.NoError(t, err)
-	var observed []zippedItem
-	for _, f := range r.File {
-		observed = append(observed, zippedItem{Name: f.Name, IsDir: f.FileInfo().IsDir()})
-	}
-
-	// Verify the results.
-	expected := []zippedItem{
-		{"logs/", true},
-		{"logs/elastic-agent-unknow/", true},
-		{"logs/elastic-agent-unknow/sub-dir/", true},
-		{"logs/elastic-agent-unknow/sub-dir/log.ndjson", false},
-	}
-	assert.Equal(t, expected, observed)
-}
 
 func TestGlobalHooks(t *testing.T) {
 	hooks := GlobalHooks()
