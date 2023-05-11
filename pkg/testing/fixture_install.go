2eb28a9620d89ca899f1e37246fbfdee435e27aa
Shaunak Kashyap <ycombinator@gmail.com>
2023-05-11 01:56:44 -0700

    Add integration test for Agent installation (#2578)
    * Add integration test for Agent installation
    
    * Make test run on all three OSs
    
    * Use fixture
    
    * Fix missing imports
    
    * Check that `--base-path` does not show up in `elastic-agent help install` text
    
    * Extract const for flag; move folder creation past usage text assertion
    
    * Remove require.OS requirement
    
    * Isolate test
    
    * Define Install and Uninstall methods on Fixture
    
    * Removing info
    
    * Assert on error
    
    * Use testing TempDir for auto-cleanup
    
    * Add some enroll options
    
    * Use ErrNotInstalled

---
+++ pkg/testing/fixture_install.go
@@ -0,0 +1,135 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package testing
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+
+	"github.com/elastic/elastic-agent/pkg/core/process"
+)
+
+var ErrNotInstalled = errors.New("Elastic Agent is not installed")
+
+type CmdOpts interface {
+	toCmdArgs() []string
+}
+
+type EnrollOpts struct {
+	URL             string // --url
+	EnrollmentToken string // --enrollment-token
+}
+
+func (e EnrollOpts) toCmdArgs() []string {
+	var args []string
+	if e.URL != "" {
+		args = append(args, "--url", e.URL)
+	}
+	if e.EnrollmentToken != "" {
+		args = append(args, "--enrollment-token", e.EnrollmentToken)
+	}
+	return args
+}
+
+type InstallOpts struct {
+	BasePath       string // --base-path
+	Force          bool   // --force
+	NonInteractive bool   // --non-interactive
+
+	EnrollOpts
+}
+
+func (i InstallOpts) toCmdArgs() []string {
+	var args []string
+	if i.BasePath != "" {
+		args = append(args, "--base-path", i.BasePath)
+	}
+	if i.Force {
+		args = append(args, "--force")
+	}
+	if i.NonInteractive {
+		args = append(args, "--non-interactive")
+	}
+
+	args = append(args, i.EnrollOpts.toCmdArgs()...)
+
+	return args
+}
+
+// Install installs the prepared Elastic Agent binary
+func (f *Fixture) Install(ctx context.Context, installOpts *InstallOpts, opts ...process.CmdOption) ([]byte, error) {
+	installArgs := []string{"install"}
+	installArgs = append(installArgs, installOpts.toCmdArgs()...)
+	out, err := f.Exec(ctx, installArgs, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	f.installed = true
+	f.installOpts = installOpts
+
+	f.t.Cleanup(func() {
+		_, err := f.Uninstall(ctx, nil)
+		if err == ErrNotInstalled {
+			// Agent fixture has already been uninstalled, perhaps by
+			// an explicit call to fixture.Uninstall, so nothing needs
+			// to be done here.
+			return
+		}
+		require.NoError(f.t, err)
+	})
+
+	return out, nil
+}
+
+type UninstallOpts struct {
+	Force bool // --force
+}
+
+func (i UninstallOpts) toCmdArgs() []string {
+	var args []string
+	if i.Force {
+		args = append(args, "--force")
+	}
+
+	return args
+}
+
+// Uninstall uninstalls the installed Elastic Agent binary
+func (f *Fixture) Uninstall(ctx context.Context, uninstallOpts *UninstallOpts, opts ...process.CmdOption) ([]byte, error) {
+	if !f.installed {
+		return nil, ErrNotInstalled
+	}
+
+	uninstallArgs := []string{"uninstall"}
+	uninstallArgs = append(uninstallArgs, uninstallOpts.toCmdArgs()...)
+	out, err := f.Exec(ctx, uninstallArgs, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check that Elastic Agent files are actually removed
+	basePath := f.installOpts.BasePath
+	if basePath == "" {
+		basePath = paths.DefaultBasePath
+	}
+	topPath := filepath.Join(basePath, "Elastic", "Agent")
+	_, err = os.Stat(topPath)
+	if os.IsExist(err) {
+		return out, fmt.Errorf("Elastic Agent is still installed at [%s]", topPath)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
