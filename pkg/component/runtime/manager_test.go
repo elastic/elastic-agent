ea5c7c8de91f80cced498f05453591c9bc909d26
Fae Charlton <fae.charlton@elastic.co>
2023-05-29 08:31:06 -0400

    Add missing timeout value in unit test (#2737)
    (cherry picked from commit ab82582334df84efe3bfba609011abd228aa235a)

--- pkg/component/runtime/manager_test.go
+++ pkg/component/runtime/manager_test.go
@@ -1727,6 +1727,7 @@ func TestManager_FakeInput_RestartsOnMissedCheckins(t *testing.T) {
 					Timeouts: component.CommandTimeoutSpec{
 						// very low checkin timeout so we can cause missed check-ins
 						Checkin: 100 * time.Millisecond,
+						Restart: 10 * time.Second,
 						Stop:    30 * time.Second,
 					},
 				},
