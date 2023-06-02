cddf1e9214b5e69501eaf6455eedc807abbcdf0c
Pavel Zorin <pavel.zorin@elastic.co>
2023-06-02 16:54:43 +0200

    Fail step if it failed. HTML report

--- .buildkite/scripts/integration_tests.sh
+++ .buildkite/scripts/integration_tests.sh
@@ -60,4 +60,13 @@ export TEST_INTEG_AUTH_ESS_APIKEY_FILE=$(realpath ./apiKey)
 mage integration:auth
 AGENT_VERSION=8.9.0-SNAPSHOT mage integration:test
 
-ls build/
+# HTML report
+go install github.com/alexec/junit2html@latest
+junit2html < build/TEST-go-integration.xml > build/TEST-report.html
+
+
+# A HORRIBLE hack to detect test failures
+if grep "<failure" build/TEST-go-integration.xml; then  
+  echo "Tests failed."
+  exit 1
+fi
