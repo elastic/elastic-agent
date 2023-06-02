50428cde5be83f7a47ee947071c6f6c04e9bd972
Anderson Queiroz <anderson.queiroz@elastic.co>
2023-06-02 14:00:57 +0200

    Create a mock fleet server for integration tests (#2695)

---
+++ testing/fleetservertest/handlers.go
@@ -0,0 +1,113 @@
+package fleetservertest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const (
+	PathAgentAcks    = "/api/fleet/agents/{id}/acks"
+	PathAgentCheckin = "/api/fleet/agents/{id}/checkin"
+	PathAgentEnroll  = "/api/fleet/agents/{id}"
+
+	PathArtifact = "/api/fleet/artifacts/{id}/{sha2}"
+	PathStatus   = "/api/status"
+
+	PathUploadBegin    = "/api/fleet/uploads"
+	PathUploadChunk    = "/api/fleet/uploads/{id}/{chunkNum}"
+	PathUploadComplete = "/api/fleet/uploads/{id}"
+)
+
+func NewPathAgentAcks(agentID string) string {
+	return strings.Replace(PathAgentAcks, "{id}", agentID, 1)
+}
+
+func NewPathCheckin(agentID string) string {
+	return strings.Replace(PathAgentCheckin, "{id}", agentID, 1)
+}
+
+func NewPathAgentEnroll(agentID string) string {
+	return strings.Replace(PathAgentEnroll, "{id}", agentID, 1)
+}
+
+func NewPathArtifact(agentID, sha2 string) string {
+	return strings.Replace(
+		strings.Replace(PathArtifact, "{id}", agentID, 1),
+		"{sha2}",
+		sha2,
+		1)
+}
+
+func NewPathUploadBegin() string {
+	return PathUploadBegin
+}
+
+func NewPathUploadChunk(agentID, chunkNum string) string {
+	return strings.Replace(
+		strings.Replace(PathUploadChunk, "{id}", agentID, 1),
+		"{chunkNum}",
+		chunkNum,
+		1)
+}
+
+func NewPathUploadComplete(agentID string) string {
+	return strings.Replace(PathUploadComplete, "{id}", agentID, 1)
+}
+
+func NewCheckinHandler(agentID, ackToken string, withEndpoint bool) func(
+	ctx context.Context,
+	id string,
+	userAgent string,
+	acceptEncoding string,
+	checkinRequest CheckinRequest) (*CheckinResponse, *HTTPError) {
+
+	policy := checkinResponseJSONPolicySystemIntegration
+	if withEndpoint {
+		policy = checkinResponseJSONPolicySystemIntegrationAndEndpoint
+	}
+
+	return func(
+		ctx context.Context,
+		id string,
+		userAgent string,
+		acceptEncoding string,
+		checkinRequest CheckinRequest) (*CheckinResponse, *HTTPError) {
+
+		resp := CheckinResponse{}
+		err := json.Unmarshal(
+			[]byte(fmt.Sprintf(policy, ackToken, agentID)),
+			&resp)
+		if err != nil {
+			return nil, &HTTPError{
+				StatusCode: http.StatusInternalServerError,
+				Error:      err.Error(),
+				Message:    "failed to unmarshal policy",
+			}
+		}
+
+		return &resp, nil
+	}
+}
+
+func NewStatusHandlerHealth() func(ctx context.Context) (*StatusResponse, *HTTPError) {
+	return func(ctx context.Context) (*StatusResponse, *HTTPError) {
+		return &StatusResponse{
+			Name:   "fleet-server",
+			Status: "HEALTHY",
+			// fleet-server does not respond with version information
+		}, nil
+	}
+}
+
+func NewStatusHandlerUnhealth() func(ctx context.Context) (*StatusResponse, *HTTPError) {
+	return func(ctx context.Context) (*StatusResponse, *HTTPError) {
+		return &StatusResponse{
+			Name:   "fleet-server",
+			Status: "UNHEALTHY",
+			// fleet-server does not respond with version information
+		}, &HTTPError{StatusCode: http.StatusInternalServerError}
+	}
+}
