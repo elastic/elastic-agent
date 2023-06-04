c6ae65bb599a3958894cf18099c9cc9c4ea916e2
Shaunak Kashyap <ycombinator@gmail.com>
2023-06-04 13:58:55 -0700

    Add unit test

--- internal/pkg/agent/application/upgrade/step_download_test.go
+++ internal/pkg/agent/application/upgrade/step_download_test.go
@@ -1,9 +1,63 @@
 package upgrade
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
+
+	"github.com/elastic/elastic-agent-libs/transport/httpcommon"
+
+	"github.com/elastic/elastic-agent/pkg/core/logger"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/artifact/download"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/artifact"
+)
+
+type mockDownloader struct {
+	downloadErr  error
+	downloadPath string
+}
+
+func (md *mockDownloader) Download(ctx context.Context, agentArtifact artifact.Artifact, version string) (string, error) {
+	return md.downloadPath, md.downloadErr
+}
 
 func TestDownloadWithRetries(t *testing.T) {
+	testLogger, _ := logger.NewTesting("TestDownloadWithRetries")
+
 	// Successful immediately (no retries)
-	// Successful after first attempt (at least one retry)
+	t.Run("successful_immediately", func(t *testing.T) {
+		expectedDownloadPath := "https://artifacts.elastic.co/downloads/beats/elastic-agent"
+		mockDownloaderCtor := func(version string, log *logger.Logger, settings *artifact.Config) (download.Downloader, error) {
+			return &mockDownloader{
+				downloadErr:  nil,
+				downloadPath: expectedDownloadPath,
+			}, nil
+		}
+
+		settings := artifact.Config{
+			RetryMaxCount: 10,
+			HTTPTransportSettings: httpcommon.HTTPTransportSettings{
+				Timeout: 2 * time.Second,
+			},
+		}
+
+		u := NewUpgrader(testLogger, &settings, &info.AgentInfo{})
+		path, err := u.downloadWithRetries(context.Background(), mockDownloaderCtor, "8.9.0", &settings)
+		require.NoError(t, err)
+		require.Equal(t, expectedDownloadPath, path)
+	})
+
+	// Successful after first attempt (at least one retry) with
+	// downloader constructor failing
+
+	// Successful after first attempt (at least one retry) with
+	// download failing
+
 	// Unsuccessful (all retries exhausted)
 }
