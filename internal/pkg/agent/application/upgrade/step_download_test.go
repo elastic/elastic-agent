1c855f1d17a829f678c69d55c3e20c9861930d98
Shaunak Kashyap <ycombinator@gmail.com>
2023-06-04 14:03:13 -0700

    Check log entry in unit test

--- internal/pkg/agent/application/upgrade/step_download_test.go
+++ internal/pkg/agent/application/upgrade/step_download_test.go
@@ -28,7 +28,7 @@ func (md *mockDownloader) Download(ctx context.Context, agentArtifact artifact.A
 }
 
 func TestDownloadWithRetries(t *testing.T) {
-	testLogger, _ := logger.NewTesting("TestDownloadWithRetries")
+	testLogger, obs := logger.NewTesting("TestDownloadWithRetries")
 
 	// Successful immediately (no retries)
 	t.Run("successful_immediately", func(t *testing.T) {
@@ -51,6 +51,10 @@ func TestDownloadWithRetries(t *testing.T) {
 		path, err := u.downloadWithRetries(context.Background(), mockDownloaderCtor, "8.9.0", &settings)
 		require.NoError(t, err)
 		require.Equal(t, expectedDownloadPath, path)
+
+		logs := obs.All()
+		require.Len(t, logs, 1)
+		require.Equal(t, "download attempt 1 of 11", logs[0].Message)
 	})
 
 	// Successful after first attempt (at least one retry) with
