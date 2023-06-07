7b8388436f9979529348e1532100e247663f5d6d
Pavel Zorin <pavel.zorin@elastic.co>
2023-06-07 15:07:47 +0200

    Moved variables initilissation to pre-command hook

--- .buildkite/scripts/integration_tests.sh
+++ .buildkite/scripts/integration_tests.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+# Install Go TODO: mode to makefile
+if ! command -v go &>/dev/null; then  
+  echo "Go is not installed. Installing Go..."
+  export GO_VERSION=`cat .go-version`
+  curl -O https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
+  sudo tar -xf go$GO_VERSION.linux-amd64.tar.gz -C /usr/local
+  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+  source ~/.bashrc
+  mkdir $HOME/go
+  mkdir $HOME/go/bin
+  export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin  
+  echo "Go has been installed."
+else
+  echo "Go is already installed."
+fi
+
 # PACKAGE
 DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64 PACKAGES=tar.gz mage -v package
 
