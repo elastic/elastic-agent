864fc9fd21804a16a102694aa2f32f28406ccda1
Shaunak Kashyap <ycombinator@gmail.com>
2023-06-08 14:16:33 -0700

    Fix imports

--- testing/integration/upgrade_test.go
+++ testing/integration/upgrade_test.go
@@ -17,20 +17,18 @@ import (
 	"testing"
 	"time"
 
-	"github.com/elastic/elastic-agent/internal/pkg/release"
 	"gopkg.in/yaml.v2"
-
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade"
-
-	"github.com/google/uuid"
-
-	"github.com/elastic/elastic-agent-libs/kibana"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/google/uuid"
+
+	"github.com/elastic/elastic-agent-libs/kibana"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade"
+	"github.com/elastic/elastic-agent/internal/pkg/release"
 	"github.com/elastic/elastic-agent/pkg/control/v2/cproto"
 	atesting "github.com/elastic/elastic-agent/pkg/testing"
 	"github.com/elastic/elastic-agent/pkg/testing/define"
