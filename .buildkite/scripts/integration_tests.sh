9805efc7d2a3242e52de3dea9ca6f0dbea18710b
Pavel Zorin <pavel.zorin@elastic.co>
2023-06-09 10:53:29 +0200

    Removed AGENT_VERSION

--- .buildkite/scripts/integration_tests.sh
+++ .buildkite/scripts/integration_tests.sh
@@ -24,7 +24,7 @@ make mage
 DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64 PACKAGES=tar.gz mage package
 
 # Run integration tests
-mage integration:test
+SNAPSHOT=true mage integration:test
 TESTS_EXIT_STATUS=$?
 
 # HTML report
