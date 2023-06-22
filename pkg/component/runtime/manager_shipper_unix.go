c95b89089d3297c2e2e32d3d1cbc7ef4a5190b72
Lee E. Hinman <lee.e.hinman@elastic.co>
2023-06-22 10:55:47 -0500

    change shipper socket

--- pkg/component/runtime/manager_shipper_unix.go
+++ pkg/component/runtime/manager_shipper_unix.go
@@ -7,26 +7,10 @@
 package runtime
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"path/filepath"
-
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+	"github.com/elastic/elastic-agent/pkg/utils"
 )
 
 func getShipperAddr(componentID string) string {
-	// when installed the address is fixed to a location
-	if info.RunningInstalled() {
-		return fmt.Sprintf(paths.ShipperSocketPipePattern, componentID)
-	}
-
-	// unix socket path must be less than 104 characters
-	path := fmt.Sprintf("unix://%s.sock", filepath.Join(paths.TempDir(), fmt.Sprintf("elastic-agent-%s-pipe", componentID)))
-	if len(path) < 104 {
-		return path
-	}
-	// place in global /tmp to ensure that its small enough to fit; current path is way to long
-	// for it to be used, but needs to be unique per Agent (in the case that multiple are running)
-	return fmt.Sprintf(`unix:///tmp/elastic-agent/%x.sock`, sha256.Sum256([]byte(path)))
+	return utils.SocketURLWithFallback(componentID, "unix", paths.TempDir())
 }
--- pkg/component/runtime/manager_shipper_windows.go
+++ pkg/component/runtime/manager_shipper_windows.go
@@ -7,22 +7,10 @@
 package runtime
 
 import (
-	"crypto/sha256"
-	"fmt"
-
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+	"github.com/elastic/elastic-agent/pkg/utils"
 )
 
 func getShipperAddr(componentID string) string {
-	// when installed the address is fixed to a location
-	if info.RunningInstalled() {
-		return fmt.Sprintf(paths.ShipperSocketPipePattern, componentID)
-	}
-
-	// not install, adjust the path based on data path
-	data := paths.Data()
-	// entire string cannot be longer than 256 characters, this forces the
-	// length to always be 87 characters (but unique per data path)
-	return fmt.Sprintf(`\\.\pipe\elastic-agent-%x-%s-pipe`, sha256.Sum256([]byte(data)), componentID)
+	return utils.SocketURLWithFallback(componentID, "windows", paths.TempDir())
 }
