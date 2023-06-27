286b01e54c0baf8eb7b4a2c6f5ddb76735571562
Aleksandr Maus <aleksandr.maus@elastic.co>
2023-06-27 12:55:16 -0400

    Restore logging statement that was unnecessarily changed

--- internal/pkg/agent/application/actions/handlers/handler_action_upgrade.go
+++ internal/pkg/agent/application/actions/handlers/handler_action_upgrade.go
@@ -71,7 +71,7 @@ func (h *Upgrade) Handle(ctx context.Context, a fleetapi.Action, ack acker.Acker
 	}
 
 	go func() {
-		h.log.Debugf("starting upgrade to version %s in background", action.Version)
+		h.log.Infof("starting upgrade to version %s in background", action.Version)
 		if err := h.coord.Upgrade(asyncCtx, action.Version, action.SourceURI, action, false); err != nil {
 			h.log.Errorf("upgrade to version %s failed: %v", action.Version, err)
 			// If context is cancelled in getAsyncContext, the actions are acked there
