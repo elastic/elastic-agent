0ad8280e5c1e9b9f1b1da9633b46670fe846d871
Blake Rouse <blake.rouse@elastic.co>
2023-07-06 02:17:11 -0400

    Add support for timestamps on runner (#2998)
    * Add support for timestamps on runner.
    
    * Fix printing missing timestamps.

--- magefile.go
+++ magefile.go
@@ -1474,13 +1474,13 @@ func integRunner(ctx context.Context, matrix bool, singleTest string) error {
 		return fmt.Errorf("error writing test out xml file: %w", err)
 	}
 	if results.Failures > 0 {
-		fmt.Printf(">>> Testing completed (%d failures, %d successful)\n", results.Failures, results.Tests-results.Failures)
+		r.Logger().Logf("Testing completed (%d failures, %d successful)", results.Failures, results.Tests-results.Failures)
 	} else {
-		fmt.Printf(">>> Testing completed (%d successful)\n", results.Tests)
+		r.Logger().Logf("Testing completed (%d successful)", results.Tests)
 	}
-	fmt.Printf(">>> Console output written here: build/TEST-go-integration.out\n")
-	fmt.Printf(">>> Console JSON output written here: build/TEST-go-integration.out.json\n")
-	fmt.Printf(">>> JUnit XML written here: build/TEST-go-integration.xml\n")
+	r.Logger().Logf("Console output written here: build/TEST-go-integration.out")
+	r.Logger().Logf("Console JSON output written here: build/TEST-go-integration.out.json")
+	r.Logger().Logf("JUnit XML written here: build/TEST-go-integration.xml")
 	if results.Failures > 0 {
 		os.Exit(1)
 	}
@@ -1538,6 +1538,7 @@ func createTestRunner(matrix bool, singleTest string, batches ...define.Batch) (
 	if essRegion == "" {
 		essRegion = "gcp-us-central1"
 	}
+	timestamp := timestampEnabled()
 	r, err := runner.NewRunner(runner.Config{
 		AgentVersion:      agentVersion,
 		AgentStackVersion: agentStackVersion,
@@ -1555,6 +1556,7 @@ func createTestRunner(matrix bool, singleTest string, batches ...define.Batch) (
 		Matrix:      matrix,
 		SingleTest:  singleTest,
 		VerboseMode: mg.Verbose(),
+		Timestamp:   timestamp,
 	}, batches...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create runner: %w", err)
@@ -1572,6 +1574,15 @@ func shouldBuildAgent() bool {
 		return false
 	}
 	return ret
+}
+
+func timestampEnabled() bool {
+	timestamp := os.Getenv("TEST_INTEG_TIMESTAMP")
+	if timestamp == "" {
+		return false
+	}
+	b, _ := strconv.ParseBool(timestamp)
+	return b
 }
 
 // Pre-requisite: user must have the gcloud CLI installed
