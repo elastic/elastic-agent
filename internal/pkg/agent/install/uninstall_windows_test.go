6e546dcaf9f7d23b75fe517eced033d553d1246f
Blake Rouse <blake.rouse@elastic.co>
2023-07-06 21:24:41 -0400

    Improve uninstall on Windows to remove directory even when uninstall is being executed from same directory (#3014)
    * Change how we delete topDir on Windows to work when exe is running inside of dir.
    
    * Add changelog entry.
    
    * Fix lint.
    
    * Fix lint.
    
    * Changes from code review
    
    * Change to waiting forever
    
    (cherry picked from commit 087396065b25ad049655795ac9022cfd80a3cf07)

---
+++ internal/pkg/agent/install/uninstall_windows_test.go
@@ -0,0 +1,55 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+//go:build windows
+
+package install
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const simpleBlockForever = `
+package main
+
+import (
+    "math"
+    "time"
+)
+
+func main() {
+    <-time.After(time.Duration(math.MaxInt64))
+}
+`
+
+func TestRemovePath(t *testing.T) {
+	dir := filepath.Join(t.TempDir(), "subdir")
+	err := os.Mkdir(dir, 0644)
+	require.NoError(t, err)
+
+	src := filepath.Join(dir, "main.go")
+	err = os.WriteFile(src, []byte(simpleBlockForever), 0644)
+	require.NoError(t, err)
+
+	binary := filepath.Join(dir, "main.exe")
+	cmd := exec.Command("go", "build", "-o", binary, src)
+	_, err = cmd.CombinedOutput()
+	require.NoError(t, err)
+
+	cmd = exec.Command(binary)
+	err = cmd.Start()
+	require.NoError(t, err)
+	defer func() {
+		_ = cmd.Process.Kill()
+		_ = cmd.Wait()
+	}()
+
+	err = RemovePath(dir)
+	require.NoError(t, err)
+}
