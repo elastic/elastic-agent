df52b140c58c61fe0fa5e654eb7b98a21ac2718f
Fae Charlton <fae.charlton@elastic.co>
2023-07-12 17:00:08 -0400

    disable flaky test

--- internal/pkg/agent/install/uninstall_windows_test.go
+++ internal/pkg/agent/install/uninstall_windows_test.go
@@ -29,6 +29,7 @@ func main() {
 `
 
 func TestRemovePath(t *testing.T) {
+	t.Skip("Flaky test https://github.com/elastic/elastic-agent/issues/3069")
 	dir := filepath.Join(t.TempDir(), "subdir")
 	err := os.Mkdir(dir, 0644)
 	require.NoError(t, err)
