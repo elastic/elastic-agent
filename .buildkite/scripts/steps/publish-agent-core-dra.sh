7bbce227fad9912457743311914454b13a8a4d2b
Marc Guasch <marc.guasch@elastic.co>
2023-07-14 12:00:51 +0200

    Split in various steps and move creds to pre-command

--- .buildkite/scripts/steps/publish-agent-core-dra.sh
+++ .buildkite/scripts/steps/publish-agent-core-dra.sh
@@ -4,20 +4,12 @@ set -uo pipefail
 
 source .buildkite/scripts/bootstrap.sh
 
-echo "+++ Setting DRA params"
-# Shared secret path containing the dra creds for project teams
-DRA_CREDS=$(vault kv get -field=data -format=json kv/ci-shared/release/dra-role)
-VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr')
-VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id')
-VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id')
-export VAULT_ADDR VAULT_ROLE_ID VAULT_SECRET_ID
-
 # Publish DRA artifacts
 function run_release_manager() {
     echo "+++ Publishing $BUILDKITE_BRANCH ${WORKFLOW} DRA artifacts..."
     dry_run=""
     if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
-        # dry_run="--dry-run"
+        dry_run="--dry-run"
         # force main branch on PR's or it won't execute
         # because the PR branch does not have a project folder in release-manager
         BRANCH=main
@@ -25,8 +17,8 @@ function run_release_manager() {
     docker run --rm \
         --name release-manager \
         -e VAULT_ADDR="${VAULT_ADDR}" \
-        -e VAULT_ROLE_ID="${VAULT_ROLE_ID}" \
-        -e VAULT_SECRET_ID="${VAULT_SECRET_ID}" \
+        -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
+        -e VAULT_SECRET_ID="${VAULT_SECRET}" \
         --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
         docker.elastic.co/infra/release-manager:latest \
         cli collect \
@@ -38,6 +30,8 @@ function run_release_manager() {
         --artifact-set agent-core \
         $dry_run
 }
+
+buildkite-agent artifact download "build/" .
 
 run_release_manager
 RM_EXIT_CODE=$?
