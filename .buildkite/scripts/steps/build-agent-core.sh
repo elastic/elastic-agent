349cd443f668ac0adc9b9958eea284b7c0bffe3b
Marc Guasch <marc.guasch@elastic.co>
2023-07-14 12:36:59 +0200

    Use version for dependencies file

--- .buildkite/scripts/steps/build-agent-core.sh
+++ .buildkite/scripts/steps/build-agent-core.sh
@@ -15,6 +15,6 @@ chmod -R 777 build/distributions
 echo  "+++ Generate dependencies report"
 ./dev-tools/dependencies-report
 mkdir -p build/distributions/reports
-mv dependencies.csv build/distributions/reports/dependencies.csv
+mv dependencies.csv "build/distributions/reports/dependencies-${BEAT_VERSION}.csv"
 
 buildkite-agent artifact upload "build/**/*"
