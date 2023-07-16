b882ee9c8e07acb985e727daf15459e09e29182a
Alexandros, Sapranidis <alexandros@elastic.co>
2023-07-16 18:33:49 +0300

    Make COMMIT and BRANCH variables
    The job will now accept the DRA_COMMIT and DRA_BRANCH as variables for
    the build for the release manager.
    
    Signed-off-by: Alexandros, Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/steps/dra-publish.sh
+++ .buildkite/scripts/steps/dra-publish.sh
@@ -7,12 +7,12 @@ source "${_SELF}/../common.sh"
 
 # Publish DRA artifacts
 function run_release_manager() {
-    local PROJECT_ID="${1}" PROJECT_ARTIFACT_ID="${2}" WORKFLOW="${3}" DRY_RUN="${4}"
+    local PROJECT_ID="${1}" PROJECT_ARTIFACT_ID="${2}" WORKFLOW="${3}" COMMIT="${4}" BRANCH="${5}" DRY_RUN="${6}"
     if [[ -z "${WORKFLOW}" ]]; then
       echo "+++ Missing DRA workflow";
       exit 1
     fi
-    if [[ -z "${BUILDKITE_COMMIT:-""}" ]]; then
+    if [[ -z "${COMMIT:-""}" ]]; then
       echo "+++ Missing git commit sha";
       exit 1
     fi
@@ -20,6 +20,10 @@ function run_release_manager() {
       echo "+++ Missing BEAT_VERSION";
       exit 1
     fi
+    if [[ -z "${BRANCH:-""}" ]]; then
+          echo "+++ Missing BRANCH";
+          exit 1
+        fi
 
     echo "+++ :hammer_and_pick: Publishing $BUILDKITE_BRANCH ${WORKFLOW} DRA artifacts..."
     if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
@@ -38,7 +42,7 @@ function run_release_manager() {
         cli collect \
         --project "${PROJECT_ID}" \
         --branch "${BRANCH}" \
-        --commit "${BUILDKITE_COMMIT}" \
+        --commit "${COMMIT}" \
         --workflow "${WORKFLOW}" \
         --version "${BEAT_VERSION}" \
         --artifact-set "${PROJECT_ARTIFACT_ID}" \
@@ -47,6 +51,9 @@ function run_release_manager() {
 
 DRA_DRY_RUN="${DRA_DRY_RUN:="--dry-run"}"
 WORKFLOW="${DRA_WORKFLOW:=""}"
+COMMIT="${DRA_COMMIT:=""}"
+BRANCH="${DRA_BRANCH:=""}"
+
 if [[ -z "${WORKFLOW:=""}" ]]; then
   if [[ "${ManifestURL}" =~ "staging" ]]; then
     WORKFLOW="staging"
@@ -55,9 +62,8 @@ if [[ -z "${WORKFLOW:=""}" ]]; then
     WORKFLOW="snapshot"
   fi
 fi
-echo "+++ Release Manager ${WORKFLOW}";
-run_release_manager "${DRA_PROJECT_ID}" "${DRA_PROJECT_ARTIFACT_ID}" "${WORKFLOW:=""}" "${DRA_DRY_RUN}"
+echo "+++ Release Manager ${WORKFLOW} / ${BRANCH} / ${COMMIT}";
+run_release_manager "${DRA_PROJECT_ID}" "${DRA_PROJECT_ARTIFACT_ID}" "${WORKFLOW:=""}" "${COMMIT}" "${BRANCH}" "${DRA_DRY_RUN}"
 
 RM_EXIT_CODE=$?
-
 exit $RM_EXIT_CODE
