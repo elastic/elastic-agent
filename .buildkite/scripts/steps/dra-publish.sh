5ea4d0c8fc482d7c463b441f6a34030730d71b4f
Alexandros Sapranidis <alexandros@elastic.co>
2023-07-19 14:20:14 +0300

    Update dra-publish script and update parameters
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/steps/dra-publish.sh
+++ .buildkite/scripts/steps/dra-publish.sh
@@ -2,37 +2,61 @@
 
 set -uo pipefail
 
-_SELF=$(dirname $0)
-source "${_SELF}/../common.sh"
+DRA_DRY_RUN="${DRA_DRY_RUN:="--dry-run"}"
+WORKFLOW="${DRA_WORKFLOW:=""}"
+COMMIT="${DRA_COMMIT:=""}"
+BRANCH="${DRA_BRANCH:=""}"
+PACKAGE_VERSION="${DRA_VERSION:=""}"
+
+if [[ -z "${WORKFLOW}" ]]; then
+  echo "+++ Missing DRA workflow";
+  exit 1
+fi
+if [[ -z "${COMMIT:-""}" ]]; then
+  echo "+++ Missing DRA_COMMIT";
+  exit 1
+fi
+if [[ -z "${PACKAGE_VERSION:-""}" ]]; then
+  echo "+++ Missing DRA_VERSION";
+  exit 1
+fi
+if [[ -z "${BRANCH:-""}" ]]; then
+  echo "+++ Missing DRA_BRANCH";
+  exit 1
+fi
+
+# force main branch on PR's or it won't execute
+# because the PR branch does not have a project folder in release-manager
+if [ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" ]; then
+    DRA_BRANCH=main
+    DRA_DRY_RUN="--dry-run"
+fi
+
+# Listing Release manager
+function run_release_manager_list() {
+    local _project_id="${1}" _artifact_set="${2}" _workflow="${3}" _commit="${4}" _branch="${5}" _version="${6}"
+    echo "+++ :hammer_and_pick: Release manager listing ${_branch} ${_workflow} DRA artifacts..."
+    echo docker run --rm \
+        --name release-manager \
+        -e VAULT_ADDR="${VAULT_ADDR_DRA}" \
+        -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
+        -e VAULT_SECRET_ID="${VAULT_SECRET}" \
+        --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
+        docker.elastic.co/infra/release-manager:latest \
+        cli list \
+        --project "${_project_id}" \
+        --branch "${_branch}" \
+        --commit "${_commit}" \
+        --workflow "${_workflow}" \
+        --version "${_version}" \
+        --artifact-set "${_artifact_set}"
+}
 
 # Publish DRA artifacts
-function run_release_manager() {
-    local PROJECT_ID="${1}" PROJECT_ARTIFACT_ID="${2}" WORKFLOW="${3}" COMMIT="${4}" BRANCH="${5}" DRY_RUN="${6}"
-    if [[ -z "${WORKFLOW}" ]]; then
-      echo "+++ Missing DRA workflow";
-      exit 1
-    fi
-    if [[ -z "${COMMIT:-""}" ]]; then
-      echo "+++ Missing git commit sha";
-      exit 1
-    fi
-    if [[ -z "${BEAT_VERSION:-""}" ]]; then
-      echo "+++ Missing BEAT_VERSION";
-      exit 1
-    fi
-    if [[ -z "${BRANCH:-""}" ]]; then
-          echo "+++ Missing BRANCH";
-          exit 1
-        fi
-
-    echo "+++ :hammer_and_pick: Publishing $BUILDKITE_BRANCH ${WORKFLOW} DRA artifacts..."
-    if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
-        # force main branch on PR's or it won't execute
-        # because the PR branch does not have a project folder in release-manager
-        BRANCH=main
-        DRY_RUN="--dry-run"
-    fi
-    docker run --rm \
+function run_release_manager_collect() {
+    local _project_id="${1}" _artifact_set="${2}" _workflow="${3}" _commit="${4}" _branch="${5}" _version="${6}" _dry_run="${6}"
+    echo "+++ :hammer_and_pick: Publishing ${_branch} ${_workflow} DRA artifacts..."
+    echo docker run --rm \
         --name release-manager \
         -e VAULT_ADDR="${VAULT_ADDR_DRA}" \
         -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
@@ -40,30 +64,18 @@ function run_release_manager() {
         --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
         docker.elastic.co/infra/release-manager:latest \
         cli collect \
-        --project "${PROJECT_ID}" \
-        --branch "${BRANCH}" \
-        --commit "${COMMIT}" \
-        --workflow "${WORKFLOW}" \
-        --version "${BEAT_VERSION}" \
-        --artifact-set "${PROJECT_ARTIFACT_ID}" \
-        "${DRY_RUN}"
+        --project "${_project_id}" \
+        --branch "${_branch}" \
+        --commit "${_commit}" \
+        --workflow "${_workflow}" \
+        --version "${_version}" \
+        --artifact-set "${_artifact_set}" \
+        "${_dry_run}"
 }
 
-DRA_DRY_RUN="${DRA_DRY_RUN:="--dry-run"}"
-WORKFLOW="${DRA_WORKFLOW:=""}"
-COMMIT="${DRA_COMMIT:=""}"
-BRANCH="${DRA_BRANCH:=""}"
-
-if [[ -z "${WORKFLOW:=""}" ]]; then
-  if [[ "${ManifestURL}" =~ "staging" ]]; then
-    WORKFLOW="staging"
-  fi
-  if [[ "${ManifestURL}" =~ "snapshots" ]]; then
-    WORKFLOW="snapshot"
-  fi
-fi
 echo "+++ Release Manager ${WORKFLOW} / ${BRANCH} / ${COMMIT}";
-run_release_manager "${DRA_PROJECT_ID}" "${DRA_PROJECT_ARTIFACT_ID}" "${WORKFLOW:=""}" "${COMMIT}" "${BRANCH}" "${DRA_DRY_RUN}"
+run_release_manager_list "${DRA_PROJECT_ID}" "${DRA_PROJECT_ARTIFACT_ID}" "${WORKFLOW}" "${COMMIT}" "${BRANCH}" "${PACKAGE_VERSION}"
+run_release_manager_collect "${DRA_PROJECT_ID}" "${DRA_PROJECT_ARTIFACT_ID}" "${WORKFLOW}" "${COMMIT}" "${BRANCH}" "${PACKAGE_VERSION}" "${DRA_DRY_RUN}"
 
 RM_EXIT_CODE=$?
 exit $RM_EXIT_CODE
