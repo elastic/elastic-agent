0836a4d5cf53e61dfec1fdda829c7d3d9477d10b
Alexandros Sapranidis <alexandros@elastic.co>
2023-07-26 14:55:16 +0300

    Adjust the pre-exit functions
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -108,18 +108,3 @@ retry() {
     done
     return 0
 }
-
-function docker_login {
-  export DOCKER_USERNAME_SECRET=$(retry 5 vault kv get -field user "${DOCKER_REGISTRY_SECRET_PATH}")
-  export DOCKER_PASSWORD_SECRET=$(retry 5 vault kv get -field password "${DOCKER_REGISTRY_SECRET_PATH}")
-  docker login -u "${DOCKER_USERNAME_SECRET}" -p "${DOCKER_PASSWORD_SECRET}" "${DOCKER_REGISTRY}" 2>/dev/null
-  unset DOCKER_USERNAME_SECRET DOCKER_PASSWORD_SECRET
-}
-
-function release_manager_login {
-  DRA_CREDS_SECRET=$(retry 5 vault kv get -field=data -format=json ${CI_DRA_ROLE_PATH})
-  VAULT_ADDR_SECRET=$(echo ${DRA_CREDS_SECRET} | jq -r '.vault_addr')
-  VAULT_ROLE_ID_SECRET=$(echo ${DRA_CREDS_SECRET} | jq -r '.role_id')
-  VAULT_SECRET=$(echo ${DRA_CREDS_SECRET} | jq -r '.secret_id')
-  export VAULT_ADDR_SECRET VAULT_ROLE_ID_SECRET VAULT_SECRET
-}
