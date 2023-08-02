ef90a403c2afb8473d9937e2402fbe3b35bb4c62
Marc Guasch <marc.guasch@elastic.co>
2023-08-02 09:22:07 +0200

    Pass dra_version through meta-data

--- .buildkite/scripts/steps/build-agent-core.sh
+++ .buildkite/scripts/steps/build-agent-core.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-source .buildkite/scripts/common.sh
-BEAT_VERSION=$(grep -oE '[0-9]+\.[0-9]+\.[0-9]+(\-[a-zA-Z]+[0-9]+)?' "${WORKSPACE}/version/version.go")
-export DRA_VERSION="${BEAT_VERSION}"
-
 echo "+++ Build Agent artifacts"
 SNAPSHOT=""
 BEAT_VERSION_FULL=$BEAT_VERSION
