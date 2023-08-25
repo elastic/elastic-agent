8a19380b2373f2da04d5cd88cdf9e1806655ec4a
Anderson Queiroz <anderson.queiroz@elastic.co>
2023-08-25 07:34:12 +0200

    make teat work with only one snapshot version
    It fixes TestStandaloneUpgradeToSpecificSnapshotBuild to work is there is only one snapshot build available.

--- pkg/testing/tools/artifacts_api.go
+++ pkg/testing/tools/artifacts_api.go
@@ -23,7 +23,7 @@ const (
 	artifactsAPIV1VersionsEndpoint      = "v1/versions/"
 	artifactsAPIV1VersionBuildsEndpoint = "v1/versions/%s/builds/"
 	artifactAPIV1BuildDetailsEndpoint   = "v1/versions/%s/builds/%s"
-	//artifactAPIV1SearchVersionPackage = "v1/search/%s/%s"
+	// artifactAPIV1SearchVersionPackage = "v1/search/%s/%s"
 )
 
 var (
@@ -275,9 +275,10 @@ func GetLatestSnapshotVersion(ctx context.Context, log logger, aac *ArtifactAPIC
 		return nil, ErrSnapshotVersionsEmpty
 	}
 
-	// normally the output of the versions returned by artifact API is already sorted in ascending order,
-	// if we want to sort in descending order we could use
-	sort.Sort(sort.Reverse(sortedParsedVersions))
+	// normally the output of the versions returned by artifact API is already
+	// sorted in ascending order.If we want to sort in descending order we can
+	// use sort.Reverse.
+	sort.Sort(sortedParsedVersions)
 
 	var latestSnapshotVersion *version.ParsedSemVer
 	// fetch the latest SNAPSHOT build
--- testing/integration/upgrade_test.go
+++ testing/integration/upgrade_test.go
@@ -273,7 +273,6 @@ func TestStandaloneUpgradeToSpecificSnapshotBuild(t *testing.T) {
 		t.Skipf("Version %s is lower than min version %s", define.Version(), minVersion)
 	}
 
-	// prepare the agent fixture
 	agentFixture, err := define.NewFixture(t, define.Version())
 	require.NoError(t, err)
 
@@ -293,11 +292,13 @@ func TestStandaloneUpgradeToSpecificSnapshotBuild(t *testing.T) {
 	// get all the builds of the snapshot version (need to pass x.y.z-SNAPSHOT format)
 	builds, err := aac.GetBuildsForVersion(ctx, latestSnapshotVersion.VersionWithPrerelease())
 	require.NoError(t, err)
-	// TODO if we don't have at least 2 builds, select the next older snapshot build
-	require.Greater(t, len(builds.Builds), 1)
 
-	// take the penultimate build of the snapshot (the builds are ordered from most to least recent)
-	upgradeVersionString := builds.Builds[1]
+	upgradeVersionString := builds.Builds[0]
+	if len(builds.Builds) > 2 {
+		// if ther is more than 1 build, take the penultimate build of the
+		// snapshot (the builds are ordered from most to least recent)
+		upgradeVersionString = builds.Builds[1]
+	}
 
 	t.Logf("Targeting build %q of version %q", upgradeVersionString, latestSnapshotVersion)
 
