4cd83bf2d4fdc5ca0f54e8e314af73be2eff0ab5
Anderson Queiroz <anderson.queiroz@elastic.co>
2023-09-01 13:59:01 +0200

    diagnostics creates the necessary folder for -f argumen (#3340)
    - makes diagnostics command to create the necessary folders when a custom path is provided by the '-f' flag.
     - refactors the diagnosticCmd splitting it into smaller functions
     - add test  createFile
     - rename logs_test.go createFile function to createFileEmpty so its name wont collide with the newly created 'createFile' function.
    add license headaer

--- internal/pkg/agent/cmd/logs_test.go
+++ internal/pkg/agent/cmd/logs_test.go
@@ -38,10 +38,10 @@ func TestGetLogFilenames(t *testing.T) {
 	t.Run("returns the correct sorted filelist", func(t *testing.T) {
 		dir := t.TempDir()
 
-		createFile(t, dir, file2)
-		createFile(t, dir, file)
-		createFile(t, dir, file1)
-		createFile(t, dir, file3)
+		createFileEmpty(t, dir, file2)
+		createFileEmpty(t, dir, file)
+		createFileEmpty(t, dir, file1)
+		createFileEmpty(t, dir, file3)
 
 		names, err := getLogFilenames(dir)
 		require.NoError(t, err)
@@ -62,14 +62,14 @@ func TestGetLogFilenames(t *testing.T) {
 		prevDayFile2 := "elastic-agent-20230529-2.ndjson"
 		prevDayFile3 := "elastic-agent-20230529-3.ndjson"
 
-		createFile(t, dir, file2)
-		createFile(t, dir, file)
-		createFile(t, dir, prevDayFile1)
-		createFile(t, dir, file1)
-		createFile(t, dir, prevDayFile)
-		createFile(t, dir, prevDayFile2)
-		createFile(t, dir, file3)
-		createFile(t, dir, prevDayFile3)
+		createFileEmpty(t, dir, file2)
+		createFileEmpty(t, dir, file)
+		createFileEmpty(t, dir, prevDayFile1)
+		createFileEmpty(t, dir, file1)
+		createFileEmpty(t, dir, prevDayFile)
+		createFileEmpty(t, dir, prevDayFile2)
+		createFileEmpty(t, dir, file3)
+		createFileEmpty(t, dir, prevDayFile3)
 
 		names, err := getLogFilenames(dir)
 		require.NoError(t, err)
@@ -99,7 +99,7 @@ func TestGetLogFilenames(t *testing.T) {
 
 	t.Run("does not return non-log entries", func(t *testing.T) {
 		dir := t.TempDir()
-		createFile(t, dir, "excluded")
+		createFileEmpty(t, dir, "excluded")
 
 		names, err := getLogFilenames(dir)
 		require.NoError(t, err)
@@ -109,7 +109,7 @@ func TestGetLogFilenames(t *testing.T) {
 
 	t.Run("returns a list of one", func(t *testing.T) {
 		dir := t.TempDir()
-		createFile(t, dir, file1)
+		createFileEmpty(t, dir, file1)
 
 		names, err := getLogFilenames(dir)
 		require.NoError(t, err)
@@ -539,7 +539,7 @@ func generateLines(prefix string, start, end int) string {
 	return b.String()
 }
 
-func createFile(t *testing.T, dir, name string) {
+func createFileEmpty(t *testing.T, dir, name string) {
 	createFileContent(t, dir, name, nil)
 }
 
