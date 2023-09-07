d62f92a6bb0b9ae2bd8de2eb03fac6f96d69b4f5
Shaunak Kashyap <ycombinator@gmail.com>
2023-09-06 17:17:07 -0700

    Add logging for debugging

--- pkg/testing/fixture_install.go
+++ pkg/testing/fixture_install.go
@@ -85,6 +85,7 @@ func (i InstallOpts) toCmdArgs() []string {
 //   - the combined output of stdout and stderr
 //   - an error if any.
 func (f *Fixture) Install(ctx context.Context, installOpts *InstallOpts, opts ...process.CmdOption) ([]byte, error) {
+	f.t.Logf("[test %s] Inside fixture install function", f.t.Name())
 	installArgs := []string{"install"}
 	if installOpts != nil {
 		installArgs = append(installArgs, installOpts.toCmdArgs()...)
@@ -108,6 +109,7 @@ func (f *Fixture) Install(ctx context.Context, installOpts *InstallOpts, opts ..
 	f.setClient(c)
 
 	f.t.Cleanup(func() {
+		f.t.Logf("[test %s] Inside fixture cleanup function", f.t.Name())
 		if !f.installed {
 			// not installed; no need to clean up or collect diagnostics
 			return
