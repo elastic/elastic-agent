5f28319ae818f37353d9cec3c1c60e38c4fc7478
Blake Rouse <blake.rouse@elastic.co>
2023-09-13 09:24:50 -0400

    Revert "Revert "Skip TestStandaloneUpgradeFailsStatus to fix failing integration tests again (#3391)""
    This reverts commit 3b0f040460cba561d9503d716b33c241893c01db.

--- testing/integration/upgrade_test.go
+++ testing/integration/upgrade_test.go
@@ -912,6 +912,8 @@ func TestStandaloneUpgradeFailsStatus(t *testing.T) {
 		Sudo:    true, // requires Agent installation
 	})
 
+	t.Skip("Affected by https://github.com/elastic/elastic-agent/issues/3371, watcher left running at end of test")
+
 	upgradeFromVersion, err := version.ParseVersion(define.Version())
 	require.NoError(t, err)
 
