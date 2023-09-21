70f327f2b733984b84154365382c3077dc31cefd
Craig MacKenzie <craig.mackenzie@elastic.co>
2023-09-21 15:44:20 -0400

    Use previous stack version in tests until latest is ready.

--- .buildkite/scripts/steps/integration_tests.sh
+++ .buildkite/scripts/steps/integration_tests.sh
@@ -8,7 +8,8 @@ DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64 PACKAGES=
 
 # Run integration tests
 set +e
-TEST_INTEG_CLEAN_ON_EXIT=true SNAPSHOT=true mage integration:test
+# Use 8.10.2-SNAPSHOT until the first 8.10.3-SNAPSHOT is produced.
+AGENT_STACK_VERSION="8.10.2-SNAPSHOT" TEST_INTEG_CLEAN_ON_EXIT=true SNAPSHOT=true mage integration:test
 TESTS_EXIT_STATUS=$?
 set -e
 
