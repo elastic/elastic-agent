5682966e9e0ec240780c155bb7be1cec9189c0a8
Shaunak Kashyap <ycombinator@gmail.com>
2023-10-02 18:49:34 -0700

    Implementing TODO

--- internal/pkg/agent/application/upgrade/fsm.go
+++ internal/pkg/agent/application/upgrade/fsm.go
@@ -1,11 +1,16 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package upgrade
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/elastic/elastic-agent/pkg/core/logger"
 	"github.com/looplab/fsm"
+
+	"github.com/elastic/elastic-agent/pkg/core/logger"
 )
 
 const (
