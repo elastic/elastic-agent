108a01a01009c92d5c0f87b1331050cffe7cf28d
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-13 03:14:55 +0200

    Windows pre-command hook and unit tests ps

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,5 +1,6 @@
 # Install gvm and go
 $env:GvmVersion = "0.5.2"
+$Env:GOTMPDIR = $Env:BUILDKITE_BUILD_CHECKOUT_PATH https://github.com/golang/go/issues/42224#issuecomment-1021149948
 [Net.ServicePointManager]::SecurityProtocol = "tls12"
 $env:GoVersion = Get-Content -Path .go-version
 Invoke-WebRequest -URI https://github.com/andrewkroh/gvm/releases/download/v$env:GvmVersion/gvm-windows-amd64.exe -Outfile C:\Windows\System32\gvm.exe
--- .buildkite/scripts/steps/unit-tests.ps1
+++ .buildkite/scripts/steps/unit-tests.ps1
@@ -1,5 +1,10 @@
 $ErrorActionPreference = "Stop"
-
 Write-Host "--- Unit tests"
+#debug
+Write-Host "GOPATH: $env:GOPATH"
+Get-ChildItem -Path $env:GOPATH\bin
+
+# run tests
 $Env:TEST_COVERAGE = $true
-mage.exe unitTest
+& "$env:GOPATH\bin\mage.exe" unitTest
+
