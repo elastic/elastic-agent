43ebbedffac210f94bb7f0570145098c37364458
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-13 15:25:12 +0200

    set GOPATH

--- .buildkite/scripts/steps/unit-tests.ps1
+++ .buildkite/scripts/steps/unit-tests.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "--- Unit tests"
 #debug
 Write-Host "GOPATH: $env:GOPATH"
-Get-ChildItem -Path $env:GOPATH\bin
+# Get-ChildItem -Path $env:GOPATH\bin
 
 # run tests
 $Env:TEST_COVERAGE = $true
