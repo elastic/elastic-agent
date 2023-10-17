be87596eb0671ba863158021758cd62b8ec55805
Shaunak Kashyap <ycombinator@gmail.com>
2023-10-17 14:03:58 -0700

    Add nil guard

--- internal/pkg/agent/application/coordinator/coordinator.go
+++ internal/pkg/agent/application/coordinator/coordinator.go
@@ -490,6 +490,9 @@ func (c *Coordinator) Upgrade(ctx context.Context, version string, sourceURI str
 }
 
 func (c *Coordinator) logUpgradeDetails(details *details.Details) {
+	if details == nil {
+		return
+	}
 	c.logger.Infow("updated upgrade details", "upgrade_details", details)
 }
 
