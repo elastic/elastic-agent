dcaddb2b2041c9d127e5319cc5f638a5c174e040
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-17 15:46:06 +0200

    Propagate exit code on windows

--- .buildkite/scripts/steps/unit-tests.ps1
+++ .buildkite/scripts/steps/unit-tests.ps1
@@ -3,6 +3,14 @@ $ErrorActionPreference = "Stop"
 Write-Host "--- Build"
 mage build
 
+if ($LASTEXITCODE -ne 0) {  
+  exit 1 
+}
+
 Write-Host "--- Unit tests"
 $env:TEST_COVERAGE = $true
 mage unitTest
+
+if ($LASTEXITCODE -ne 0) {  
+  exit 1 
+}
