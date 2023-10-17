afa26f53401c02b93fa1f256bae38c70e7d13ac5
Shaunak Kashyap <ycombinator@gmail.com>
2023-10-16 17:48:19 -0700

    Regenerating protobuf implementation files

--- pkg/control/v2/cproto/control_v2.pb.go
+++ pkg/control/v2/cproto/control_v2.pb.go
@@ -11,12 +11,11 @@
 package cproto
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
