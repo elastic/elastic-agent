542556eb004e84de8bb2f8415b5e090fa3ad5699
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-19 15:38:28 +0200

    test

--- .buildkite/scripts/steps/merge.sh
+++ .buildkite/scripts/steps/merge.sh
@@ -3,9 +3,9 @@ set -exuo pipefail
 COV_FILE="build/TEST-go-unit.cov"
 
 go install github.com/wadey/gocovmerge@latest
-mkdir unit-tests-2204 && buildkite-agent artifact download --step unit-tests-2204 $COV_FILE unit-tests-2204
-mkdir unit-tests-2204-arm64 && buildkite-agent artifact download --step unit-tests-2204-arm64 $COV_FILE unit-tests-2204-arm64
+mkdir -p unit-tests-2204 && buildkite-agent artifact download --step unit-tests-2204 $COV_FILE unit-tests-2204
+mkdir -p unit-tests-2204-arm64 && buildkite-agent artifact download --step unit-tests-2204-arm64 $COV_FILE unit-tests-2204-arm64
 ls unit-tests-2204
 ls unit-tests-2204-arm64
-gocovmerge unit-tests-2204/$COV_FILE unit-tests-2204-arm64/$COV_FILE > build/TEST-go-unit.cov
+mkdir -p build && gocovmerge unit-tests-2204/$COV_FILE unit-tests-2204-arm64/$COV_FILE > $COV_FILE
 cat build/TEST-go-unit.cov
