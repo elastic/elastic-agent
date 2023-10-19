22583c9bf1507aaf85dcdaadb2dd4c3fc76e8ffd
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-19 14:39:17 +0200

    changed installation path

--- .buildkite/scripts/steps/merge.sh
+++ .buildkite/scripts/steps/merge.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 $COV_FILE="build/TEST-go-unit.cov"
 
 go install github.com/wadey/gocovmerge@latest
-mkdir unit-tests-2204 && coverage-unit-tests-2204buildkite-agent artifact download --step unit-tests-2204 $COV_FILE unit-tests-2204
+mkdir unit-tests-2204 && buildkite-agent artifact download --step unit-tests-2204 $COV_FILE unit-tests-2204
 mkdir unit-tests-2204-arm64 && buildkite-agent artifact download --step unit-tests-2204-arm64 $COV_FILE unit-tests-2204-arm64
 ls unit-tests-2204
 ls unit-tests-2204-arm64
