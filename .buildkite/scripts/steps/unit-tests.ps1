a6878040c7bfd5d7eeabaaf93e4440901d2d0c44
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-20 03:22:43 +0200

    Fixed artifacts copy

--- .buildkite/scripts/steps/unit-tests.ps1
+++ .buildkite/scripts/steps/unit-tests.ps1
@@ -12,9 +12,11 @@ if ($LASTEXITCODE -ne 0) {
 Write-Host "--- Unit tests"
 $env:TEST_COVERAGE = $true
 mage unitTest
+# Copy coverage file to build directory so it can be downloaded as an artifact
+cp .\build\TEST-go-unit.cov coverage.out
 
 if ($LASTEXITCODE -ne 0) {
   exit 1 
 }
-cp .\build\TEST-go-unit.cov coverage.out
+
 
