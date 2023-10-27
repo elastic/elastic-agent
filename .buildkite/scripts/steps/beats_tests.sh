8da7a0b9173d097834ee555fe605621c26bdabf6
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-27 16:18:39 -0700

    set workspace

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export WORKSPACE=build/beats/x-pack/metricbeat
+
 source .buildkite/scripts/common.sh
 
 mkdir -p build
 cd build
 
 git clone git@github.com:elastic/beats.git
+cd beats
 
-cd beats/x-pack/metricbeat
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
