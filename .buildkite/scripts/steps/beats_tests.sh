c6ce64153e89d5d7a7fa8921538f9057c15f9029
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-27 16:39:02 -0700

    trying to debug buildkite

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+set +x
 
 source .buildkite/scripts/common.sh
 
@@ -11,5 +11,5 @@ git clone git@github.com:elastic/beats.git
 
 cd beats/x-pack/metricbeat
 export WORKSPACE=build/beats/x-pack/metricbeat
-
+ls
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
