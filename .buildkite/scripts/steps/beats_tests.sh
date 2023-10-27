18523dcf32635e4a96495a4661854337d9c3e883
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-27 16:07:14 -0700

    still trying to get it to work

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -8,8 +8,5 @@ cd build
 
 git clone git@github.com:elastic/beats.git
 
-cd beats
-echo "testing mage"
-mage -l
-echo "running mage"
-SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage -v package
+cd beats/x-pack/metricbeat
+SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
