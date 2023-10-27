03d3705be0a500c09e296057094183c3121826a9
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-27 16:01:00 -0700

    still fighting with buildkite

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -9,5 +9,7 @@ cd build
 git clone git@github.com:elastic/beats.git
 
 cd beats
-
+echo "testing mage"
+mage -l
+echo "running mage"
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage -v package
