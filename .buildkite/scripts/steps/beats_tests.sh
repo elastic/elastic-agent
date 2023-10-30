04d501d66f5c6011fb1015078e90502c36d76cd6
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-30 10:41:35 -0700

    I swear I'll figure this out

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set +x
+set -x
 
 source .buildkite/scripts/common.sh
 
@@ -11,7 +11,7 @@ cd build
 
 git clone git@github.com:elastic/beats.git
 
-cd beats/x-pack/metricbeat
+#cd beats/x-pack/metricbeat
 export WORKSPACE=build/beats/x-pack/metricbeat
 ls
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
