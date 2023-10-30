bf78cc1e1498d19651857da3e69930f6db185903
Pavel Zorin <pavel.zorin@elastic.co>
2023-10-30 15:44:05 +0100

    [CI] Pull requests migration to buildkite (#3573)
    * buildkite test on Ubuntu
    
    * Fixed step ids
    
    * fix image family
    
    * Use aws from arm64
    
    * try windows post-checkout hook
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * Windows pre-command hook and unit tests ps
    
    * set GOPATH
    
    * set GOPATH
    
    * set GOPATH
    
    * set GOPATH
    
    * set GOPATH
    
    * Installed GCC
    
    * Installed GCC
    
    * Installed GCC
    
    * Windows 2016
    
    * Propagate exit code on windows
    
    * Added GOTMPDIR
    
    * Added GOTMPDIR
    
    * Merge coverage reports step
    
    * Merge coverage reports step
    
    * Merge coverage reports step
    
    * changed installation path
    
    * changed installation path
    
    * changed installation path
    
    * changed installation path
    
    * changed installation path
    
    * changed installation path
    
    * test
    
    * test
    
    * merge.sh
    
    * Added coverage.out to simplify artifacts download
    
    * Fixed artifacts copy
    
    * buildite orka integration
    
    * fix os name
    
    * Add err message to upgrade test
    
    * Fix autocrlf for windows
    
    * Craig's patch
    
    * Complete test pipeline
    
    * Complete test pipeline
    
    * Remove macos GHA workflow
    
    * Fix Processing test results step
    
    * fix k8s jenkins step
    
    * Removed m1 stage
    
    * Enabled race detector for unit tests
    
    * Enables race detector for windows

--- .buildkite/scripts/steps/unit-tests.sh
+++ .buildkite/scripts/steps/unit-tests.sh
@@ -4,4 +4,6 @@ set -euxo pipefail
 source .buildkite/scripts/common.sh
 
 echo "--- Unit tests"
-TEST_COVERAGE=true mage unitTest
+RACE_DETECTOR=true TEST_COVERAGE=true mage unitTest
+# Copy coverage file to build directory so it can be downloaded as an artifact
+cp build/TEST-go-unit.cov coverage.out
