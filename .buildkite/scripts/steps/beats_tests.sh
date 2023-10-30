478e357b354a4edcb0cf3616d378ebe6a04d7a56
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-30 12:25:44 -0700

    use proper env vars

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -16,12 +16,13 @@ git clone git@github.com:elastic/beats.git
 export WORKSPACE=beats/x-pack/metricbeat
 
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
+STACK_PROVISIONER="${1:-"serverless"}"
 
 popd
 
 export AGENT_BUILD_DIR=build/beats/x-pack/metricbeat/build/distributions
 
 set +e
-AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless
+TEST_INTEG_CLEAN_ON_EXIT=true STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless
 TESTS_EXIT_STATUS=$?
 set -e
