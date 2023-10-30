a9ed68cf59e81173e88e84aa9f1434d26dcb492a
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-30 11:55:22 -0700

    fix workspace path, add tests,remove debug code

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -3,19 +3,25 @@ set -euo pipefail
 set -x
 
 source .buildkite/scripts/common.sh
-
+#run before setup, since this will install go and mage
+# the setup scripts will do a few things that assume we care about agent, so run before we do actual setup
 mage -l
 
 mkdir -p build
-cd build
+pushd build
 
 git clone git@github.com:elastic/beats.git
 
 #cd beats/x-pack/metricbeat
-export WORKSPACE=build/beats/x-pack/metricbeat
-ls
-echo -e "go: $GOPATH"
-echo -e "path: $PATH"
-type -p go
-type -p mage
+export WORKSPACE=beats/x-pack/metricbeat
+
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
+
+popd
+
+export AGENT_BUILD_DIR=build/beats/x-pack/metricbeat/build/distributions
+
+set +e
+AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless
+TESTS_EXIT_STATUS=$?
+set -e
