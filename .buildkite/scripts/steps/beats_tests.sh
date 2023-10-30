a81c543363e30875e250bafc323a5ca6311be503
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-30 10:56:18 -0700

    potential script install issue?

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -14,4 +14,8 @@ git clone git@github.com:elastic/beats.git
 #cd beats/x-pack/metricbeat
 export WORKSPACE=build/beats/x-pack/metricbeat
 ls
+echo -e "go: $GOPATH"
+echo -e "path: $PATH"
+type -p go
+type -p mage
 SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip mage package
