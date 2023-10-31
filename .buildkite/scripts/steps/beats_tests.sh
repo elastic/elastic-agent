ff339742d8778e67887b0e36aef627c041861d58
fearful-symmetry <alexpkristiansen@icloud.com>
2023-10-31 12:35:30 -0700

    refactor, run filebeat

--- .buildkite/scripts/steps/beats_tests.sh
+++ .buildkite/scripts/steps/beats_tests.sh
@@ -2,29 +2,58 @@
 set -euo pipefail
 
 source .buildkite/scripts/common.sh
-#run before setup, since this will install go and mage
-# the setup scripts will do a few things that assume we care about agent, so run before we do actual setup
+STACK_PROVISIONER="${1:-"serverless"}"
+#run mage before setup, since this will install go and mage
+#the setup scripts will do a few things that assume we're running out of elastic-agent and will break things for beats, so run before we do actual setup
 mage -l
 
 mkdir -p build
 cd build
 
 git clone git@github.com:elastic/beats.git
-
-#cd beats/x-pack/metricbeat
-export WORKSPACE=beats/x-pack/metricbeat
-
-SNAPSHOT=true PLATFORMS=linux/amd64,windows/amd64 PACKAGES=tar.gz,zip mage package
-STACK_PROVISIONER="${1:-"serverless"}"
-
 cd ..
 
-export AGENT_BUILD_DIR=build/beats/x-pack/metricbeat/build/distributions
-export WORKSPACE=$(pwd)
+# export WORKSPACE=beats/x-pack/metricbeat
 
-set +e
-TEST_INTEG_CLEAN_ON_EXIT=true TEST_PLATFORMS="linux/amd64" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless metricbeat
-TESTS_EXIT_STATUS=$?
-set -e
+# SNAPSHOT=true PLATFORMS=linux/amd64,windows/amd64 PACKAGES=tar.gz,zip mage package
 
-exit $TESTS_EXIT_STATUS
+
+# cd ..
+
+# export AGENT_BUILD_DIR=build/beats/x-pack/metricbeat/build/distributions
+# export WORKSPACE=$(pwd)
+
+# set +e
+# TEST_INTEG_CLEAN_ON_EXIT=true TEST_PLATFORMS="linux/amd64" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless metricbeat
+# TESTS_EXIT_STATUS=$?
+# set -e
+
+# exit $TESTS_EXIT_STATUS
+
+if !run_test_for_beat metricbeat; then
+    exit $?
+fi
+
+if !run_test_for_beat filebeat; then
+    exit $?
+fi
+
+
+run_test_for_beat(){
+    local beat_name=$1
+    
+    #build
+    export WORKSPACE="build/beats/x-pack/${beat_name}"
+    SNAPSHOT=true PLATFORMS=linux/amd64 PACKAGES=tar.gz,zip mage package
+
+    #run
+    export AGENT_BUILD_DIR="build/beats/x-pack/${beat_name}/build/distributions"
+    export WORKSPACE=$(pwd)
+
+    set +e
+    TEST_INTEG_CLEAN_ON_EXIT=true TEST_PLATFORMS="linux/amd64" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:testBeatServerless metricbeat
+    TESTS_EXIT_STATUS=$?
+    set -e
+
+    return $TESTS_EXIT_STATUS
+}
