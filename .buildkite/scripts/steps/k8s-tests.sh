03e6f4ceeece263d3038b85f18e2ba3f00cf3c5f
Pavel Zorin <pavel.zorin@elastic.co>
2023-11-07 02:03:32 +0100

    Fix review comments

--- .buildkite/scripts/steps/k8s-tests.sh
+++ .buildkite/scripts/steps/k8s-tests.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 source .buildkite/scripts/install-kubectl.sh
 source .buildkite/scripts/install-kind.sh
 
-echo $PATH
-
 kind create cluster --image "kindest/node:${K8S_VERSION}" --config - <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
