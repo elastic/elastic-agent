0fd80e6ac4f956114d105526dcf7c6cf21acf902
Pavel Zorin <pavel.zorin@elastic.co>
2023-11-13 12:55:27 +0100

    Migrate check-ci step to buildkite

---
+++ .buildkite/scripts/steps/check-ci.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common.sh
+echo "--- Check CI"
+
+BEAT_VERSION=$(make get-version)
+echo "Beat version: $BEAT_VERSION"
+make check-ci
