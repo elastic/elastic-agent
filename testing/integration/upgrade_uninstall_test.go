8a8abd046a1f549557d006031223919d71314f96
Blake Rouse <blake.rouse@elastic.co>
2023-11-28 23:30:00 -0500

    Add ability to split integration tests into different groups (#3544)
    * Add ability to shard integration tests across muliple hosts.
    
    * Change to Group.
    
    * Add to dev guide.
    
    * Fix magefile.
    
    * Apply suggestions from code review
    
    Co-authored-by: Craig MacKenzie <craig.mackenzie@elastic.co>
    
    * Add define.Default constant.
    
    * Remove isolate.
    
    * Fix tests.
    
    * Update groups.
    
    * Adjust groups.
    
    * Adjust some groups, add debugging SSH key.
    
    * Only windows default group with all logs.
    
    * Remove windows specific.
    
    * Fix add_cloud_metadata allowed errors.
    
    * Another allowed error.
    
    * Yet another error.
    
    ---------
    
    Co-authored-by: Craig MacKenzie <craig.mackenzie@elastic.co>

--- testing/integration/upgrade_uninstall_test.go
+++ testing/integration/upgrade_uninstall_test.go
@@ -24,6 +24,7 @@ import (
 
 func TestStandaloneUpgradeUninstallKillWatcher(t *testing.T) {
 	define.Require(t, define.Requirements{
+		Group: Upgrade,
 		Local: false, // requires Agent installation
 		Sudo:  true,  // requires Agent installation
 	})
