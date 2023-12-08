918411a9f6ef4d4050c983b8fd8f88a6f81fe84f
Craig MacKenzie <craig.mackenzie@elastic.co>
2023-12-08 15:04:26 -0500

    Add missing serverless provisioning contexts.

--- pkg/testing/ess/serverless_test.go
+++ pkg/testing/ess/serverless_test.go
@@ -16,6 +16,9 @@ import (
 )
 
 func TestProvisionGetRegions(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*240)
+	defer cancel()
+
 	_ = logp.DevelopmentSetup()
 	key, found, err := GetESSAPIKey()
 	if !found {
@@ -29,13 +32,16 @@ func TestProvisionGetRegions(t *testing.T) {
 		cfg: cfg,
 		log: &defaultLogger{wrapped: logp.L()},
 	}
-	err = prov.CheckCloudRegion()
+	err = prov.CheckCloudRegion(ctx)
 	require.NoError(t, err)
 	require.NotEqual(t, "bad-region-ID", prov.cfg.Region)
 
 }
 
 func TestStackProvisioner(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
+	defer cancel()
+
 	_ = logp.DevelopmentSetup()
 	key, found, err := GetESSAPIKey()
 	if !found {
@@ -45,12 +51,10 @@ func TestStackProvisioner(t *testing.T) {
 	require.True(t, found)
 
 	cfg := ProvisionerConfig{Region: "aws-eu-west-1", APIKey: key}
-	provClient, err := NewServerlessProvisioner(cfg)
+	provClient, err := NewServerlessProvisioner(ctx, cfg)
 	require.NoError(t, err)
 	request := runner.StackRequest{ID: "stack-test-one", Version: "8.9.0"}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
-	defer cancel()
 	stack, err := provClient.Create(ctx, request)
 	require.NoError(t, err)
 	t.Logf("got results:")
