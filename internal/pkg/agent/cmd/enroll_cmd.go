e82ad16b122807d9d4f942f46f8aaf0831b34b30
Tiago Queiroz <tiago.queiroz@elastic.co>
2023-12-11 15:53:46 +0100

    Fix enroll command

--- internal/pkg/agent/cmd/enroll_cmd.go
+++ internal/pkg/agent/cmd/enroll_cmd.go
@@ -276,7 +276,7 @@ func (c *enrollCmd) Execute(ctx context.Context, streams *cli.IOStreams) error {
 		}
 	}()
 
-	if c.agentProc == nil && !c.options.SkipDaemonRestart {
+	if c.agentProc != nil && !c.options.SkipDaemonRestart {
 		if err = c.daemonReloadWithBackoff(ctx); err != nil {
 			c.log.Errorf("Elastic Agent might not be running; unable to trigger restart: %v", err)
 			return fmt.Errorf("could not reload agent daemon, unable to trigger restart: %w", err)
