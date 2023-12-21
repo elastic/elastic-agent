0ed905f6b76b81f1439c3b48b86ccb1369514fae
Craig MacKenzie <craig.mackenzie@elastic.co>
2023-12-21 14:52:33 -0500

    Revert "Skip APM propagation test on Windows. (#3932)"
    This reverts commit a76f4ba6debee80f6e9eb4b201ba824fe60b5566.

--- testing/integration/apm_propagation_test.go
+++ testing/integration/apm_propagation_test.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"runtime"
 	"strings"
 	"testing"
 	"text/template"
@@ -57,12 +56,6 @@ func TestAPMConfig(t *testing.T) {
 		Group: Default,
 		Stack: &define.Stack{},
 	})
-
-	if runtime.GOOS == "windows" {
-		// This test hangs indefinitely on Windows. Root cause is TBD.
-		t.Skip("Flaky test: https://github.com/elastic/ingest-dev/issues/2668")
-	}
-
 	f, err := define.NewFixture(t, define.Version())
 	require.NoError(t, err)
 
