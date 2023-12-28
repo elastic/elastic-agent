53700cef80b752a5a661f634a2aae1690c778ebc
Anderson Queiroz <anderson.queiroz@elastic.co>
2023-12-28 15:25:53 +0100

    test restoring upgrade marker watcher

--- internal/pkg/agent/application/upgrade/marker_access_test.go
+++ internal/pkg/agent/application/upgrade/marker_access_test.go
@@ -5,6 +5,9 @@
 package upgrade
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -13,9 +16,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/stretchr/testify/require"
-
-	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
 )
 
 func TestWriteMarkerFile(t *testing.T) {
@@ -43,21 +45,21 @@ func TestWriteMarkerFileWithTruncation(t *testing.T) {
 	//
 	// Watch marker file for the duration of this test, to ensure
 	// it's never empty (truncated).
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
-	// errCh := make(chan error)
-	// watchFileNotEmpty(t, ctx, testMarkerFile, errCh)
-	// var watchErr error
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-ctx.Done():
-	// 			return
-	// 		case err := <-errCh:
-	// 			watchErr = err
-	// 		}
-	// 	}
-	// }()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	errCh := make(chan error)
+	watchFileNotEmpty(t, ctx, testMarkerFile, errCh)
+	var watchErr error
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case err := <-errCh:
+				watchErr = err
+			}
+		}
+	}()
 
 	// Write a long marker file
 	t.Log("marker file path:", testMarkerFile)
@@ -97,11 +99,11 @@ func TestWriteMarkerFileWithTruncation(t *testing.T) {
 	// Make sure shorter file is smaller than the original long marker file.
 	require.Less(t, newSize, originalSize)
 
-	// // Cancel watch on marker file now that we're at the end of the test and
-	// // check that there were no errors.
-	// cancel()
-	// require.NoError(t, watchErr)
-	// close(errCh)
+	// Cancel watch on marker file now that we're at the end of the test and
+	// check that there were no errors.
+	cancel()
+	require.NoError(t, watchErr)
+	close(errCh)
 }
 
 // inOpenfiles uses Windows 'openfiles' program to try to find out which
@@ -150,59 +152,59 @@ func inOpenfiles(t *testing.T, file string) bool {
 	return found
 }
 
-// func watchFileNotEmpty(t *testing.T, ctx context.Context, filePath string, errCh chan error) {
-// 	watcher, err := fsnotify.NewWatcher()
-// 	require.NoError(t, err)
-//
-// 	dirPath := filepath.Dir(filePath)
-// 	err = watcher.Add(dirPath)
-// 	require.NoError(t, err)
-//
-// 	// Watch file
-// 	go func() {
-// 		defer watcher.Close()
-// 		for {
-// 			select {
-// 			case <-ctx.Done():
-// 				return
-// 			case err, ok := <-watcher.Errors:
-// 				if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-// 					errCh <- errors.New("fsnotify.Watcher's error channel was closed")
-// 					return
-// 				}
-//
-// 				errCh <- fmt.Errorf("upgrade marker watch returned error: %w", err)
-// 				continue
-// 			case e, ok := <-watcher.Events:
-// 				if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-// 					errCh <- errors.New("fsnotify.Watcher's events channel was closed")
-// 					return
-// 				}
-//
-// 				if e.Name != filePath {
-// 					// Since we are watching the directory that will contain the file, we
-// 					// could receive events here for changes to files other than the file we're
-// 					// interested in. We ignore such events.
-// 					continue
-// 				}
-//
-// 				switch {
-// 				case e.Op&(fsnotify.Create|fsnotify.Write) != 0:
-// 					// File was created or updated; read its length
-// 					// and send error if it's zero
-// 					fileInfo, err := os.Stat(filePath)
-// 					if err != nil {
-// 						errCh <- fmt.Errorf("failed to stat file [%s]: %w", filePath, err)
-// 					}
-//
-// 					if fileInfo.Size() == 0 {
-// 						errCh <- fmt.Errorf("file [%s] has size 0", filePath)
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}()
-// }
+func watchFileNotEmpty(t *testing.T, ctx context.Context, filePath string, errCh chan error) {
+	watcher, err := fsnotify.NewWatcher()
+	require.NoError(t, err)
+
+	dirPath := filepath.Dir(filePath)
+	err = watcher.Add(dirPath)
+	require.NoError(t, err)
+
+	// Watch file
+	go func() {
+		defer watcher.Close()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case err, ok := <-watcher.Errors:
+				if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+					errCh <- errors.New("fsnotify.Watcher's error channel was closed")
+					return
+				}
+
+				errCh <- fmt.Errorf("upgrade marker watch returned error: %w", err)
+				continue
+			case e, ok := <-watcher.Events:
+				if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+					errCh <- errors.New("fsnotify.Watcher's events channel was closed")
+					return
+				}
+
+				if e.Name != filePath {
+					// Since we are watching the directory that will contain the file, we
+					// could receive events here for changes to files other than the file we're
+					// interested in. We ignore such events.
+					continue
+				}
+
+				switch {
+				case e.Op&(fsnotify.Create|fsnotify.Write) != 0:
+					// File was created or updated; read its length
+					// and send error if it's zero
+					fileInfo, err := os.Stat(filePath)
+					if err != nil {
+						errCh <- fmt.Errorf("failed to stat file [%s]: %w", filePath, err)
+					}
+
+					if fileInfo.Size() == 0 {
+						errCh <- fmt.Errorf("file [%s] has size 0", filePath)
+					}
+				}
+			}
+		}
+	}()
+}
 
 func randomBytes(length int) []byte {
 	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
