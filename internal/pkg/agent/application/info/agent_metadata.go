d7538fe249d3f7d4be0ee73d202c143e8a6cd3b0
Alex Kristiansen <alex.kristiansen@elastic.co>
2024-02-05 13:15:40 -0800

    Revert "remove calls to InstallMarkerExists"
    This reverts commit 400bb0d5cf435d042f328871e340b6957a3dec0d.

--- internal/pkg/agent/application/info/agent_metadata.go
+++ internal/pkg/agent/application/info/agent_metadata.go
@@ -11,6 +11,7 @@ import (
 	"runtime"
 	"strings"
 
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
 	"github.com/elastic/elastic-agent/internal/pkg/release"
 	"github.com/elastic/elastic-agent/pkg/core/logger"
@@ -168,7 +169,7 @@ func (i *AgentInfo) ECSMetadata(l *logger.Logger) (*ECSMeta, error) {
 				BuildOriginal: release.Info().String(),
 				// only upgradeable if running from Agent installer and running under the
 				// control of the system supervisor (or built specifically with upgrading enabled)
-				Upgradeable: release.Upgradeable() || RunningUnderSupervisor(),
+				Upgradeable: release.Upgradeable() || (paths.InstallMarkerExists() && RunningUnderSupervisor()),
 				LogLevel:    i.LogLevel(),
 			},
 		},
