d0048f9a91b2f2dbbbc68e4d69dda41149db8af4
fearful-symmetry <alexpkristiansen@icloud.com>
2024-02-09 12:03:35 -0800

    refactor install to make adding tests easier

--- internal/pkg/agent/install/install_test.go
+++ internal/pkg/agent/install/install_test.go
@@ -5,12 +5,53 @@
 package install
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
+	"github.com/elastic/elastic-agent/internal/pkg/cli"
+	"github.com/elastic/elastic-agent/pkg/utils"
 	"github.com/jaypipes/ghw"
 	"github.com/jaypipes/ghw/pkg/block"
+	"github.com/schollz/progressbar/v3"
 	"github.com/stretchr/testify/require"
 )
+
+func TestPreInstall(t *testing.T) {
+	pt := progressbar.DefaultSilent(0)
+	topPath := t.TempDir()
+
+	//fakeout path verification in setupInstall()
+	exe, err := os.Executable()
+	require.NoError(t, err)
+	t.Logf("got: %s", exe)
+
+	base := filepath.Dir(exe)
+
+	_, err = os.Create(filepath.Join(base, "elastic-agent"))
+	require.NoError(t, err)
+
+	sourceDir, err := setupInstall(pt, topPath, "", logp.L())
+	require.NoError(t, err)
+	require.Equal(t, base, sourceDir)
+}
+
+func TestFileCopy(t *testing.T) {
+	inputDir := t.TempDir()
+	outputDir := t.TempDir()
+	// create some example files
+	_, err := os.Create(filepath.Join(inputDir, "elastic-agent"))
+	require.NoError(t, err)
+	pt := progressbar.DefaultSilent(0)
+
+	err = copyFiles(pt, outputDir, cli.NewIOStreams(), inputDir, utils.CurrentFileOwner())
+	require.NoError(t, err)
+
+	require.FileExists(t, filepath.Join(outputDir, "elastic-agent"))
+	require.FileExists(t, filepath.Join(outputDir, paths.MarkerFileName))
+}
 
 func TestHasAllSSDs(t *testing.T) {
 	cases := map[string]struct {
