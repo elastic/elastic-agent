2148751a81229ba17c9b7bbfa3094fddfb128fec
Andreas Gkizas <andreas.gkizas@elastic.co>
2024-03-05 17:56:51 +0200

    updating code to postition in correct place and to be triggered only in co.elastic.hints annotations

--- internal/pkg/composable/providers/kubernetes/hints.go
+++ internal/pkg/composable/providers/kubernetes/hints.go
@@ -30,7 +30,7 @@ const (
 	processors  = "processors"
 )
 
-var allSupportedHints = []string{integration, datastreams, host, period, username, password, stream, processors}
+var allSupportedHints = []string{"enabled", integration, datastreams, host, period, timeout, metricspath, username, password, stream, processors}
 
 type hintsBuilder struct {
 	Key string
