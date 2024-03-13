9856b6e21b42ba319ef54c6340d477f9e2832764
Doug Weyrauch <douglas.weyrauch@elastic.co>
2024-03-13 13:11:08 -0400

    version with prerelease

--- dev-tools/mage/manifest/manifest.go
+++ dev-tools/mage/manifest/manifest.go
@@ -106,7 +106,9 @@ func DownloadComponentsFromManifest(manifest string, platforms []string, platfor
 	}
 
 	// For resolving manifest package name and version, just use the Major.Minor.Patch part of the version
-	majorMinorPatchVersion := fmt.Sprintf("%d.%d.%d", parsedManifestVersion.Major(), parsedManifestVersion.Minor(), parsedManifestVersion.Patch())
+	majorMinorPatchVersion := parsedManifestVersion.VersionWithPrerelease()
+
+	//fmt.Sprintf("%d.%d.%d", parsedManifestVersion.Major(), parsedManifestVersion.Minor(), parsedManifestVersion.Patch())
 
 	errGrp, downloadsCtx := errgroup.WithContext(context.Background())
 	for component, pkgs := range componentSpec {
