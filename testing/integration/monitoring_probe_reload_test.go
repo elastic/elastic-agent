0e53358c3cedeba18f9ce48dff208463e446d6ae
fearful-symmetry <alexpkristiansen@icloud.com>
2024-04-05 13:20:46 -0700

    format

--- testing/integration/monitoring_probe_reload_test.go
+++ testing/integration/monitoring_probe_reload_test.go
@@ -12,14 +12,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/elastic/elastic-agent-libs/kibana"
 	"github.com/elastic/elastic-agent/pkg/control/v2/cproto"
 	atesting "github.com/elastic/elastic-agent/pkg/testing"
 	"github.com/elastic/elastic-agent/pkg/testing/define"
 	"github.com/elastic/elastic-agent/pkg/testing/tools"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
 type MonitoringRunner struct {
