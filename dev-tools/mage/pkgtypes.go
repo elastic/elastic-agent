b9179658f29e19c519dee92ed5a6930bd456b2fb
Paolo Chila <paolo.chila@elastic.co>
2024-04-16 19:12:36 +0200

    Add exclusions when adding files to tar

--- dev-tools/mage/pkgtypes.go
+++ dev-tools/mage/pkgtypes.go
@@ -17,6 +17,7 @@ import (
 	"reflect"
 	"regexp"
 	"runtime"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -904,13 +905,22 @@ func addFileToZip(ar *zip.Writer, baseDir string, pkgFile PackageFile) error {
 
 // addFileToTar adds a file (or directory) to a tar archive.
 func addFileToTar(ar *tar.Writer, baseDir string, pkgFile PackageFile) error {
+	excludedFiles := []string{
+		"cloud-defend",
+		"cloud-defend.spec.yml",
+	}
+
 	return filepath.Walk(pkgFile.Source, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			if pkgFile.SkipOnMissing && os.IsNotExist(err) {
 				return nil
 			}
-
 			return err
+		}
+
+		if slices.Contains(excludedFiles, info.Name()) {
+			// it's a file we have to exclude
+			return nil
 		}
 
 		header, err := tar.FileInfoHeader(info, info.Name())
