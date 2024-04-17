4be14618838ed18412994b8945ec11bee99ef221
Denis <denis.rechkunov@elastic.co>
2024-04-17 21:09:46 +0200

    Delete resources created by OGC more than 5 hours ago (#4581)
    Previously it was set to 24 hours but since our build load hits some
    resource limits, the configuration should be more aggressive.

--- .buildkite/scripts/steps/gce-cleanup.sh
+++ .buildkite/scripts/steps/gce-cleanup.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 export ACCOUNT_KEY_SECRET=$(vault kv get -field=client_email $VAULT_PATH)
 export ACCOUNT_SECRET=$(vault kv get -field=private_key $VAULT_PATH)
 export ACCOUNT_PROJECT_SECRET=$(vault kv get -field=project_id $VAULT_PATH)
-export CREATION_DATE=$(date -Is -d "24 hours ago")
+export DELETE_CREATED_AFTER_DATE=$(date -Is -d "5 hours ago")
 
 docker run -v $(pwd)/.buildkite/misc/gce-cleanup.yml:/etc/cloud-reaper/config.yml \
   -e ACCOUNT_SECRET="$ACCOUNT_SECRET" \
   -e ACCOUNT_KEY="$ACCOUNT_KEY_SECRET" \
   -e ACCOUNT_PROJECT=$ACCOUNT_PROJECT_SECRET \
-  -e CREATION_DATE=$CREATION_DATE \
+  -e DELETE_CREATED_AFTER_DATE=$DELETE_CREATED_AFTER_DATE \
   ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper --config /etc/cloud-reaper/config.yml destroy --confirm
