017c4eff7a1b8798169f1c3030f09cecfe954c7d
Paolo Chila <paolo.chila@elastic.co>
2024-04-23 10:47:14 +0200

    make coordinator implement logLevelSetter interface

--- internal/pkg/agent/application/actions/handlers/handler_action_settings.go
+++ internal/pkg/agent/application/actions/handlers/handler_action_settings.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/elastic/elastic-agent-libs/logp"
 
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/coordinator"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
 	"github.com/elastic/elastic-agent/internal/pkg/fleetapi"
 	"github.com/elastic/elastic-agent/internal/pkg/fleetapi/acker"
@@ -19,21 +18,21 @@ import (
 
 // Settings handles settings change coming from fleet and updates log level.
 type Settings struct {
-	log       *logger.Logger
-	agentInfo info.Agent
-	coord     *coordinator.Coordinator
+	log            *logger.Logger
+	agentInfo      info.Agent
+	logLevelSetter logLevelSetter
 }
 
 // NewSettings creates a new Settings handler.
 func NewSettings(
 	log *logger.Logger,
 	agentInfo info.Agent,
-	coord *coordinator.Coordinator,
+	logLevelSetter logLevelSetter,
 ) *Settings {
 	return &Settings{
-		log:       log,
-		agentInfo: agentInfo,
-		coord:     coord,
+		log:            log,
+		agentInfo:      agentInfo,
+		logLevelSetter: logLevelSetter,
 	}
 }
 
@@ -66,7 +65,7 @@ func (h *Settings) Handle(ctx context.Context, a fleetapi.Action, acker acker.Ac
 	}
 
 	h.log.Infof("Settings action done, setting agent log level to %s", lvl.String())
-	return h.coord.SetLogLevel(ctx, lvl)
+	return h.logLevelSetter.SetLogLevel(ctx, &lvl)
 }
 
 func isSupportedLogLevel(level string) bool {
