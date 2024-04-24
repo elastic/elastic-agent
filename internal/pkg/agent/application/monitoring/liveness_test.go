6cdb8af0329c0ee046af38ceb64ee4ea640025e3
Alex K <8418476+fearful-symmetry@users.noreply.github.com>
2024-04-24 12:53:10 -0700

    Fix liveness reload config handling (#4586)
    * Reapply "Add `/liveness` endpoint to elastic-agent (#4499)" (#4583)
    
    This reverts commit eca5bc714b9a5569e733644641deabbf4b949e3b.
    
    * add behavior to not disable http monitor on reload with nil config, add tests
    
    * improve comments
    
    * linter
    
    * more linter...
    
    * fix spelling
    
    * check original config state when reloading config
    
    * change behavior of config set from overrides
    
    * fix tests
    
    * add second test to make sure old behavior with hard-coded monitoring config still works
    
    * rename method

---
+++ internal/pkg/agent/application/monitoring/liveness_test.go
@@ -0,0 +1,373 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package monitoring
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent-client/v7/pkg/client"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/coordinator"
+	"github.com/elastic/elastic-agent/pkg/component"
+	"github.com/elastic/elastic-agent/pkg/component/runtime"
+)
+
+type mockCoordinator struct {
+	state coordinator.State
+	isUp  bool
+}
+
+func (mc mockCoordinator) State() coordinator.State {
+	return mc.state
+}
+
+func (mc mockCoordinator) IsActive(_ time.Duration) bool {
+	return mc.isUp
+}
+
+func TestProcessHTTPHandler(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	testCases := []struct {
+		name         string
+		coord        mockCoordinator
+		expectedCode int
+		liveness     bool
+		failon       string
+	}{
+		{
+			name: "default-failed",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateFailed},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     true,
+			failon:       "heartbeat",
+		},
+		{
+			name: "default-healthy",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     true,
+			failon:       "heartbeat",
+		},
+		{
+			name: "degraded",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateDegraded},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 500,
+			liveness:     true,
+			failon:       "degraded",
+		},
+		{
+			name: "degraded-check-off",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateDegraded},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     true,
+			failon:       "failed",
+		},
+		{
+			name: "degraded-liveness-off",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateDegraded},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     false,
+			failon:       "degraded",
+		},
+		{
+			name: "healthy",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     true,
+			failon:       "degraded",
+		},
+		{
+			name: "coord-fail-only-healthy",
+			coord: mockCoordinator{
+				isUp: false,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 503,
+			liveness:     true,
+			failon:       "heartbeat",
+		},
+		{
+			name: "coord-fail-only-failed",
+			coord: mockCoordinator{
+				isUp: false,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateFailed},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 503,
+			liveness:     true,
+			failon:       "heartbeat",
+		},
+		{
+			name: "degraded-coordinator-down",
+			coord: mockCoordinator{
+				isUp: false,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateDegraded},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 503,
+			liveness:     true,
+			failon:       "degraded",
+		},
+		{
+			name: "unhealthy-coordinator-down",
+			coord: mockCoordinator{
+				isUp: false,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateFailed},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 503,
+			liveness:     true,
+			failon:       "degraded",
+		},
+		{
+			name: "healthy-coordinator-down",
+			coord: mockCoordinator{
+				isUp: false,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 503,
+			liveness:     true,
+			failon:       "degraded",
+		},
+		{
+			name: "healthy-liveness-off",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "5",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component3",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 200,
+			liveness:     false,
+			failon:       "degraded",
+		},
+		{
+			name: "degraded-and-healthy",
+			coord: mockCoordinator{
+				isUp: true,
+				state: coordinator.State{
+					Components: []runtime.ComponentComponentState{
+						{
+							LegacyPID: "2",
+							State:     runtime.ComponentState{State: client.UnitStateDegraded},
+							Component: component.Component{
+								ID: "test-component",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+						{
+							LegacyPID: "3",
+							State:     runtime.ComponentState{State: client.UnitStateHealthy},
+							Component: component.Component{
+								ID: "test-component2",
+								InputSpec: &component.InputRuntimeSpec{
+									BinaryName: "testbeat",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCode: 500,
+			liveness:     true,
+			failon:       "degraded",
+		},
+	}
+
+	// test with processesHandler
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			testSrv := httptest.NewServer(createHandler(livenessHandler(test.coord)))
+			defer testSrv.Close()
+
+			path := fmt.Sprintf("%s?failon=%s", testSrv.URL, test.failon)
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, path, nil)
+			require.NoError(t, err)
+			res, err := http.DefaultClient.Do(req)
+			require.NoError(t, err)
+			res.Body.Close()
+
+		})
+	}
+
+}
--- internal/pkg/agent/application/monitoring/process.go
+++ internal/pkg/agent/application/monitoring/process.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/coordinator"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
 	"github.com/elastic/elastic-agent/pkg/utils"
@@ -44,7 +43,7 @@ var redirectableProcesses = []string{
 	profilingServicePrefix,
 }
 
-func processHandler(coord *coordinator.Coordinator, statsHandler func(http.ResponseWriter, *http.Request) error, operatingSystem string) func(http.ResponseWriter, *http.Request) error {
+func processHandler(coord CoordinatorState, statsHandler func(http.ResponseWriter, *http.Request) error, operatingSystem string) func(http.ResponseWriter, *http.Request) error {
 	return func(w http.ResponseWriter, r *http.Request) error {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
@@ -82,8 +81,9 @@ func processHandler(coord *coordinator.Coordinator, statsHandler func(http.Respo
 
 		state := coord.State()
 
-		for _, c := range state.Components {
-			if matchesCloudProcessID(&c.Component, componentID) {
+		for iter, c := range state.Components {
+			// access the components array manually to avoid a memory aliasing error. This is fixed in go 1.22
+			if matchesCloudProcessID(&state.Components[iter].Component, componentID) {
 				data := struct {
 					State   string `json:"state"`
 					Message string `json:"message"`
