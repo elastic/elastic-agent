48c39500e0e8b63e65f4885cde699422eb221a6c
Shaunak Kashyap <ycombinator@gmail.com>
2024-05-02 12:48:47 -0700

    Add hostmetricsreceiver

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -12,6 +12,7 @@ import (
 
 	// Receivers:
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
 	otlpreceiver "go.opentelemetry.io/collector/receiver/otlpreceiver"
 
 	// Processors:
@@ -38,6 +39,7 @@ func components() (otelcol.Factories, error) {
 	factories.Receivers, err = receiver.MakeFactoryMap(
 		otlpreceiver.NewFactory(),
 		filelogreceiver.NewFactory(),
+		hostmetricsreceiver.NewFactory(),
 	)
 	if err != nil {
 		return otelcol.Factories{}, err
