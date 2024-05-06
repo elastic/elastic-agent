351b32ff19ff598fde328f38c5e15a9bfd46d4f7
Blake Rouse <blake.rouse@elastic.co>
2024-05-06 11:30:43 -0400

    Fix kill watcher process on Windows in unprivileged mode.

--- internal/pkg/agent/install/uninstall_unix.go
+++ internal/pkg/agent/install/uninstall_unix.go
@@ -6,6 +6,8 @@
 
 package install
 
+import "os"
+
 func isBlockingOnExe(_ error) bool {
 	return false
 }
@@ -16,4 +18,12 @@ func removeBlockingExe(_ error) error {
 
 func isRetryableError(_ error) bool {
 	return false
+}
+
+// killNoneChildProcess provides a way of killing a process that is not started as a child of this process.
+//
+// On Windows when running in unprivileged mode the internal way that golang uses DuplicateHandle to perform the kill
+// only works when the process is a child of this process.
+func killNoneChildProcess(proc *os.Process) error {
+	return proc.Kill()
 }
