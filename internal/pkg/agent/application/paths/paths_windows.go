3b0d997516e64cb7798cb6b9b67a1843097508d7
Craig MacKenzie <craig.mackenzie@elastic.co>
2024-05-31 14:49:15 -0400

    Use lowercase for consistency.

--- internal/pkg/agent/application/paths/paths_windows.go
+++ internal/pkg/agent/application/paths/paths_windows.go
@@ -26,7 +26,7 @@ const (
 
 	// serviceName is the service name when installed.
 	serviceName                = "Elastic Agent"
-	serviceNameDevelopmentMode = "Elastic Agent (Development)"
+	serviceNameDevelopmentMode = "Elastic Agent (development)"
 
 	// shellWrapperPath is the path to the installed shell wrapper.
 	shellWrapperPath                = ""
