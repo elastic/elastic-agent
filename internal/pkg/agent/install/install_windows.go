414ae849ebd5d67b9f6e369753607083a6c013f9
Craig MacKenzie <craig.mackenzie@elastic.co>
2024-06-13 17:11:07 -0400

    Fix ServiceName uses after merge.

--- internal/pkg/agent/install/install_windows.go
+++ internal/pkg/agent/install/install_windows.go
@@ -83,7 +83,7 @@ func withServiceOptions(username string, groupName string) ([]serviceOpt, error)
 // ReExec is not possible on Windows.
 func serviceConfigure(ownership utils.FileOwner) error {
 	// Modify registry to allow logging to eventlog as "Elastic Agent".
-	err := eventlog.InstallAsEventCreate(paths.ServiceName, eventlog.Info|eventlog.Warning|eventlog.Error)
+	err := eventlog.InstallAsEventCreate(paths.ServiceName(), eventlog.Info|eventlog.Warning|eventlog.Error)
 	if err != nil && !strings.Contains(err.Error(), "registry key already exists") {
 		return fmt.Errorf("unable to create registry key for logging: %w", err)
 	}
