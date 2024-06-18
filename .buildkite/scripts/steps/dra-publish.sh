2f05b529d5ddc02a6f51cf2c99247f1fd3bba7d5
Doug Weyrauch <douglas.weyrauch@elastic.co>
2024-06-18 12:32:26 -0400

    temp test value

--- .buildkite/scripts/steps/dra-publish.sh
+++ .buildkite/scripts/steps/dra-publish.sh
@@ -8,6 +8,9 @@ COMMIT="${DRA_COMMIT:="${BUILDKITE_COMMIT:=""}"}"
 BRANCH="${DRA_BRANCH:="${BUILDKITE_BRANCH:=""}"}"
 PACKAGE_VERSION="${DRA_VERSION:="${BEAT_VERSION:=""}"}"
 
+# XXX: DO NOT MERGE
+BRANCH="8.14"
+
 # force main branch on PR's or it won't execute
 # because the PR branch does not have a project folder in release-manager
 if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" ]]; then
