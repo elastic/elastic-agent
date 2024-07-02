2f5d8c5c62d12b697f3b1dc611b381f1237b2bf6
Blake Rouse <blake.rouse@elastic.co>
2024-07-02 11:11:32 -0400

    Add ability to escape variables.

--- internal/pkg/eql/parser/eql_base_visitor.go
+++ internal/pkg/eql/parser/eql_base_visitor.go
@@ -1,12 +1,8 @@
-// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
-
-// Code generated from Eql.g4 by ANTLR 4.12.0. DO NOT EDIT.
+// Code generated from Eql.g4 by ANTLR 4.13.1. DO NOT EDIT.
 
 package parser // Eql
 
-import "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+import "github.com/antlr4-go/antlr/v4"
 
 type BaseEqlVisitor struct {
 	*antlr.BaseParseTreeVisitor
@@ -33,6 +29,10 @@ func (v *BaseEqlVisitor) VisitVariableExp(ctx *VariableExpContext) interface{} {
 }
 
 func (v *BaseEqlVisitor) VisitExpArithmeticNEQ(ctx *ExpArithmeticNEQContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseEqlVisitor) VisitExpEVariable(ctx *ExpEVariableContext) interface{} {
 	return v.VisitChildren(ctx)
 }
 
