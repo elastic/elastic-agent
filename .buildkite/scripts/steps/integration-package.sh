a72f93c796c94799a7f664d56a6397688df66549
Pavel Zorin <pavel.zorin@elastic.co>
2024-07-11 00:56:23 +0200

    Inlined paclage variables

--- .buildkite/scripts/steps/integration-package.sh
+++ .buildkite/scripts/steps/integration-package.sh
@@ -3,11 +3,4 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
-AGENT_PACKAGE_VERSION="$(cat .package-version)"
-DEV=true 
-EXTERNAL=true 
-SNAPSHOT=true 
-PLATFORMS=linux/amd64,linux/arm64,windows/amd64 
-PACKAGES=tar.gz,zip,rpm,deb 
-
-mage package
+AGENT_PACKAGE_VERSION="$(cat .package-version)" PACKAGES=tar.gz,zip,rpm,deb PLATFORMS=linux/amd64,linux/arm64,windows/amd64  SNAPSHOT=true EXTERNAL=true  DEV=true mage package
