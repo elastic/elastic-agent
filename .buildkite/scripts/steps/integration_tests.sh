510bae55d69187090202832ea030b1ff7dc41e0d
Paolo Chila <paolo.chila@elastic.co>
2024-07-11 19:25:11 +0200

    Remove Agent override version in integration tests
    We used to override the agent version to a previous version between the
    version bump for a new release and the availability of the first
    snapshot build (usually a period of a few days) because Fleet had a
    limitation about not accepting agent versions more recent than itself.
    
    This limitation should have been removed by now so we can get rid of the
    agent version override and use the default agent version

--- .buildkite/scripts/steps/integration_tests.sh
+++ .buildkite/scripts/steps/integration_tests.sh
@@ -8,22 +8,13 @@ MAGE_TARGET="${2:-"integration:test"}"
 MAGE_SUBTARGET="${3:-""}"
 
 
-# Override the agent package version using a string with format <major>.<minor>.<patch>
-# There is a time when the snapshot is not built yet, so we cannot use the latest version automatically
-# This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
-OVERRIDE_AGENT_PACKAGE_VERSION="$(cat .package-version)"
 
-if [[ -n "$OVERRIDE_AGENT_PACKAGE_VERSION" ]]; then
-  OVERRIDE_TEST_AGENT_VERSION=${OVERRIDE_AGENT_PACKAGE_VERSION}"-SNAPSHOT"
-else
- OVERRIDE_TEST_AGENT_VERSION=""
-fi
 # PACKAGE
-AGENT_PACKAGE_VERSION="${OVERRIDE_AGENT_PACKAGE_VERSION}" DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip,rpm,deb mage package
+DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip,rpm,deb mage package
 
 # Run integration tests
 set +e
-AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true  STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
+TEST_INTEG_CLEAN_ON_EXIT=true  STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
 TESTS_EXIT_STATUS=$?
 set -e
 
