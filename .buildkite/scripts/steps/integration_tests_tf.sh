4901befc50098bc44fc4217994a8031dcc16566b
Pavel Zorin <pavel.zorin@elastic.co>
2024-07-16 14:20:45 +0200

    Test run

--- .buildkite/scripts/steps/integration_tests_tf.sh
+++ .buildkite/scripts/steps/integration_tests_tf.sh
@@ -15,4 +15,4 @@ trap 'ess_down' EXIT
 
 # Run integration tests
 AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}"
-go test -tags integration
+TEST_DEFINE_PREFIX=aaaaaa go test -tags integration github.com/elastic/elastic-agent/testing/integration
