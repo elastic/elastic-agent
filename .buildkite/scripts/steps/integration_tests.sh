4abd7b27d3032435d4b5e7d4477465a172600ab1
Pavel Zorin <pavel.zorin@elastic.co>
2024-07-18 15:18:39 +0200

    Merged

--- .buildkite/scripts/steps/integration_tests.sh
+++ .buildkite/scripts/steps/integration_tests.sh
@@ -7,16 +7,7 @@ STACK_PROVISIONER="${1:-"stateful"}"
 MAGE_TARGET="${2:-"integration:test"}"
 MAGE_SUBTARGET="${3:-""}"
 
-# There is a time when the snapshot is not built yet, so we cannot use the latest version automatically
-# This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
-OVERRIDE_AGENT_PACKAGE_VERSION="$(cat .package-version)"
 
-<<<<<<< HEAD
-if [[ -n "$OVERRIDE_AGENT_PACKAGE_VERSION" ]]; then
-  OVERRIDE_TEST_AGENT_VERSION=${OVERRIDE_AGENT_PACKAGE_VERSION}"-SNAPSHOT"
-else
- OVERRIDE_TEST_AGENT_VERSION=""
-=======
 # Override the stack version from `.package-version` contents
 # There is a time when the current snapshot is not available on cloud yet, so we cannot use the latest version automatically
 # This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
@@ -24,14 +15,11 @@ else
 STACK_VERSION="$(cat .package-version)"
 if [[ -n "$STACK_VERSION" ]]; then
     STACK_VERSION=${STACK_VERSION}"-SNAPSHOT"
->>>>>>> 9c49b37408 ([CI] Separated packaging step (#5129))
 fi
-# PACKAGE
-AGENT_PACKAGE_VERSION="${OVERRIDE_AGENT_PACKAGE_VERSION}" DEV=true EXTERNAL=true SNAPSHOT=true PLATFORMS=linux/amd64,linux/arm64,windows/amd64 PACKAGES=tar.gz,zip,rpm,deb mage package
 
 # Run integration tests
 set +e
-AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true  STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
+AGENT_STACK_VERSION="${STACK_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true  STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
 TESTS_EXIT_STATUS=$?
 set -e
 
