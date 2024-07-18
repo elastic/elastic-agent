c8cf1b5ec9a913ccffb7a7d15de90f7dbfeac995
Pavel Zorin <pavel.zorin@elastic.co>
2024-07-18 11:42:45 +0200

    Debug

--- .buildkite/scripts/steps/integration_tests_tf.sh
+++ .buildkite/scripts/steps/integration_tests_tf.sh
@@ -6,22 +6,28 @@ source .buildkite/scripts/common2.sh
 install_go
 install_mage
 
-source .buildkite/scripts/steps/ess.sh
+# source .buildkite/scripts/steps/ess.sh
 
 # Override the agent package version using a string with format <major>.<minor>.<patch>
 # There is a time when the snapshot is not built yet, so we cannot use the latest version automatically
 # This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
-OVERRIDE_AGENT_PACKAGE_VERSION="$(cat .package-version)"
-OVERRIDE_TEST_AGENT_VERSION=${OVERRIDE_AGENT_PACKAGE_VERSION}"-SNAPSHOT"
+# OVERRIDE_AGENT_PACKAGE_VERSION="$(cat .package-version)"
+# OVERRIDE_TEST_AGENT_VERSION=${OVERRIDE_AGENT_PACKAGE_VERSION}"-SNAPSHOT"
 
 # TODO fix
-go version
-mage build:testBinaries
+which go
+go env
+sudo --preserve-env go env 
 
-ess_up $OVERRIDE_TEST_AGENT_VERSION || echo "Failed to start ESS stack" >&2
-trap 'ess_down' EXIT
+echo "PATH: $PATH"
+sudo --preserve-env echo "sudo PATH: $PATH"
 
-# Run integration tests
-echo "~~~ Running integration tests"
-# AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}"
-AGENT_VERSION="8.16.0-SNAPSHOT" SNAPSHOT=true TEST_DEFINE_PREFIX=non_sudo_linux go test -tags integration github.com/elastic/elastic-agent/testing/integration
+# mage build:testBinaries
+
+# ess_up $OVERRIDE_TEST_AGENT_VERSION || echo "Failed to start ESS stack" >&2
+# trap 'ess_down' EXIT
+
+# # Run integration tests
+# echo "~~~ Running integration tests"
+# # AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}"
+# AGENT_VERSION="8.16.0-SNAPSHOT" SNAPSHOT=true TEST_DEFINE_PREFIX=non_sudo_linux go test -tags integration github.com/elastic/elastic-agent/testing/integration
