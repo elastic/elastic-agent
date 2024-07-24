d6ca5372a210ae56992d7aad9cbed4f9276a7e6d
Vihas Makwana <vihas.makwana@elastic.co>
2024-07-24 23:26:33 +0530

    fix: update go.mod and test cases

--- pkg/testing/fixture_install.go
+++ pkg/testing/fixture_install.go
@@ -379,7 +379,7 @@ func getProcesses(t *gotesting.T, regex string) []runningProcess {
 	}
 
 	_, pids, err := procStats.FetchPids()
-	if !assert.NoError(t, err, "error fetching process information") {
+	if !errors.Is(err, agentsystemprocess.NonFatalErr{}) && !assert.NoError(t, err, "error fetching process information") {
 		// we failed a bit further
 		return nil
 	}
