32e7f4154861804ab4b30a1043a5368be4e25187
Pavel Zorin <pavel.zorin@elastic.co>
2024-07-25 14:33:52 +0200

    Test new custom VM image

--- .buildkite/scripts/steps/integration_tests_tf.sh
+++ .buildkite/scripts/steps/integration_tests_tf.sh
@@ -3,10 +3,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common2.sh
 
-echo "~~~ Installing Go and mage"
-install_go
-install_mage
-
 source .buildkite/scripts/steps/ess.sh
 
 # Override the agent package version using a string with format <major>.<minor>.<patch>
@@ -26,9 +22,5 @@ trap 'ess_down' EXIT
 # # AGENT_VERSION="${OVERRIDE_TEST_AGENT_VERSION}"
 # AGENT_VERSION="8.16.0-SNAPSHOT" SNAPSHOT=true TEST_DEFINE_PREFIX=non_sudo_linux go test -tags integration github.com/elastic/elastic-agent/testing/integration
 
-echo "~~~ Adding buildkite-agent to adm group"
-sudo usermod -aG adm buildkite-agent
-sudo newgrp adm <<EOF
 echo "~~~ Running integration tests"
 AGENT_VERSION="8.16.0-SNAPSHOT" SNAPSHOT=true TEST_DEFINE_PREFIX=non_sudo_linux go test -tags integration github.com/elastic/elastic-agent/testing/integration
-EOF
