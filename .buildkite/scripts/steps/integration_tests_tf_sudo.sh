067adcce360df28001d27bca87b4d993acb9f9b4
Pavel Zorin <pavel.zorin@elastic.co>
2024-08-19 17:15:20 +0200

    BK steps by groups

--- .buildkite/scripts/steps/integration_tests_tf_sudo.sh
+++ .buildkite/scripts/steps/integration_tests_tf_sudo.sh
@@ -5,6 +5,17 @@ source .buildkite/scripts/common2.sh
 
 source .buildkite/scripts/steps/ess.sh
 
+GROUP_NAME=$1
+if [ -z "$GROUP_NAME" ]; then
+  echo "Error: Specify the group name: sudo-integration-tests.sh [group_name] [tests_to_run]" >&2
+  exit 1
+fi
+TESTS_TO_RUN=$2
+if [ -z "$TESTS_TO_RUN" ]; then
+  echo "Error: Specify the tests to run: sudo-integration-tests.sh [group_name] [tests_to_run]" >&2
+  exit 1
+fi
+
 # Override the agent package version using a string with format <major>.<minor>.<patch>
 # There is a time when the snapshot is not built yet, so we cannot use the latest version automatically
 # This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
@@ -19,4 +30,4 @@ trap 'ess_down' EXIT
 
 # Run integration tests
 echo "~~~ Running integration tests"
-sudo -E .buildkite/scripts/sudo-integration-tests.sh
+sudo -E .buildkite/scripts/sudo-integration-tests.sh "$@"
