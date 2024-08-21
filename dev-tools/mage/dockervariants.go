77dd2a5e359d49dba442342d41ec01914384c2e9
Blake Rouse <blake.rouse@elastic.co>
2024-08-21 11:37:40 -0400

    Add ability to select docker variant.

---
+++ dev-tools/mage/dockervariants.go
@@ -0,0 +1,78 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package mage
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	undefined = "undefined"
+	basic     = "basic"
+	ubi       = "ubi"
+	wolfi     = "wolfi"
+	complete  = "complete"
+	cloud     = "cloud"
+)
+
+// DockerVariant defines the docker variant to build.
+type DockerVariant int
+
+// List of possible docker variants.
+const (
+	Undefined = iota
+	Basic
+	UBI
+	Wolfi
+	Complete
+	Cloud
+)
+
+// String returns the name of the docker variant type.
+func (typ DockerVariant) String() string {
+	switch typ {
+	case Undefined:
+		return undefined
+	case Basic:
+		return basic
+	case UBI:
+		return ubi
+	case Wolfi:
+		return wolfi
+	case Complete:
+		return complete
+	case Cloud:
+		return cloud
+	default:
+		return invalid
+	}
+}
+
+// MarshalText returns the text representation of DockerVariant.
+func (typ DockerVariant) MarshalText() ([]byte, error) {
+	return []byte(typ.String()), nil
+}
+
+// UnmarshalText returns a DockerVariant based on the given text.
+func (typ *DockerVariant) UnmarshalText(text []byte) error {
+	switch strings.ToLower(string(text)) {
+	case "":
+		*typ = Undefined
+	case basic:
+		*typ = Basic
+	case ubi:
+		*typ = UBI
+	case wolfi:
+		*typ = Wolfi
+	case complete:
+		*typ = Complete
+	case cloud:
+		*typ = Cloud
+	default:
+		return fmt.Errorf("unknown docker variant: %v", string(text))
+	}
+	return nil
+}
