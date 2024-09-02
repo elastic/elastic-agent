276230554df3f57cb0fe891123b3fafb10140154
Vihas Makwana <vihas.makwana@elastic.co>
2024-09-02 21:35:56 +0530

    chore: install nginx, start status

--- testing/integration/unprivileged_test.go
+++ testing/integration/unprivileged_test.go
@@ -6,6 +6,8 @@ package integration
 
 import (
 	"context"
+	"os"
+	"os/exec"
 	"testing"
 	"time"
 
@@ -19,29 +21,57 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-var defaultTextCfgUnprivileged = `
-outputs:
-  default:
-    type: elasticsearch
-    hosts: [127.0.0.1:9200]
-    api_key: "example-key"
-    preset: balanced
-    allow_older_versions: true
+// var defaultTextCfgUnprivileged = `
+// outputs:
+//   default:
+//     type: elasticsearch
+//     hosts: [127.0.0.1:9200]
+//     api_key: "example-key"
+//     preset: balanced
+//     allow_older_versions: true
 
-inputs:
-  - type: nginx/metrics
-    id: unique-system-metrics-input
-    data_stream.namespace: default
-    use_output: default
-    streams:
-      - metricsets:
-        - stubstatus
-        data_stream:
-			dataset: nginx.stubstatus
-			type: metrics
+// inputs:
+//   - type: nginx/metrics
+//     id: unique-system-metrics-input
+//     data_stream.namespace: default
+//     use_output: default
+//     streams:
+//       - metricsets:
+//         - stubstatus
+//         data_stream.dataset: nginx.stubstatus
+//         data_stream.type: metrics
 
-agent.logging.level: debug
-agent.logging.to_stderr: true
+// agent.logging.level: debug
+// agent.logging.to_stderr: true
+// `
+
+var nginxStatusModule string = `
+
+server {
+  listen 81;
+  server_name localhost;
+
+  access_log off;
+  allow 127.0.0.1;
+  # deny all;
+
+  location /nginx_status {
+    # Choose your status module
+
+    # freely available with open source NGINX
+    stub_status;
+
+    # for open source NGINX < version 1.7.5
+    # stub_status on;
+
+    # available only with NGINX Plus
+    # status;
+
+    # ensures the version information can be retrieved
+    server_tokens on;
+  }
+}
+
 `
 
 // ExtendedRunner is the main test runner
@@ -73,6 +103,19 @@ func TestNginxUnprivileged(t *testing.T) {
 }
 
 func (runner *UnprivilegedRunner) SetupSuite() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "apt-get", "install", "-v", "nginx-full")
+	out, err := cmd.CombinedOutput()
+	require.NoError(runner.T(), err, "error while installing nginx: %s", string(out))
+
+	err = os.WriteFile("/etc/nginx/conf.d/status.conf", []byte(nginxStatusModule), 0750)
+	require.NoError(runner.T(), err)
+
+	cmd = exec.CommandContext(ctx, "nginx", "-s", "reload")
+	out, err = cmd.CombinedOutput()
+	require.NoError(runner.T(), err, "error while reloading nginx: %s", string(out))
+
 	fixture, err := define.NewFixtureFromLocalBuild(runner.T(), define.Version())
 	require.NoError(runner.T(), err)
 	runner.agentFixture = fixture
@@ -94,12 +137,9 @@ func (runner *UnprivilegedRunner) SetupSuite() {
 		Privileged:     true,
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-	defer cancel()
-
-	// write a default config file that enables monitoring
-	err = runner.agentFixture.WriteFileToWorkDir(ctx, defaultTextCfgUnprivileged, "elastic-agent.yml")
-	require.NoError(runner.T(), err)
+	// // write a default config file that enables monitoring
+	// err = runner.agentFixture.WriteFileToWorkDir(ctx, defaultTextCfgUnprivileged, "elastic-agent.yml")
+	// require.NoError(runner.T(), err)
 
 	policyResp, err := tools.InstallAgentWithPolicy(ctx, runner.T(), installOpts, runner.agentFixture, runner.info.KibanaClient, basePolicy)
 	require.NoError(runner.T(), err)
@@ -107,7 +147,7 @@ func (runner *UnprivilegedRunner) SetupSuite() {
 	runner.policyID = policyResp.ID
 	runner.policyName = basePolicy.Name
 
-	_, err = tools.InstallPackageFromDefaultFile(ctx, runner.info.KibanaClient, "system", "1.53.1", "system_integration_setup.json", uuid.Must(uuid.NewV4()).String(), policyResp.ID)
+	_, err = tools.InstallPackageFromDefaultFile(ctx, runner.info.KibanaClient, "nginx", "1.23.0", "nginx_integration_setup.json", uuid.Must(uuid.NewV4()).String(), policyResp.ID)
 	require.NoError(runner.T(), err)
 }
 
@@ -131,10 +171,16 @@ func (runner *UnprivilegedRunner) AllComponentsHealthy(ctx context.Context) {
 		}
 
 		for _, comp := range status.Components {
-			runner.T().Logf("component state: %s", comp.Message)
+			runner.T().Logf("%s: component state: %s", comp.Name, comp.Message)
 			if comp.State != int(cproto.State_HEALTHY) {
 				compDebugName = comp.Name
 				allHealthy = false
+			}
+			for _, unit := range comp.Units {
+				runner.T().Logf("%s: unit state: %s", unit.UnitID, unit.Message)
+				if unit.State != int(cproto.State_HEALTHY) {
+					allHealthy = false
+				}
 			}
 		}
 		return allHealthy
