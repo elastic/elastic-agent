0126540de078a2e834322f520c3aef03eb579fa2
Anderson Queiroz <anderson.queiroz@elastic.co>
2024-09-06 08:26:43 +0200

    improve testing logger (#5346)
    * add loggertesting package
    
    move logger.NewTesting to loggertesting.New
    add loggertesting.PrintObservedLogs to pretty print logs from a observer.ObservedLogs

--- internal/pkg/agent/application/upgrade/artifact/download/fs/verifier_test.go
+++ internal/pkg/agent/application/upgrade/artifact/download/fs/verifier_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/artifact/download"
 	"github.com/elastic/elastic-agent/internal/pkg/release"
 	"github.com/elastic/elastic-agent/pkg/core/logger"
+	"github.com/elastic/elastic-agent/pkg/core/logger/loggertest"
 	agtversion "github.com/elastic/elastic-agent/pkg/version"
 	"github.com/elastic/elastic-agent/testing/pgptest"
 )
@@ -201,7 +202,7 @@ func TestVerify(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.Name, func(t *testing.T) {
-			log, obs := logger.NewTesting("TestVerify")
+			log, obs := loggertest.New("TestVerify")
 			targetDir := t.TempDir()
 
 			timeout := 30 * time.Second
