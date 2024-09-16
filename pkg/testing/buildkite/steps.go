50878e61e73bcafbb64f46b87f692a3e86767c3c
Blake Rouse <blake.rouse@elastic.co>
2024-09-16 13:03:15 -0400

    Add json struct tags.

--- pkg/testing/buildkite/steps.go
+++ pkg/testing/buildkite/steps.go
@@ -5,20 +5,20 @@
 package buildkite
 
 type StepAgent struct {
-	Provider     string `yaml:"provider,omitempty"`
-	ImageProject string `yaml:"imageProject,omitempty"`
-	MachineType  string `yaml:"machineType,omitempty"`
-	Image        string `yaml:"image,omitempty"`
+	Provider     string `json:"provider,omitempty" yaml:"provider,omitempty"`
+	ImageProject string `json:"imageProject,omitempty" yaml:"imageProject,omitempty"`
+	MachineType  string `json:"machineType,omitempty" yaml:"machineType,omitempty"`
+	Image        string `json:"image,omitempty" yaml:"image,omitempty"`
 }
 
 type Step struct {
-	Key                    string            `yaml:"key,omitempty"`
-	Label                  string            `yaml:"label,omitempty"`
-	Command                string            `yaml:"command,omitempty"`
-	Env                    map[string]string `yaml:"env,omitempty"`
-	ArtifactPaths          []string          `yaml:"artifact_paths,omitempty"`
-	Agents                 []StepAgent       `yaml:"agents,omitempty"`
-	DependsOn              []string          `yaml:"depends_on,omitempty"`
-	AllowDependencyFailure bool              `yaml:"allow_dependency_failure,omitempty"`
-	Steps                  []Step            `yaml:"steps,omitempty"`
+	Key                    string            `json:"key,omitempty" yaml:"key,omitempty"`
+	Label                  string            `json:"label,omitempty" yaml:"label,omitempty"`
+	Command                string            `json:"command,omitempty" yaml:"command,omitempty"`
+	Env                    map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	ArtifactPaths          []string          `json:"artifact_paths,omitempty" yaml:"artifact_paths,omitempty"`
+	Agents                 []StepAgent       `json:"agents,omitempty" yaml:"agents,omitempty"`
+	DependsOn              []string          `json:"depends_on,omitempty" yaml:"depends_on,omitempty"`
+	AllowDependencyFailure bool              `json:"allow_dependency_failure,omitempty" yaml:"allow_dependency_failure,omitempty"`
+	Steps                  []Step            `json:"steps,omitempty" yaml:"steps,omitempty"`
 }
