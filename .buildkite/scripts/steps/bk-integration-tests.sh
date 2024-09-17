dd735d66e02a194ec90db61f32c28a9ea4db2c91
Pavel Zorin <pavel.zorin@elastic.co>
2024-09-17 14:44:57 +0200

    Renamed the yaml file

--- .buildkite/scripts/steps/bk-integration-tests.sh
+++ .buildkite/scripts/steps/bk-integration-tests.sh
@@ -15,12 +15,12 @@ if [[ -n "$STACK_VERSION" ]]; then
 fi
 
 # Generate the integration test pipeline
-AGENT_STACK_VERSION="${STACK_VERSION}" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:buildkite > buildkite.yml
+AGENT_STACK_VERSION="${STACK_VERSION}" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage integration:buildkite > it_buildkite.yml
 
 # Debug output the pipeline
 echo "--- START BUILDKITE GENERATED PIPELINE ---"
-cat buildkite.yml
+cat it_buildkite.yml
 echo "--- END BUILDKITE GENERATED PIPELINE ---"
 
 # Upload the pipeline
-buildkite-agent pipeline upload --debug buildkite.yml
+buildkite-agent pipeline upload --debug ./it_buildkite.yml
