99757d316a70ff0cd8dc9359cf02e75da40a3783
Blake Rouse <blake.rouse@elastic.co>
2024-09-21 10:02:18 -0400

    mage check

--- testing/integration/kubernetes_agent_standalone_test.go
+++ testing/integration/kubernetes_agent_standalone_test.go
@@ -24,8 +24,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/go-elasticsearch/v8"
 
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
