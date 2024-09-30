4f1b53b5090b441abf75e6ac362a5cb329a44e4a
Blake Rouse <blake.rouse@elastic.co>
2024-09-30 14:32:37 -0400

    Add ability to generate buildkite pipelines from integration testing framework (#5391)
    Adds the ability to generate the integration testing workload as buildkite pipelines.
    
    This doesn't actually complete the enablement. This just provides a great starting point for generating the buildkite steps. More work is still to be done to actually perform the work in the steps.
    
    (cherry picked from commit c645a5af3f9597e5aecd6eafc6e1d06d1f0a555c)

---
+++ pkg/testing/buildkite/steps.go
@@ -0,0 +1,25 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package buildkite
+
+type StepAgent struct {
+	Provider     string `json:"provider,omitempty" yaml:"provider,omitempty"`
+	ImageProject string `json:"imageProject,omitempty" yaml:"imageProject,omitempty"`
+	MachineType  string `json:"machineType,omitempty" yaml:"machineType,omitempty"`
+	Image        string `json:"image,omitempty" yaml:"image,omitempty"`
+}
+
+type Step struct {
+	Key                    string            `json:"key,omitempty" yaml:"key,omitempty"`
+	Label                  string            `json:"label,omitempty" yaml:"label,omitempty"`
+	Command                string            `json:"command,omitempty" yaml:"command,omitempty"`
+	Env                    map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	ArtifactPaths          []string          `json:"artifact_paths,omitempty" yaml:"artifact_paths,omitempty"`
+	Agents                 []StepAgent       `json:"agents,omitempty" yaml:"agents,omitempty"`
+	DependsOn              []string          `json:"depends_on,omitempty" yaml:"depends_on,omitempty"`
+	AllowDependencyFailure bool              `json:"allow_dependency_failure,omitempty" yaml:"allow_dependency_failure,omitempty"`
+	Steps                  []Step            `json:"steps,omitempty" yaml:"steps,omitempty"`
+	Skip                   bool              `json:"skip,omitempty" yaml:"skip,omitempty"`
+}
