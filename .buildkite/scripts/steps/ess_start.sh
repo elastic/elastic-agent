e1ecab714e582395ef55f3f7090f70c986af5b8a
Pavel Zorin <pavel.zorin@elastic.co>
2024-10-01 11:39:12 +0200

    Debug

--- .buildkite/scripts/steps/ess_start.sh
+++ .buildkite/scripts/steps/ess_start.sh
@@ -9,3 +9,11 @@ OVERRIDE_AGENT_PACKAGE_VERSION="$(cat .package-version)"
 OVERRIDE_TEST_AGENT_VERSION=${OVERRIDE_AGENT_PACKAGE_VERSION}"-SNAPSHOT"
 
 ess_up $OVERRIDE_TEST_AGENT_VERSION || echo "Failed to start ESS stack" >&2
+
+echo "ES_HOST: ${ELASTICSEARCH_HOST}"
+buildkite-agent meta-data set "es.host" $ELASTICSEARCH_HOST
+buildkite-agent meta-data set "es.username" $ELASTICSEARCH_USERNAME
+buildkite-agent meta-data set "es.pwd" $ELASTICSEARCH_PASSWORD
+buildkite-agent meta-data set "kibana.host" $KIBANA_HOST
+buildkite-agent meta-data set "kibana.username" $KIBANA_USERNAME
+buildkite-agent meta-data set "kibana.pwd" $KIBANA_PASSWORD
