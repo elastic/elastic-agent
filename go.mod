145ce2bd1100b0daa2a54a908a7d837633804c00
kruskall <99559985+kruskall@users.noreply.github.com>
2024-10-03 04:12:34 +0200

    feat: bump to go 1.23.2

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/elastic/elastic-agent
 
-go 1.23.1
+go 1.23.2
 
 require (
 	github.com/Jeffail/gabs/v2 v2.6.0
--- pkg/testing/runner/runner_test.go
+++ pkg/testing/runner/runner_test.go
@@ -28,7 +28,7 @@ func TestNewRunner_Clean(t *testing.T) {
 		AgentVersion: "8.10.0",
 		StackVersion: "8.10.0-SNAPSHOT",
 		BuildDir:     filepath.Join(tmpdir, "build"),
-		GOVersion:    "1.20.7",
+		GOVersion:    "1.23.2",
 		RepoDir:      filepath.Join(tmpdir, "repo"),
 		StateDir:     stateDir,
 		ExtraEnv:     nil,
