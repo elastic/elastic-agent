c5812aa830a3e0045e4e2fd75db30af3d13d093e
Alexandros Sapranidis <alexandros@elastic.co>
2024-10-07 12:43:37 +0300

    Adjust coverage files
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/steps/unit-tests.ps1
+++ .buildkite/scripts/steps/unit-tests.ps1
@@ -21,7 +21,7 @@ mage unitTest
 # Copy coverage file to build directory so it can be downloaded as an artifact
 Write-Host "--- Prepare artifacts"
 $buildkiteJobId = $env:BUILDKITE_JOB_ID
-Move-Item -Path "build/TEST-go-unit.cov" -Destination coverage.out
+Move-Item -Path "build/TEST-go-unit.cov" -Destination "coverage-$buildkiteJobId.out"
 Move-Item -Path "build/TEST-go-unit.xml" -Destination "build/TEST-$buildkiteJobId.xml"
 if ($LASTEXITCODE -ne 0) {
   exit 1
--- .buildkite/scripts/steps/unit-tests.sh
+++ .buildkite/scripts/steps/unit-tests.sh
@@ -8,6 +8,6 @@ RACE_DETECTOR=true TEST_COVERAGE=true mage unitTest
 TESTS_EXIT_STATUS=$?
 echo "--- Prepare artifacts"
 # Copy coverage file to build directory so it can be downloaded as an artifact
-mv build/TEST-go-unit.cov coverage.out
+mv build/TEST-go-unit.cov "coverage-${BUILDKITE_JOB_ID:go-unit}.out"
 mv build/TEST-go-unit.xml build/"TEST-${BUILDKITE_JOB_ID:go-unit}.xml"
 exit $TESTS_EXIT_STATUS
