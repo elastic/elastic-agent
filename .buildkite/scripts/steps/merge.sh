55e0200f0270ac61f7641e0b1b7252cc97591ba7
Alexandros Sapranidis <alexandros@elastic.co>
2024-10-07 18:47:30 +0300

    gocoverage flag
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/steps/merge.sh
+++ .buildkite/scripts/steps/merge.sh
@@ -12,5 +12,5 @@ go install github.com/wadey/gocovmerge@latest
 buildkite-agent artifact download "coverage-*.out" .
 # Space separated list of paths to coverage files
 COV_PATHS=$(find coverage-*.out -exec printf '%s ' {} \;)
-gocovmerge "$COV_PATHS" > "$MERGED_COV_FILE"
+gocovmerge $COV_PATHS > "$MERGED_COV_FILE"
 echo "Merged coverage file: $MERGED_COV_FILE. See artifacts"
