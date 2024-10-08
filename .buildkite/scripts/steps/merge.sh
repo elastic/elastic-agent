0d5b3354d73ddd8f025ab6e3110bf9eb23aaa23e
Alexandros Sapranidis <alexandros@elastic.co>
2024-10-08 17:09:06 +0300

    Add JUnit annotate BK plugin (#5473)
    * Add JUnit annotate BK plugin
    
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/steps/merge.sh
+++ .buildkite/scripts/steps/merge.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 # Downloads and merges coverage files from multiple steps into a single file (build/TEST-go-unit.cov).
-# Usage: merge.sh <step1> <step2> ... Where <step> is the id of the step that contains the coverage artifact.#  
-
+# Usage: merge.sh <step1> <step2> ... Where <step> is the id of the step that contains the coverage artifact.#
 set -euo pipefail
 set -x # for debugging
 
-COV_ARTIFACT="coverage.out"
-MERGED_COV_FILE="build/TEST-go-unit.cov"
-# Space separated list of paths to coverage files
-COV_PATHS=""
+MERGED_COV_FILE="TEST-go-unit.cov"
 
 go install github.com/wadey/gocovmerge@latest
-mkdir -p build
 
-for STEP_ID in "$@"; do
-  mkdir -p $STEP_ID 
-  buildkite-agent artifact download --step $STEP_ID $COV_ARTIFACT $STEP_ID
-  COV_PATHS="${COV_PATHS} $STEP_ID/$COV_ARTIFACT"
-done
-
-gocovmerge $COV_PATHS > $MERGED_COV_FILE
+buildkite-agent artifact download "coverage-*.out" .
+# Space separated list of paths to coverage files
+find coverage-*.out -exec printf '%s ' {} \; | xargs gocovmerge > "$MERGED_COV_FILE"
 echo "Merged coverage file: $MERGED_COV_FILE. See artifacts"
