279aec449cda10342bb99844ec3add0119443a05
michel-laterman <michel.laterman@elastic.co>
2024-10-09 14:14:54 -0700

    Change to yaml.v3 in integration tests

--- testing/integration/diagnostics_test.go
+++ testing/integration/diagnostics_test.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/elastic/elastic-agent/pkg/control/v2/client"
 	integrationtest "github.com/elastic/elastic-agent/pkg/testing"
--- testing/integration/proxy_url_test.go
+++ testing/integration/proxy_url_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/gofrs/uuid/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/elastic/elastic-agent-libs/testing/certutil"
 	integrationtest "github.com/elastic/elastic-agent/pkg/testing"
