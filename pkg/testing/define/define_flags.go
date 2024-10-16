6a3fffb5c904281dde50b9fd5da798cdc7006ba5
Paolo Chila <paolo.chila@elastic.co>
2024-10-16 19:04:29 +0200

    Fix integration test flags and add dry-run

--- pkg/testing/define/define_flags.go
+++ pkg/testing/define/define_flags.go
@@ -1,17 +1,53 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 package define
 
 import (
-	"github.com/spf13/pflag"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
 )
 
 var (
 	DryRun    bool
 	Groups    []string
 	Platforms []string
+
+	groupStringFlag    string
+	platformStringFlag string
 )
 
-func RegisterFlags(prefix string, set *pflag.FlagSet) {
+func RegisterFlags(prefix string, set *flag.FlagSet) {
 	set.BoolVar(&DryRun, prefix+"dry-run", false, "Forces test in dry-run mode: drops platform/group/sudo requirements")
-	set.StringArrayVar(&Groups, prefix+"groups", nil, "test groups")
-	set.StringArrayVar(&Platforms, prefix+"plarforms", nil, "test platforms")
+	set.StringVar(&groupStringFlag, prefix+"groups", "", "test groups, comma-separated")
+	set.StringVar(&platformStringFlag, prefix+"platforms", "", "test platforms, comma-separated")
+}
+
+func ParseFlags() {
+	Groups = splitStringToArray(groupStringFlag)
+	Platforms = splitStringToArray(platformStringFlag)
+}
+
+func splitStringToArray(stringFlag string) []string {
+	if stringFlag == "" {
+		return nil
+	}
+	fmt.Fprintf(os.Stderr, "Splitting %q...", stringFlag)
+	return strings.Split(stringFlag, ",")
+}
+
+func dryRun(t *testing.T, req Requirements) *Info {
+	// always validate requirement is valid
+	if err := req.Validate(); err != nil {
+		t.Logf("test %s has invalid requirements: %s", t.Name(), err)
+		t.FailNow()
+		return nil
+	}
+	// skip the test as we are in dry run
+	t.Skip(fmt.Sprintf("Skipped because dry-run mode has been specified."))
+	return nil
 }
--- testing/integration/main_test.go
+++ testing/integration/main_test.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 package integration
 
 import (
@@ -6,10 +10,9 @@ import (
 	"testing"
 
 	"github.com/elastic/elastic-agent/pkg/testing/define"
-	"github.com/spf13/pflag"
 )
 
-var flagSet = pflag.CommandLine
+var flagSet = flag.CommandLine
 
 func init() {
 	define.RegisterFlags("integration.", flagSet)
@@ -17,7 +20,7 @@ func init() {
 
 func TestMain(m *testing.M) {
 	flag.Parse()
-
+	define.ParseFlags()
 	runExitCode := m.Run()
 
 	if define.DryRun {
