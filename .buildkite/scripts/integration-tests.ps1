6741e3d938328e1d3b58d294eb9d17846626cfca
Pavel Zorin <pavel.zorin@elastic.co>
2024-11-08 18:40:55 +0100

    Test PS5.1

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -15,49 +15,53 @@ $env:KIBANA_PASSWORD = & buildkite-agent meta-data get "kibana.pwd"
 Write-Output "~~~ Running integration tests as $env:USERNAME"
 Write-Output "~~~ Integration tests: $GROUP_NAME"
 
-# Check gotestsum version
-# $Env:PATH
-# go env
-# TODO: add to the image
-# $Env:PATH += ";C:\Users\Buildkite\.go\go-1.22.8\bin"
 go install gotest.tools/gotestsum
-# tree "C:\Users\Buildkite\.go\go-1.22.8\packages\bin"
 gotestsum --version
 
-$PSVersionTable.PSVersion
+# Read package version from .package-version file
+$PACKAGE_VERSION = Get-Content .package-version -ErrorAction SilentlyContinue
+if ($PACKAGE_VERSION) {
+    $PACKAGE_VERSION = "${PACKAGE_VERSION}-SNAPSHOT"
+}
 
-gotestsum --no-color -f standard-quiet --junitfile "build/${GROUP_NAME}.integration.xml" --jsonfile "build/${GROUP_NAME}.integration.out.json" -- "-tags=integration" "-test.shuffle=on" "-test.timeout=2h0m0s" "github.com/elastic/elastic-agent/testing/integration" -v "-args -integration.groups=$GROUP_NAME -integration.sudo=true"
+echo "~~~ Building test binaries"
+mage build:testBinaries
 
-# # Read package version from .package-version file
-# $PACKAGE_VERSION = Get-Content .package-version -ErrorAction SilentlyContinue
-# if ($PACKAGE_VERSION) {
-#     $PACKAGE_VERSION = "${PACKAGE_VERSION}-SNAPSHOT"
-# }
+# Run integration tests with gotestsum
+echo "~~~ Running tests"
+$env:TEST_BINARY_NAME = "elastic-agent"
+$env:AGENT_VERSION = $PACKAGE_VERSION
+$env:SNAPSHOT = $true
 
-# echo "~~~ Building test binaries"
-# mage build:testBinaries
+# Error handling setup for gotestsum execution
 
-# # Run integration tests with gotestsum
-# echo "~~~ Running tests"
-# $env:TEST_BINARY_NAME = "elastic-agent"
-# $env:AGENT_VERSION = $PACKAGE_VERSION
-# $env:SNAPSHOT = $true
+$arguments = @(
+    "--no-color"
+    "-f", "standard-quiet"
+    "--junitfile", "build/${GROUP_NAME}.integration.xml"
+    "--jsonfile", "build/${GROUP_NAME}.integration.out.json"
+    "--", "-tags=integration"
+    "-test.shuffle=on"
+    "-test.timeout=2h0m0s"
+    "github.com/elastic/elastic-agent/testing/integration"
+    "-v"
+    "-args", "-integration.groups=$GROUP_NAME", "-integration.sudo=true"
+)
 
-# # Error handling setup for gotestsum execution
-# $ErrorActionPreference = 'Continue'
-# gotestsum --no-color -f standard-quiet --junitfile "build/${GROUP_NAME}.integration.xml" --jsonfile "build/${GROUP_NAME}.integration.out.json" -- "-tags=integration" "-test.shuffle=on" "-test.timeout=2h0m0s" "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=true"
-# $TESTS_EXIT_STATUS = $LASTEXITCODE
-# $ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Continue'
+Start-Process -FilePath "gotestsum" -ArgumentList $arguments -NoNewWindow -Wait
+$TESTS_EXIT_STATUS = $LASTEXITCODE
+$ErrorActionPreference = 'Stop'
 
-# # Generate HTML report if XML output exists
-# $outputXML = "build/${GROUP_NAME}.integration.xml"
-# if (Test-Path $outputXML) {
-#     # Install junit2html if not installed
-#     go install github.com/alexec/junit2html@latest
-#     Get-Content $outputXML | junit2html > "build/TEST-report.html"
-# } else {
-#     Write-Output "Cannot generate HTML test report: $outputXML not found"
-# }
+# Generate HTML report if XML output exists
+$outputXML = "build/${GROUP_NAME}.integration.xml"
+if (Test-Path $outputXML) {
+    # Install junit2html if not installed
+    go install github.com/alexec/junit2html@latest
+    Get-Content $outputXML | junit2html > "build/TEST-report.html"
+} else {
+    Write-Output "Cannot generate HTML test report: $outputXML not found"
+}
 
-# # Exit with the test exit status
-# exit $TESTS_EXIT_STATUS
+# Exit with the test exit status
+exit $TESTS_EXIT_STATUS
