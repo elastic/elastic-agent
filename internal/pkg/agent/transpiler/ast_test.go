83fcb38275c5c5c5f2dde3808d0db38965a6b3c9
Mikołaj Świątek <mail@mikolajswiatek.com>
2024-11-08 18:45:03 +0100

    Cache conditions when applying variables to config

--- internal/pkg/agent/transpiler/ast_test.go
+++ internal/pkg/agent/transpiler/ast_test.go
@@ -9,6 +9,8 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/elastic/elastic-agent/internal/pkg/eql"
+
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -1069,6 +1071,29 @@ func TestLookup(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCondition(t *testing.T) {
+	vars := mustMakeVars(map[string]interface{}{
+		"other": map[string]interface{}{
+			"data": "info",
+		}})
+
+	input := NewKey("condition", NewStrVal("${other.data} == 'info'"))
+	expected := NewKey("condition", NewBoolVal(true))
+
+	// the condition string hasn't been parsed yet
+	assert.Nil(t, input.condition)
+
+	output, err := input.Apply(vars)
+	require.NoError(t, err)
+	assert.Equal(t, expected, output)
+
+	// check if the condition was parsed and cached
+	assert.NotNil(t, input.condition)
+	condition, err := eql.New(input.value.Value().(string))
+	require.NoError(t, err)
+	assert.Equal(t, condition, input.condition)
 }
 
 func mustMakeVars(mapping map[string]interface{}) *Vars {
