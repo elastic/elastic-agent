c01636e7383a9b2af9a588e0fcf1a4cae7d0d65c
Pavel Zorin <pavel.zorin@elastic.co>
2024-11-11 12:55:04 +0100

    [CI] Buildkite integration tests: Run integration tests on ARM (#5956)
    * [CI]Run Ubuntu x86 and ARM integration tests on buildkite
    
    * Updated the new pipeline
    
    * Applied review changes

--- .buildkite/scripts/sudo-integration-tests.sh
+++ .buildkite/scripts/sudo-integration-tests.sh
@@ -5,7 +5,6 @@ source /opt/buildkite-agent/hooks/pre-command
 source .buildkite/hooks/pre-command || echo "No pre-command hook found"
 
 GROUP_NAME=$1
-TESTS_TO_RUN=$2
 
 echo "~~~ Running integration tests as $USER"
 echo "~~~ Integration tests: ${GROUP_NAME}"
@@ -15,7 +14,7 @@ if [[ -n "$PACKAGE_VERSION" ]]; then
     PACKAGE_VERSION=${PACKAGE_VERSION}"-SNAPSHOT"
 fi
 set +e
-AGENT_VERSION="${PACKAGE_VERSION}" SNAPSHOT=true TEST_DEFINE_PREFIX="sudo_${GROUP_NAME}_ubuntu" gotestsum --no-color -f standard-quiet --junitfile "build/${GROUP_NAME}.integration.xml" --jsonfile "build/${GROUP_NAME}.integration.out.json" -- -tags integration -test.shuffle on -test.timeout 2h0m0s -test.run "${TESTS_TO_RUN}" github.com/elastic/elastic-agent/testing/integration
+TEST_BINARY_NAME="elastic-agent" AGENT_VERSION="${PACKAGE_VERSION}" SNAPSHOT=true gotestsum --no-color -f standard-quiet --junitfile "build/${GROUP_NAME}.integration.xml" --jsonfile "build/${GROUP_NAME}.integration.out.json" -- -tags integration -test.shuffle on -test.timeout 2h0m0s github.com/elastic/elastic-agent/testing/integration -v -args -integration.groups="${GROUP_NAME}" -integration.sudo=true
 TESTS_EXIT_STATUS=$?
 set -e
 
