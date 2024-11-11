baaeb4d20afc946ffe79dd51732dc006e8c62d7c
kaanyalti <kaan.yalti@elastic.co>
2024-11-11 04:15:59 +0300

    enhancement(5320): added test case where we get install package and get diagnostics

--- testing/integration/escaping_secrets_in_policy_test.go
+++ testing/integration/escaping_secrets_in_policy_test.go
@@ -3,12 +3,12 @@
 package integration
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
-	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
-	"text/template"
 	"time"
 
 	"github.com/elastic/elastic-agent-libs/kibana"
@@ -17,19 +17,19 @@ import (
 	"github.com/elastic/elastic-agent/pkg/testing/tools"
 	"github.com/gofrs/uuid/v5"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 //go:embed custom_log_package.json.tmpl
 var customLogPackagePolicy string
 
-func TestEscapingSecretsInPolicy(t *testing.T) {
+func TestEscapingSecretsInPolicy2(t *testing.T) {
 	info := define.Require(t, define.Requirements{
 		Group: Default,
 		Stack: &define.Stack{},
 		Sudo:  true,
 	})
 	ctx := context.Background()
-
 	fixture, err := define.NewFixtureFromLocalBuild(t, define.Version())
 	require.NoError(t, err)
 	installOpts := atesting.InstallOpts{
@@ -38,44 +38,145 @@ func TestEscapingSecretsInPolicy(t *testing.T) {
 		Privileged:     true,
 	}
 
-	polId := uuid.Must(uuid.NewV4()).String()
-	policy := kibana.AgentPolicy{
-		Name:        "test-policy-" + polId,
+	randId := uuid.Must(uuid.NewV4()).String()
+	policyReq := kibana.AgentPolicy{
+		Name:        "test-policy-" + randId,
 		Namespace:   "default",
-		Description: "Test policy " + polId,
+		Description: "Test policy " + randId,
 		MonitoringEnabled: []kibana.MonitoringEnabledOption{
 			kibana.MonitoringEnabledLogs,
 			kibana.MonitoringEnabledMetrics,
 		},
 	}
+	policy, err := info.KibanaClient.CreatePolicy(ctx, policyReq)
 
-	policyResp, err := tools.InstallAgentWithPolicy(ctx, t, installOpts, fixture, info.KibanaClient, policy)
-
-	tmpl := template.Must(template.New("custom_log_package_policy").Parse(customLogPackagePolicy))
-	pkgPolId := uuid.Must(uuid.NewV4()).String()
-
-	templateVars := struct {
-		PolicyId    string
-		PkgPolicyId string
-	}{
-		PolicyId:    policyResp.AgentPolicy.ID,
-		PkgPolicyId: pkgPolId,
+	pkgPolicyReq := kibana.PackagePolicyRequest{
+		Name:      fmt.Sprintf("log-%s", randId),
+		Namespace: "default",
+		PolicyID:  policy.ID,
+		Package: kibana.PackagePolicyRequestPackage{
+			Name:    "log",
+			Version: "2.3.2",
+		},
+		Vars: map[string]interface{}{}, // Empty as shown in the example
+		Inputs: []map[string]interface{}{
+			{
+				"enabled":         true,
+				"policy_template": "logs",
+				"type":            "logfile",
+				"streams": []map[string]interface{}{
+					{
+						"data_stream": map[string]interface{}{
+							"dataset": "log.logs",
+							"type":    "logs",
+						},
+						"enabled": true,
+						"vars": map[string]interface{}{
+							"custom": map[string]interface{}{
+								"type":  "yaml",
+								"value": "testing: $$$$",
+							},
+							"data_stream.dataset": map[string]interface{}{
+								"type":  "text",
+								"value": "generic",
+							},
+							"paths": map[string]interface{}{
+								"type":  "text",
+								"value": []string{"asdf"},
+							},
+							"exlude_files": map[string]interface{}{
+								"type":  "text",
+								"value": []string{},
+							},
+							"tags": map[string]interface{}{
+								"type":  "text",
+								"value": []string{},
+							},
+						},
+					},
+				},
+			},
+		},
+		Force: false,
 	}
-
-	var tmplBuf bytes.Buffer
-	err = tmpl.Execute(&tmplBuf, templateVars)
-	require.NoError(t, err)
-
-	pkgPolicyReq := kibana.PackagePolicyRequest{}
-	err = json.Unmarshal(tmplBuf.Bytes(), &pkgPolicyReq)
-	require.NoError(t, err)
 
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
 	defer cancel()
 
-	// pkgResp, err := info.KibanaClient.InstallFleetPackage(ctx, pkgPolicyReq)
-	// require.NoError(t, err)
 	_, err = info.KibanaClient.InstallFleetPackage(ctx, pkgPolicyReq)
-	// require.NoError(t, err)
-	require.Error(t, err)
+	require.NoError(t, err)
+
+	err = tools.InstallAgentForPolicy(ctx, t, installOpts, fixture, info.KibanaClient, policy.ID)
+	require.NoError(t, err)
+
+	diagZip, err := fixture.ExecDiagnostics(ctx, "diagnostics", "-p")
+	require.NoError(t, err)
+
+	extractDir := t.TempDir()
+
+	extractZipArchive(t, diagZip, extractDir)
+	fmt.Println("=================================== HERE ====================================")
+	fmt.Printf("%+v\n", pkgPolicyReq)
+	fmt.Println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+	entries, _ := os.ReadDir(extractDir)
+	for _, entry := range entries {
+		fmt.Println(entry.Name())
+	}
+	fmt.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+	preConfPath := filepath.Join(extractDir, "pre-config.yaml")
+	preStat, err := os.Stat(preConfPath)
+	require.NoErrorf(t, err, "stat file %q failed", preConfPath)
+	require.Greaterf(t, preStat.Size(), int64(0), "file %s has incorrect size", preConfPath)
+	pref, err := os.Open(preConfPath)
+	require.NoErrorf(t, err, "open file %q failed", preConfPath)
+	defer pref.Close()
+
+	preConfObj := struct {
+		Inputs []map[string]interface{} `yaml:"inputs"`
+	}{}
+	err = yaml.NewDecoder(pref).Decode(&preConfObj)
+	require.NoError(t, err)
+
+	preConfCheck := false
+
+	for _, input := range preConfObj.Inputs {
+		if name, ok := input["name"]; ok && name == pkgPolicyReq.Name {
+			streamArr, ok := input["stream"].([]map[string]interface{})
+			require.True(t, ok)
+
+			for _, stream := range streamArr {
+				actual, ok := stream["testing"]
+				require.True(t, ok)
+				require.Equal(t, "$$$$", actual)
+				preConfCheck = true
+			}
+		}
+	}
+
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Printf("%+v\n", preConfObj)
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+	rendConfPath := filepath.Join(extractDir, "components", "log-default", "beat-rendered-config.yml")
+	rendStat, err := os.Stat(rendConfPath)
+	require.NoErrorf(t, err, "stat file %q failed", rendConfPath)
+	require.Greaterf(t, rendStat.Size(), int64(0), "file %s has incorrect size", rendConfPath)
+	rendf, err := os.Open(rendConfPath)
+	require.NoErrorf(t, err, "open file %q failed", rendConfPath)
+	defer rendf.Close()
+
+	rendConfObj := struct {
+		Inputs []map[string]interface{} `yaml:"inputs"`
+	}{}
+	err = yaml.NewDecoder(rendf).Decode(&rendConfObj)
+	require.NoError(t, err)
+
+	fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+	inspOut, err := fixture.ExecInspect(ctx)
+	require.NoError(t, err)
+	ymlbyte, _ := yaml.Marshal(inspOut)
+	fmt.Println(string(ymlbyte))
+	fmt.Println("#############################################################################")
+
+	require.Equal(t, true, false)
 }
