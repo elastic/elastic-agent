6fc6254dc246cafd551ad1db59f6e641dfb49880
Pavel Zorin <pavel.zorin@elastic.co>
2024-11-29 16:29:44 +0200

    Avoid unbound variable

--- .buildkite/scripts/set-bk-analytics-token.ps1
+++ .buildkite/scripts/set-bk-analytics-token.ps1
@@ -2,8 +2,9 @@ $ErrorActionPreference = "Stop"
 
 . "$PWD\.buildkite\scripts\retry.ps1"
 
-Write-Host "--- Set BUILDKITE_ANALYTICS_TOKEN :vault:"
+Write-Host "~~~ Set BUILDKITE_ANALYTICS_TOKEN :vault:"
 $BUILDKITE_ANALYTICS_TOKEN = Retry-Command -ScriptBlock {
   vault kv get -field=token kv/ci-shared/platform-ingest/buildkite_analytics_token
 }
-[System.Environment]::SetEnvironmentVariable("BUILDKITE_ANALYTICS_TOKEN", $BUILDKITE_ANALYTICS_TOKEN, [System.EnvironmentVariableTarget]::User)
+# [System.Environment]::SetEnvironmentVariable("BUILDKITE_ANALYTICS_TOKEN", $BUILDKITE_ANALYTICS_TOKEN, [System.EnvironmentVariableTarget]::User)
+$env.BUILDKITE_ANALYTICS_TOKEN = $BUILDKITE_ANALYTICS_TOKEN
