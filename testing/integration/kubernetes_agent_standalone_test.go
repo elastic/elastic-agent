1d8531922126c775682067e6adcfd17681db013d
kaanyalti <kaan.yalti@elastic.co>
2024-12-03 06:49:51 -0500

    enhancement(5832): fixed imports

--- testing/integration/kubernetes_agent_standalone_test.go
+++ testing/integration/kubernetes_agent_standalone_test.go
@@ -31,6 +31,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
