7e21c014820042943cf1ca7f6a5a943a99c8e7f8
kaanyalti <kaan.yalti@elastic.co>
2024-12-03 19:27:15 -0500

    enhancement(5832): added integration tests

---
+++ testing/integration/restrict_upgrade_rpm_test.go
@@ -0,0 +1,54 @@
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/cmd"
+	atesting "github.com/elastic/elastic-agent/pkg/testing"
+	"github.com/elastic/elastic-agent/pkg/testing/define"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRestrictUpgradeRPM(t *testing.T) {
+	define.Require(t, define.Requirements{
+		Group: RPM,
+		Stack: &define.Stack{},
+		Sudo:  true,
+		OS: []define.OS{
+			{
+				Type:   define.Linux,
+				Distro: "rhel",
+			},
+		},
+	})
+	t.Run("when agent is deployed via rpm, a user should not be able to upgrade the agent using the cli", func(t *testing.T) {
+		ctx := context.Background()
+
+		fixture, err := define.NewFixtureFromLocalBuild(t, define.Version(), atesting.WithPackageFormat("rpm"))
+		require.NoError(t, err)
+		installOpts := atesting.InstallOpts{
+			NonInteractive: true,
+			Privileged:     true,
+			Force:          true,
+		}
+
+		_, err = fixture.InstallWithoutEnroll(ctx, &installOpts)
+		require.NoError(t, err)
+
+		require.Eventuallyf(t, func() bool {
+			err = fixture.IsHealthy(ctx)
+			return err == nil
+		}, 5*time.Minute, time.Second,
+			"Elastic-Agent did not report healthy. Agent status error: \"%v\"",
+			err,
+		)
+
+		out, err := fixture.Exec(ctx, []string{"upgrade", "1.0.0"})
+		require.Error(t, err)
+		require.Contains(t, string(out), cmd.UpgradeDisabledError.Error())
+	})
+}
