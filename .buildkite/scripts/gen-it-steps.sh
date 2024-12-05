b5c09637916a1a3cbd720a61ef3399c7cdc3200d
Paolo Chila <paolo.chila@elastic.co>
2024-12-05 09:44:32 +0100

    fixup! fixup! WIP try to add a dynamic last step

--- .buildkite/scripts/gen-it-steps.sh
+++ .buildkite/scripts/gen-it-steps.sh
@@ -2,17 +2,17 @@
 
 set -eo pipefail
 
+AUTODISCOVERY_YAML_FILE="${PWD}/build/discovered_tests.yaml"
 echo "--- Autodiscovering tests"
 [ -d "${PWD}/build" ] || mkdir -p "${PWD}/build"
 
-IT_YAML_FILE="${PWD}/build/discovered_tests.yaml"
-go mod download
-echo "--- Downloaded go mods"
-go test -tags integration github.com/elastic/elastic-agent/testing/integration -v -args -integration.dry-run=true -integration.autodiscover -integration.autodiscoveryoutput=${IT_YAML_FILE}
-GROUPS_YAML=$(yq '[.[].groups.[].name]|unique' ${IT_YAML_FILE})
+go test -tags integration github.com/elastic/elastic-agent/testing/integration -v -args -integration.dry-run=true -integration.autodiscover -integration.autodiscoveryoutput="${AUTODISCOVERY_YAML_FILE}"
+GROUPS_YAML=$(yq '[.[].groups.[].name]|unique' "${AUTODISCOVERY_YAML_FILE}")
+echo "--- Test autodiscovery output: ${AUTODISCOVERY_YAML_FILE}"
 echo "--- Detected groups"
-echo ${GROUPS_YAML}
+echo "${GROUPS_YAML}"
 echo "--- Generating dynamic pipeline"
-DYN_PIPELINE="${PWD}/build/dyn.pipeline.yml"
+DYNAMIC_PIPELINE="${PWD}/build/dyn.pipeline.yml"
 # The 'has("steps")' filter is needed to avoid creating empty arrays in 'label' items
-GROUPS_YAML=${GROUPS_YAML} yq '((.steps.[]|has("steps")).steps.[]|has("matrix")).matrix=env(GROUPS_YAML)' .buildkite/bk.integration.pipeline.yml > "${DYN_PIPELINE}"
+GROUPS_YAML=${GROUPS_YAML} yq '((.steps.[]|has("steps")).steps.[]|has("matrix")).matrix=env(GROUPS_YAML)' .buildkite/bk.integration.pipeline.yml > "${DYNAMIC_PIPELINE}"
+echo "--- Generated dynamic pipeline in ${DYNAMIC_PIPELINE}"
