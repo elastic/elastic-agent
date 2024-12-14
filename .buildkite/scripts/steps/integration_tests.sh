2f45cf18cf69f04a998b811105036253871014f0
kaanyalti <kaan.yalti@elastic.co>
2024-12-13 23:45:02 -0500

    enhancement(5832): debugging ci issues

--- .buildkite/scripts/steps/integration_tests.sh
+++ .buildkite/scripts/steps/integration_tests.sh
@@ -7,19 +7,18 @@ STACK_PROVISIONER="${1:-"stateful"}"
 MAGE_TARGET="${2:-"integration:test"}"
 MAGE_SUBTARGET="${3:-""}"
 
-
 # Override the stack version from `.package-version` contents
 # There is a time when the current snapshot is not available on cloud yet, so we cannot use the latest version automatically
 # This file is managed by an automation (mage integration:UpdateAgentPackageVersion) that check if the snapshot is ready.
 
 STACK_VERSION="$(cat .package-version)"
 if [[ -n "$STACK_VERSION" ]]; then
-    STACK_VERSION=${STACK_VERSION}"-SNAPSHOT"
+	STACK_VERSION=${STACK_VERSION}"-SNAPSHOT"
 fi
 
 # Run integration tests
 set +e
-AGENT_STACK_VERSION="${STACK_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true  STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
+AGENT_STACK_VERSION="${STACK_VERSION}" TEST_INTEG_CLEAN_ON_EXIT=true TEST_PACKAGES="rpm" TEST_GROUPS="rpm" STACK_PROVISIONER="$STACK_PROVISIONER" SNAPSHOT=true mage $MAGE_TARGET $MAGE_SUBTARGET
 TESTS_EXIT_STATUS=$?
 set -e
 
@@ -27,10 +26,10 @@ set -e
 outputXML="build/TEST-go-integration.xml"
 
 if [ -f "$outputXML" ]; then
-  go install github.com/alexec/junit2html@latest
-  junit2html < "$outputXML" > build/TEST-report.html
+	go install github.com/alexec/junit2html@latest
+	junit2html <"$outputXML" >build/TEST-report.html
 else
-    echo "Cannot generate HTML test report: $outputXML not found"
+	echo "Cannot generate HTML test report: $outputXML not found"
 fi
 
 exit $TESTS_EXIT_STATUS
