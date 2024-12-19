3698258e2a29c5c5b152f631248548c32752d5a6
Olga Naydyonock <olga.naidjonoka@elastic.co>
2024-12-19 12:31:16 +0200

    updated script and pipelines

--- .buildkite/scripts/steps/integration-package.sh
+++ .buildkite/scripts/steps/integration-package.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 source .buildkite/scripts/common.sh
 
 PACKAGES=tar.gz,zip,rpm,deb PLATFORMS=linux/amd64,linux/arm64,windows/amd64  SNAPSHOT=true EXTERNAL=true  mage package
+
+#PACKAGES="${PACKAGES}" PLATFORMS="${PLATFORMS}" SNAPSHOT="${SNAPSHOT}" EXTERNAL="${EXTERNAL}" DEV="${DEV}" mage package
