86dd799eb9587003d2d70278d7f78fe8a40b6667
Olga Naydyonock <olga.naidjonoka@elastic.co>
2024-12-19 20:30:05 +0200

    test

--- .buildkite/scripts/steps/integration-package.sh
+++ .buildkite/scripts/steps/integration-package.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Set required env vars
 source .buildkite/scripts/common.sh
 
-PACKAGES=tar.gz,zip,rpm,deb PLATFORMS=linux/amd64,linux/arm64,windows/amd64  SNAPSHOT=true EXTERNAL=true  mage package
-
-#PACKAGES="${PACKAGES}" PLATFORMS="${PLATFORMS}" SNAPSHOT="${SNAPSHOT}" EXTERNAL="${EXTERNAL}" DEV="${DEV}" mage package
+mage package
