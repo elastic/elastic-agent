5714904312c61d725554c5978562e71305c84189
kaanyalti <kaan.yalti@elastic.co>
2024-12-26 22:21:43 -0500

    fixs(6023): ran mage update

--- testing/integration/validate_items_deprecated_list_test.go
+++ testing/integration/validate_items_deprecated_list_test.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 package integration
 
 import (
@@ -6,8 +10,9 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/elastic/elastic-agent/pkg/testing/define"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent/pkg/testing/define"
 )
 
 type testKibanaApiKey struct {
