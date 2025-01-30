f266c8b839e25b41d0a6764cc0111b64cb873a2a
Pavel Zorin <pavel.zorin@elastic.co>
2025-01-30 19:54:01 +0100

    Use LASTEXITCODE to catch test failures

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -34,13 +34,12 @@ if ($TEST_SUDO -eq "true") {
 $fully_qualified_group_name="${GROUP_NAME}${root_suffix}_${osInfo}"
 $outputXML = "build/${fully_qualified_group_name}.integration.xml"
 $outputJSON = "build/${fully_qualified_group_name}.integration.out.json"
+$TestsExitCode = 0
 try {
     Get-Ess-Stack -StackVersion $PACKAGE_VERSION
     Write-Output "~~~ Running integration test group: $GROUP_NAME as user: $env:USERNAME"
     gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
-} catch {
-    Write-Output "There are test failures: $_"
-    throw
+    $TestsExitCode = $LASTEXITCODE    
 } finally {
     ess_down
     
@@ -51,4 +50,8 @@ try {
     } else {
         Write-Output "Cannot generate HTML test report: $outputXML not found"
     }
+}
+
+if ($TestsExitCode -ne 0) {    
+    exit 1
 }
