714f450289f2cc4a3f0c547696cdbea8a0c287d4
Pavel Zorin <pavel.zorin@elastic.co>
2025-01-30 17:08:11 +0100

    Integration tests: Parse agent version from version.go (#6611)
    * Integration tests: use the mage target to get agent version
    
    * Added snapshot to version
    
    * Test run
    
    * Test run Windows
    
    * Removed -v flag
    
    * Parse agent version
    
    * Cleanup
    
    (cherry picked from commit cab384c5735ddfa9e4d66d6c551b1486c34a4cb4)
    
    # Conflicts:
    #	.buildkite/scripts/integration-tests.ps1

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -17,16 +17,23 @@ if ($PACKAGE_VERSION) {
     $PACKAGE_VERSION = "${PACKAGE_VERSION}-SNAPSHOT"
 }
 $env:TEST_BINARY_NAME = "elastic-agent"
-$env:AGENT_VERSION = $PACKAGE_VERSION
+# Parsing version.go. Will be simplified here: https://github.com/elastic/ingest-dev/issues/4925
+$AGENT_VERSION = (Get-Content version/version.go | Select-String -Pattern 'const defaultBeatVersion =' | ForEach-Object { $_ -replace '.*?"(.*?)".*', '$1' })
+$env:AGENT_VERSION = $AGENT_VERSION + "-SNAPSHOT"
+echo "~~~ Agent version: $env:AGENT_VERSION"
 $env:SNAPSHOT = $true
 
 echo "~~~ Building test binaries"
 mage build:testBinaries
 
 try {
-    Get-Ess-Stack -StackVersion $PACKAGE_VERSION    
+    Get-Ess-Stack -StackVersion $PACKAGE_VERSION
     Write-Output "~~~ Running integration test group: $GROUP_NAME as user: $env:USERNAME"
+<<<<<<< HEAD
     gotestsum --no-color -f standard-quiet --junitfile "build/${GROUP_NAME}.integration.xml" --jsonfile "build/${GROUP_NAME}.integration.out.json" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=true"    
+=======
+    gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
+>>>>>>> cab384c57 (Integration tests: Parse agent version from version.go (#6611))
 } finally {
     ess_down
     # Generate HTML report if XML output exists
