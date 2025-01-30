0b0c3bcdba00b36182cf36d1665b2d594c88667d
Pavel Zorin <pavel.zorin@elastic.co>
2025-01-30 20:15:13 +0100

    Update .buildkite/scripts/integration-tests.ps1
    Co-authored-by: Doug W <55553826+dwhyrock@users.noreply.github.com>

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -38,7 +38,7 @@ $TestsExitCode = 0
 try {
     Get-Ess-Stack -StackVersion $PACKAGE_VERSION
     Write-Output "~~~ Running integration test group: $GROUP_NAME as user: $env:USERNAME"
-    gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
+    & gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
     $TestsExitCode = $LASTEXITCODE    
 } finally {
     ess_down
