724623ac3a684bdfeeae37907cc465ede02e971c
Pavel Zorin <pavel.zorin@elastic.co>
2025-02-03 09:11:40 +0100

    [CI] Fix throttled windows test failures (#6662)
    * CI: Fifx throttled windows test failures
    
    * Use LASTEXITCODE to catch test failures
    
    * Update .buildkite/scripts/integration-tests.ps1
    
    Co-authored-by: Doug W <55553826+dwhyrock@users.noreply.github.com>
    
    * Added  check to buildTestBinaries
    
    * Apply suggestions from code review
    
    Co-authored-by: Doug W <55553826+dwhyrock@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Doug W <55553826+dwhyrock@users.noreply.github.com>
    Co-authored-by: Julien Lind <julien.lind@elastic.co>
    (cherry picked from commit 87360e6980a17fef106ab1014f75924c4037f982)
    
    # Conflicts:
    #	.buildkite/scripts/integration-tests.ps1

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -22,7 +22,11 @@ $env:AGENT_VERSION = $PACKAGE_VERSION
 $env:SNAPSHOT = $true
 
 echo "~~~ Building test binaries"
-mage build:testBinaries
+& mage build:testBinaries
+if ($LASTEXITCODE -ne 0) {    
+    Write-Error "Failed to build test binaries"
+    exit 1
+}
 $osInfo = (Get-CimInstance Win32_OperatingSystem).Caption + " " + (Get-CimInstance Win32_OperatingSystem).OSArchitecture -replace " ", "_"
 $root_suffix=""
 if ($TEST_SUDO -eq "true") {
@@ -31,10 +35,16 @@ if ($TEST_SUDO -eq "true") {
 $fully_qualified_group_name="${GROUP_NAME}${root_suffix}_${osInfo}"
 $outputXML = "build/${fully_qualified_group_name}.integration.xml"
 $outputJSON = "build/${fully_qualified_group_name}.integration.out.json"
+$TestsExitCode = 0
 try {
     Get-Ess-Stack -StackVersion $PACKAGE_VERSION    
     Write-Output "~~~ Running integration test group: $GROUP_NAME as user: $env:USERNAME"
+<<<<<<< HEAD
     gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO" 
+=======
+    & gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
+    $TestsExitCode = $LASTEXITCODE    
+>>>>>>> 87360e698 ([CI] Fix throttled windows test failures (#6662))
 } finally {
     ess_down
     
@@ -45,4 +55,8 @@ try {
     } else {
         Write-Output "Cannot generate HTML test report: $outputXML not found"
     }
+}
+
+if ($TestsExitCode -ne 0) {    
+    exit 1
 }
