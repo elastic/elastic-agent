522279504a3b722dae334b4f9d1e925b9cb2682d
Pierre HILBERT <pierre.hilbert@elastic.co>
2025-02-07 14:44:50 +0100

    Update integration-package.sh

--- .buildkite/scripts/steps/integration-package.sh
+++ .buildkite/scripts/steps/integration-package.sh
@@ -3,9 +3,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
-<<<<<<< HEAD
-PACKAGES=tar.gz,zip,rpm,deb PLATFORMS=linux/amd64,linux/arm64,windows/amd64  SNAPSHOT=true EXTERNAL=true  mage package
-=======
 # Remove AGENT_PACKAGE_VERSION pinning as soon as 9.0.0 is released
 export AGENT_PACKAGE_VERSION=9.0.0
 
@@ -13,4 +10,3 @@ export SNAPSHOT="true"
 export EXTERNAL="true"
 
 mage package
->>>>>>> e956b4d2d (Split packaging into separate steps (#6401))
