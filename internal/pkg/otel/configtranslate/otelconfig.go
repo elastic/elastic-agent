07ff2503d0632dfe7bab72d03b96c6efa87671e5
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-02-18 18:21:36 +0100

    Code Review fixes

--- internal/pkg/otel/configtranslate/otelconfig.go
+++ internal/pkg/otel/configtranslate/otelconfig.go
@@ -204,7 +204,7 @@ func getExportersConfigForComponent(comp *component.Component) (map[string]any, 
 	}
 	for _, unit := range comp.Units {
 		if unit.Type == client.UnitTypeOutput {
-			unitExportersConfig, expErr := unitToExporterConfig(unit, exporterType)
+			unitExportersConfig, expErr := unitToExporterConfig(unit, exporterType, comp.InputType)
 			if expErr != nil {
 				return nil, expErr
 			}
@@ -261,7 +261,7 @@ func getExporterTypeForComponent(comp *component.Component) (otelcomponent.Type,
 }
 
 // unitToExporterConfig translates a component.Unit to an otel exporter configuration.
-func unitToExporterConfig(unit component.Unit, exporterType otelcomponent.Type) (map[string]any, error) {
+func unitToExporterConfig(unit component.Unit, exporterType otelcomponent.Type, inputType string) (map[string]any, error) {
 	if unit.Type == client.UnitTypeInput {
 		return nil, fmt.Errorf("unit type is an input, expected output: %v", unit)
 	}
