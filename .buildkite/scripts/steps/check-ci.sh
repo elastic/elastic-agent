0f82bec9e02672f5432391a42ac057acbfb00efb
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-02-25 10:39:21 +0100

    fix: grep is not working correctly

--- .buildkite/scripts/steps/check-ci.sh
+++ .buildkite/scripts/steps/check-ci.sh
@@ -4,6 +4,13 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
+set +e
+set +x
+echo "--- Check grep"
+grep -oE '[0-9]+\.[0-9]+\.[0-9]+(\-[a-zA-Z]+[0-9]+)?' "version/version.go"
+set -e
+set -x
+
 echo "--- Check CI"
 go version
 mage --version
