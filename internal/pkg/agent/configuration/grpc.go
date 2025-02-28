9bae402c4eee4565199d69cb2f89083c465edfe5
kruskall <99559985+kruskall@users.noreply.github.com>
2025-02-28 17:00:54 +0100

    fix(ipv6): support ipv6 address in grpc config (#7035)
    * fix(ipv6): support ipv6 address in grpc config
    
    joining addr port using string concat only works with ipv4
    
    stdlib has funcs in the net pkg to properly support ipv6
    
    * changelog: add fragment
    
    ---------
    
    Co-authored-by: Blake Rouse <blake.rouse@elastic.co>
    (cherry picked from commit 5d91620b22cde971b49bc8e6a0641de8e723e3b9)
    
    # Conflicts:
    #	internal/pkg/agent/configuration/grpc.go
    #	internal/pkg/agent/configuration/grpc_test.go

--- internal/pkg/agent/configuration/grpc.go
+++ internal/pkg/agent/configuration/grpc.go
@@ -5,7 +5,13 @@
 package configuration
 
 import (
+<<<<<<< HEAD
 	"fmt"
+=======
+	"net"
+	"os"
+	"strconv"
+>>>>>>> 5d91620b2 (fix(ipv6): support ipv6 address in grpc config (#7035))
 
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 )
@@ -43,7 +49,7 @@ func DefaultGRPCConfig() *GRPCConfig {
 
 // String returns the composed listen address for the GRPC.
 func (cfg *GRPCConfig) String() string {
-	return fmt.Sprintf("%s:%d", cfg.Address, cfg.Port)
+	return net.JoinHostPort(cfg.Address, strconv.Itoa(int(cfg.Port)))
 }
 
 // IsLocal returns true if port value is less than 0
