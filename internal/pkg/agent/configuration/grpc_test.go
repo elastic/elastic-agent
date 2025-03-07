0625d79f560cde979e99b95cdb67be130573ef47
kruskall <99559985+kruskall@users.noreply.github.com>
2025-03-07 10:41:41 +0100

    Update grpc_test.go

--- internal/pkg/agent/configuration/grpc_test.go
+++ internal/pkg/agent/configuration/grpc_test.go
@@ -5,43 +5,10 @@
 package configuration
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestOverrideDefaultGRPCPort(t *testing.T) {
-	testcases := []struct {
-		name     string
-		env      string
-		expected uint16
-	}{{
-		name:     "no env var",
-		env:      "",
-		expected: DefaultGPRCPortInContainer,
-	}, {
-		name:     "valid env var",
-		env:      "1234",
-		expected: 1234,
-	}, {
-		name:     "invalid env var",
-		env:      "not a number",
-		expected: DefaultGPRCPortInContainer,
-	}}
-
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg := GRPCConfig{}
-			if tc.env != "" {
-				os.Setenv(grpcPortContainerEnvVar, tc.env)
-				defer os.Unsetenv(grpcPortContainerEnvVar)
-			}
-			OverrideDefaultContainerGRPCPort(&cfg)
-			assert.Equal(t, tc.expected, cfg.Port)
-		})
-	}
-}
 
 func TestGRPCAddr(t *testing.T) {
 	testcases := []struct {
