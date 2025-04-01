f7df54c063c05dd4e3311d674daa8ecd08e1a9cd
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-04-01 22:42:59 +0200

    use adifferent approach

--- .buildkite/scripts/install-asdf.sh
+++ .buildkite/scripts/install-asdf.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-ASDF_DIR=/opt/buildkite-agent/.asdf
+ASDF_DIR=~/.asdf
 ASDF_VERSION="v0.14.0"
 
 # Installation instructions from https://asdf-vm.com/guide/getting-started.html
-if [ ! -d "${ASDF_DIR}" ]; then
+if ! command -v asdf ; then
     echo "--- Install asdf"
     git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch ${ASDF_VERSION}
-    echo 'source $ASDF_DIR/asdf.sh' >> $AGENT_HOME/.bashrc
+    echo "source $ASDF_DIR/asdf.sh" >> ~/.bashrc
     source $ASDF_DIR/asdf.sh
     asdf plugin update --all
 fi
