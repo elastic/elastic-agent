0b0b16be37b460123ad95c0b7789cfcd8f2c6382
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-04-07 18:47:59 +0300

    fix: utilise clientsetscheme for registering all the builtin resources

--- pkg/testing/kubernetes/loader.go
+++ pkg/testing/kubernetes/loader.go
@@ -9,15 +9,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
-
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	schedulingv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/e2e-framework/klient/k8s"
 
 	"github.com/elastic/cloud-on-k8s/v2/pkg/apis/agent/v1alpha1"
@@ -27,18 +23,10 @@ import (
 func LoadFromYAML(reader *bufio.Reader) ([]k8s.Object, error) {
 	// if we need to encode/decode more k8s object types in our tests, add them here
 	k8sScheme := runtime.NewScheme()
-	k8sScheme.AddKnownTypes(rbacv1.SchemeGroupVersion, &rbacv1.ClusterRoleBinding{}, &rbacv1.ClusterRoleBindingList{})
-	k8sScheme.AddKnownTypes(rbacv1.SchemeGroupVersion, &rbacv1.ClusterRole{}, &rbacv1.ClusterRoleList{})
-	k8sScheme.AddKnownTypes(rbacv1.SchemeGroupVersion, &rbacv1.RoleBinding{}, &rbacv1.RoleBindingList{})
-	k8sScheme.AddKnownTypes(rbacv1.SchemeGroupVersion, &rbacv1.Role{}, &rbacv1.RoleList{})
-	k8sScheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.ServiceAccount{}, &corev1.ServiceAccountList{})
-	k8sScheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.Pod{}, &corev1.PodList{})
-	k8sScheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.Service{}, &corev1.ServiceList{})
-	k8sScheme.AddKnownTypes(appsv1.SchemeGroupVersion, &appsv1.DaemonSet{})
-	k8sScheme.AddKnownTypes(appsv1.SchemeGroupVersion, &appsv1.StatefulSet{})
-	k8sScheme.AddKnownTypes(appsv1.SchemeGroupVersion, &appsv1.Deployment{})
-	k8sScheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.Secret{}, &corev1.ConfigMap{})
-	k8sScheme.AddKnownTypes(schedulingv1.SchemeGroupVersion, &schedulingv1.PriorityClass{})
+	err := clientsetscheme.AddToScheme(k8sScheme)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add clientsetscheme: %w", err)
+	}
 	k8sScheme.AddKnownTypes(schema.GroupVersion{Group: "agent.k8s.elastic.co", Version: "v1alpha1"}, &v1alpha1.Agent{})
 
 	var objects []k8s.Object
