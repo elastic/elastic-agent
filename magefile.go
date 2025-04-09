e06e980241b693c8d73322f7a87c4b8f44619d3e
Shaunak Kashyap <ycombinator@gmail.com>
2025-04-08 17:20:25 -0700

    Updating dependencies of mage update target

--- magefile.go
+++ magefile.go
@@ -705,7 +705,7 @@ func commitID() string {
 
 // Update is an alias for executing control protocol, configs, and specs.
 func Update() {
-	mg.Deps(Config, BuildPGP, BuildFleetCfg, Otel.Readme)
+	mg.Deps(Config, BuildPGP, BuildFleetCfg, ControlProto, Otel.Readme, devtools.AddLicenseHeaders, devtools.GoImports)
 }
 
 func EnsureCrossBuildOutputDir() error {
