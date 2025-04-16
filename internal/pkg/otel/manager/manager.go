6b208640ee415e8d866d0139f2fe3b94ea2b9094
Khushi Jain <khushi.jain@elastic.co>
2025-04-16 11:32:36 +0530

    [Chore] Improve error reporting when otel collector fails to start

--- internal/pkg/otel/manager/manager.go
+++ internal/pkg/otel/manager/manager.go
@@ -7,6 +7,7 @@ package manager
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/status"
 	"go.opentelemetry.io/collector/confmap"
@@ -81,7 +82,7 @@ func (m *OTelManager) Run(ctx context.Context) error {
 				if err != nil {
 					// failed to create the collector (this is different then
 					// it's failing to run). we do not retry creation on failure
-					// as it will always fail a new configuration is required for
+					// as it will always fail. A new configuration is required for
 					// it not to fail (a new configuration will result in the retry)
 					m.reportErr(ctx, err)
 				} else {
@@ -108,6 +109,7 @@ func (m *OTelManager) Run(ctx context.Context) error {
 				}
 				// pass the error to the errCh so the coordinator, unless it's a cancel error
 				if !errors.Is(err, context.Canceled) {
+					m.logger.Errorf(fmt.Sprintf("Failed to start the collector: %w", err))
 					m.reportErr(ctx, err)
 				}
 			}
@@ -143,7 +145,7 @@ func (m *OTelManager) Run(ctx context.Context) error {
 					if err != nil {
 						// failed to create the collector (this is different then
 						// it's failing to run). we do not retry creation on failure
-						// as it will always fail a new configuration is required for
+						// as it will always fail. A new configuration is required for
 						// it not to fail (a new configuration will result in the retry)
 						m.reportErr(ctx, err)
 					} else {
