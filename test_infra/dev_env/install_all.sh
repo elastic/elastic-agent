40385f090ac601ed6318718f6fe99f93436974d8
Pavel Zorin <pavel.zorin@elastic.co>
2025-04-17 17:17:20 +0200

    Flatten install_all.sh

--- test_infra/dev_env/install_all.sh
+++ test_infra/dev_env/install_all.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source test_infra/dev_env/mise_tools.sh
+#
+# Installs mise and tools listed in .tool-versions
+#
+MISE_VERSION="v2025.4.4" 
+if ! command -v mise &> /dev/null; then
+  # Install mise. See installation guide: https://mise.jdx.dev/installing-mise.html
+  curl https://mise.run | sh
+  # Add default mise installation to PATH
+  export PATH="$HOME/.local/bin:$PATH"
+fi 
+
+# Install tools from .tool-versions
+mise install 
+# Add mise activation to .bashrc (makes tools available directly)
+echo 'eval "$(~/mise activate bash)"' >> ~/.bashrc
+# Activate mise for current session
+eval "$(mise activate bash --shims)"
---
+++ test_infra/dev_env/readme.md
@@ -0,0 +1,15 @@
+### Cross-platform dev environment
+Installs all required tools with required versions with mise
+
+#### Usage
+
+Linux
+```
+source test_infra/dev_env/install_all.sh
+```
+
+Windows:
+(TBD)
+```
+test_infra/dev_env/install_all.ps1
+```
