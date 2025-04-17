47d8c0db19aab4ab22b693448892f4010459f1db
Shaunak Kashyap <ycombinator@gmail.com>
2025-04-17 15:31:51 -0700

    Add test for RSA keypair with < 2048 key length

--- internal/pkg/remote/client_fips_test.go
+++ internal/pkg/remote/client_fips_test.go
@@ -7,7 +7,14 @@
 package remote
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,6 +27,8 @@ import (
 func TestClientWithUnsupportedTLSConfig(t *testing.T) {
 	testLogger, _ := loggertest.New("TestClientWithUnsupportedTLSVersions")
 	const unsupportedErrorMsg = "invalid configuration: unsupported tls version: %s"
+
+	privateKeyPEM, certificatePEM := makeRSA1024KeyCertPair(t)
 
 	cases := map[string]struct {
 		tlsConfig      tlscommon.Config
@@ -51,7 +60,20 @@ func TestClientWithUnsupportedTLSConfig(t *testing.T) {
 			versions:       []tlscommon.TLSVersion{tlscommon.TLSVersion11, tlscommon.TLSVersion12},
 			expectedErrMsg: fmt.Sprintf(unsupportedErrorMsg, tlscommon.TLSVersion11),
 		},
+		"rsa_1024": {
+			tlsConfig: tlscommon.Config{Certificate: tlscommon.CertificateConfig{
+				Certificate: certificatePEM,
+				Key:         privateKeyPEM,
+			}},
+		},
 	}
+
+	/* TODO: move block to elastic-agent-libs/transport/tlscommon */
+	cert, err := tls.X509KeyPair([]byte(certificatePEM), []byte(privateKeyPEM))
+	require.NoError(t, err)
+	privateKey := cert.PrivateKey.(*rsa.PrivateKey)
+	t.Log(privateKey.N.BitLen())
+	/* end block */
 
 	for name, test := range cases {
 		t.Run(name, func(t *testing.T) {
@@ -73,4 +95,46 @@ func TestClientWithUnsupportedTLSConfig(t *testing.T) {
 			}
 		})
 	}
+}
+
+// makeRSA1024KeyCertPair returns a private key and certificate encoded
+// in PEM format, generated from an RSA 1024-bit keypair.
+func makeRSA1024KeyCertPair(t *testing.T) (string, string) {
+	// Generate RSA keypair
+	keyBytes, err := rsa.GenerateKey(rand.Reader, 1024)
+	require.NoError(t, err)
+
+	err = keyBytes.Validate()
+	require.NoError(t, err)
+
+	// Generate certificate from keypair
+	tpl := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Country:      []string{"US"},
+			Organization: []string{"Elastic"},
+			Province:     []string{"CA"},
+			CommonName:   "CN",
+		},
+	}
+
+	cert, err := x509.CreateCertificate(rand.Reader, &tpl, &tpl, &keyBytes.PublicKey, keyBytes)
+	require.NoError(t, err)
+
+	// Convert private key to PEM format
+	b, err := x509.MarshalPKCS8PrivateKey(keyBytes)
+	require.NoError(t, err)
+
+	keyPem := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: b,
+	})
+
+	// Convert certificate to PEM format
+	certPem := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert,
+	})
+
+	return string(keyPem), string(certPem)
 }
