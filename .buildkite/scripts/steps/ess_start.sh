68faba0747c92999adacf8bc75524e09c468a2f8
Pavel Zorin <pavel.zorin@elastic.co>
2025-04-22 16:34:01 +0200

    Returned ess_start.sh back

--- .buildkite/scripts/steps/ess_start.sh
+++ .buildkite/scripts/steps/ess_start.sh
@@ -9,5 +9,17 @@ echo "~~~ Getting stable stack version"
 DEFAULT_STACK_VERSION="$(cat .package-version)-SNAPSHOT"
 STABLE_ESS_VERSION="$(getStableEssSnapshotForBranch)-SNAPSHOT"
 ess_up $DEFAULT_STACK_VERSION $STABLE_STACK_VERSION
+echo "ES_HOST: ${ELASTICSEARCH_HOST}"
 
 preinstall_fleet_packages
+
+if [ "${CI:=false}" == 'true' ]; then  
+  buildkite-agent meta-data set "es.host" $ELASTICSEARCH_HOST
+  buildkite-agent meta-data set "es.username" $ELASTICSEARCH_USERNAME
+  buildkite-agent meta-data set "es.pwd" $ELASTICSEARCH_PASSWORD
+  buildkite-agent meta-data set "kibana.host" $KIBANA_HOST
+  buildkite-agent meta-data set "kibana.username" $KIBANA_USERNAME
+  buildkite-agent meta-data set "kibana.pwd" $KIBANA_PASSWORD
+
+  buildkite-agent meta-data set "stable.ess.version" $STABLE_ESS_VERSION
+fi
