064d0c8ced0f2177e255ca7d7e09690cb432e681
Shaunak Kashyap <ycombinator@gmail.com>
2025-04-24 15:27:43 -0700

    Introduce GoDebugFIPS140() function

--- internal/pkg/testutils/fipsutils/fipsOnlySkip.go
+++ internal/pkg/testutils/fipsutils/fipsOnlySkip.go
@@ -10,13 +10,34 @@ import (
 	"testing"
 )
 
+type GoDebugFIPS140Value string
+
+const (
+	GoDebugFIPS140NotSet GoDebugFIPS140Value = ""
+	GoDebugFIPS140On                         = "on"
+	GoDebugFIPS140Only                       = "only"
+)
+
 // SkipIfFIPSOnly will mark the passed test as skipped if GODEBUG=fips140=only is detected.
 // If GODBUG=fips140=on, go may call non-compliant algorithms and the test does not need to be skipped.
 func SkipIfFIPSOnly(t *testing.T, msg string) {
 	// NOTE: This only checks env var; at the time of writing fips140 can only be set via env
 	// other GODEBUG settings can be set via embedded comments or in go.mod, we may need to account for this in the future.
-	s := os.Getenv("GODEBUG")
-	if strings.Contains(s, "fips140=only") {
+	if GoDebugFIPS140() == GoDebugFIPS140Only {
 		t.Skip("GODEBUG=fips140=only detected, skipping test:", msg)
 	}
+}
+
+// GoDebugFIPS140 returns one of "on", "only", or "" depending on
+// whether the GODEBUG environment variable contains fips140=on or
+// fips140=only, or neither.
+func GoDebugFIPS140() GoDebugFIPS140Value {
+	s := os.Getenv("GODEBUG")
+	if strings.Contains(s, "fips140=only") {
+		return GoDebugFIPS140Only
+	}
+	if strings.Contains(s, "fips140=on") {
+		return GoDebugFIPS140On
+	}
+	return GoDebugFIPS140NotSet
 }
