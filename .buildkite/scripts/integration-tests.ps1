8ba445e40480940d743af268cc364cf7459dabca
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-04-25 10:44:37 +0300

    fix: resolve conflicts

--- .buildkite/scripts/integration-tests.ps1
+++ .buildkite/scripts/integration-tests.ps1
@@ -31,15 +31,11 @@ echo "~~~ Agent version: $env:AGENT_VERSION"
 $env:SNAPSHOT = $true
 
 echo "~~~ Building test binaries"
-<<<<<<< HEAD
-mage build:testBinaries
-=======
 & mage build:testBinaries
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Failed to build test binaries"
     exit 1
 }
->>>>>>> 2a274c2c9 ([ci] migrate serverless integration tests to buildkite (#7919))
 $osInfo = (Get-CimInstance Win32_OperatingSystem).Caption + " " + (Get-CimInstance Win32_OperatingSystem).OSArchitecture -replace " ", "_"
 $root_suffix=""
 if ($TEST_SUDO -eq "true") {
@@ -48,6 +44,7 @@ if ($TEST_SUDO -eq "true") {
 $fully_qualified_group_name="${GROUP_NAME}${root_suffix}_${osInfo}"
 $outputXML = "build/${fully_qualified_group_name}.integration.xml"
 $outputJSON = "build/${fully_qualified_group_name}.integration.out.json"
+$TestsExitCode = 0
 try {
     if ($STACK_TYPE -eq "ess") {
         Get-Ess-Stack -StackVersion $PACKAGE_VERSION
@@ -55,9 +52,6 @@ try {
         Get-Serverless-Project
     }
     Write-Output "~~~ Running integration test group: $GROUP_NAME as user: $env:USERNAME"
-<<<<<<< HEAD
-    gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- -tags=integration -shuffle=on -timeout=2h0m0s "github.com/elastic/elastic-agent/testing/integration" -v -args "-integration.groups=$GROUP_NAME" "-integration.sudo=$TEST_SUDO"
-=======
     $gotestArgs = @("-tags=integration", "-shuffle=on", "-timeout=2h0m0s")
     if ($TEST_NAME_PATTERN -ne "") {
         $gotestArgs += "-run=${TEST_NAME_PATTERN}"
@@ -65,7 +59,6 @@ try {
     $gotestArgs += @("github.com/elastic/elastic-agent/testing/integration", "-v", "-args", "-integration.groups=$GROUP_NAME", "-integration.sudo=$TEST_SUDO")
     & gotestsum --no-color -f standard-quiet --junitfile "${outputXML}" --jsonfile "${outputJSON}" -- @gotestArgs
     $TestsExitCode = $LASTEXITCODE
->>>>>>> 2a274c2c9 ([ci] migrate serverless integration tests to buildkite (#7919))
 } finally {
     if ($STACK_TYPE -eq "ess") {
         ess_down
@@ -81,10 +74,7 @@ try {
         Write-Output "Cannot generate HTML test report: $outputXML not found"
     }
 }
-<<<<<<< HEAD
-=======
 
 if ($TestsExitCode -ne 0) {
     exit 1
 }
->>>>>>> 2a274c2c9 ([ci] migrate serverless integration tests to buildkite (#7919))
