bdc86fcae95baa42998373c955cadd75dea9dbd7
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2025-05-07 13:45:50 -0600

    Retry enrollment requests when an error is returned, add enrollment timeout (#8056)
    Retry enrollment requests when an error is returned until a timeout is reached.
    Add --enroll-timeout and FLEET_ENROLL_TIMEOUT to control how long the timeout is; default 10m.
    A negative value disables the timeout.
    
    (cherry picked from commit b201e16a0f56d2937d509372ae60e401849bb629)

--- internal/pkg/agent/cmd/enroll_cmd.go
+++ internal/pkg/agent/cmd/enroll_cmd.go
@@ -543,25 +543,23 @@ func (c *enrollCmd) enrollWithBackoff(ctx context.Context, persistentConfig map[
 	defer close(signal)
 	backExp := c.backoffFactory(signal)
 
+RETRYLOOP:
 	for {
-		retry := false
 		switch {
 		case errors.Is(err, fleetapi.ErrTooManyRequests):
 			c.log.Warn("Too many requests on the remote server, will retry in a moment.")
-			retry = true
 		case errors.Is(err, fleetapi.ErrConnRefused):
 			c.log.Warn("Remote server is not ready to accept connections(Connection Refused), will retry in a moment.")
-			retry = true
 		case errors.Is(err, fleetapi.ErrTemporaryServerError):
 			c.log.Warnf("Remote server failed to handle the request(%s), will retry in a moment.", err.Error())
-			retry = true
+		case errors.Is(err, context.Canceled), errors.Is(err, context.DeadlineExceeded), err == nil:
+			break RETRYLOOP
 		case err != nil:
-			c.log.Warnf("Enrollment failed: %s", err.Error())
+			c.log.Warnf("Error detected: %s, will retry in a moment.", err.Error())
 		}
-		if !retry {
-			break
+		if !backExp.Wait() {
+			break RETRYLOOP
 		}
-		backExp.Wait()
 		c.log.Infof("Retrying enrollment to URL: %s", c.client.URI())
 		err = c.enroll(ctx, persistentConfig)
 	}
@@ -597,9 +595,7 @@ func (c *enrollCmd) enroll(ctx context.Context, persistentConfig map[string]inte
 
 	resp, err := cmd.Execute(ctx, r)
 	if err != nil {
-		return errors.New(err,
-			"fail to execute request to fleet-server",
-			errors.TypeNetwork)
+		return fmt.Errorf("failed to execute request to fleet-server: %w", err)
 	}
 
 	fleetConfig, err := createFleetConfigFromEnroll(resp.Item.AccessAPIKey, c.options.EnrollAPIKey, c.options.ReplaceToken, c.remoteConfig)
