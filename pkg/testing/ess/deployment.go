5c5f20dc40d6e56e1eeb25eb778de15d4eec5f17
Shaunak Kashyap <ycombinator@gmail.com>
2025-05-21 05:01:47 -0700

    WIP: Implementing UpgradeDeployment method

--- pkg/testing/ess/deployment.go
+++ pkg/testing/ess/deployment.go
@@ -18,6 +18,8 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/elastic/elastic-agent/pkg/version"
+
 	"gopkg.in/yaml.v2"
 )
 
@@ -190,6 +192,468 @@ func (c *Client) CreateDeployment(ctx context.Context, req CreateDeploymentReque
 	r.IntegrationsServerEndpoint = getRespBody.Resources.IntegrationsServer[0].Info.Metadata.ServiceUrl
 
 	return &r, nil
+}
+
+// UpgradeDeployment upgrades the specified deployment to the specified version.
+func (c *Client) UpgradeDeployment(ctx context.Context, deploymentID string, version *version.ParsedSemVer) error {
+	u, err := url.JoinPath("deployments", deploymentID)
+	if err != nil {
+		return fmt.Errorf("unable to create deployment update API URL: %w", err)
+	}
+
+	// See also: https://www.elastic.co/docs/api/doc/cloud/operation/operation-update-deployment
+	// - Only elements that change (version?) could be specified?
+	// See also: create_deployment_request.tmpl.json
+	//{
+	//  "name": "test-8181",
+	//  "alias": "test-8181",
+	//  "prune_orphans": true,
+	//  "metadata": {
+	//    "system_owned": false,
+	//    "hidden": false
+	//  },
+	//  "settings": {
+	//    "autoscaling_enabled": false
+	//  },
+	//  "resources": {
+	//    "elasticsearch": [
+	//      {
+	//        "region": "gcp-us-west2",
+	//        "ref_id": "main-elasticsearch",
+	//        "plan": {
+	//          "tiebreaker_topology": {
+	//            "memory_per_node": 1024
+	//          },
+	//          "cluster_topology": [
+	//            {
+	//              "id": "hot_content",
+	//              "node_roles": [
+	//                "master",
+	//                "ingest",
+	//                "transform",
+	//                "data_hot",
+	//                "remote_cluster_client",
+	//                "data_content"
+	//              ],
+	//              "zone_count": 2,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                },
+	//                "node_attributes": {
+	//                  "data": "hot"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.datahot.n2.68x10x45",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 8192,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_max": {
+	//                "value": 131072,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "warm",
+	//              "node_roles": [
+	//                "data_warm",
+	//                "remote_cluster_client"
+	//              ],
+	//              "zone_count": 2,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                },
+	//                "node_attributes": {
+	//                  "data": "warm"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.datawarm.n2.68x10x190",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_max": {
+	//                "value": 131072,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "cold",
+	//              "node_roles": [
+	//                "data_cold",
+	//                "remote_cluster_client"
+	//              ],
+	//              "zone_count": 1,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                },
+	//                "node_attributes": {
+	//                  "data": "cold"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.datacold.n2.68x10x190",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_max": {
+	//                "value": 65536,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "frozen",
+	//              "node_roles": [
+	//                "data_frozen"
+	//              ],
+	//              "zone_count": 1,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                },
+	//                "node_attributes": {
+	//                  "data": "frozen"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.datafrozen.n2.68x10x90",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_max": {
+	//                "value": 131072,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "master",
+	//              "node_roles": [
+	//                "master",
+	//                "remote_cluster_client"
+	//              ],
+	//              "zone_count": 3,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.master.n2.68x32x45",
+	//              "instance_configuration_version": 2,
+	//              "size": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "coordinating",
+	//              "node_roles": [
+	//                "ingest",
+	//                "remote_cluster_client"
+	//              ],
+	//              "zone_count": 2,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.coordinating.n2.68x16x45",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              }
+	//            },
+	//            {
+	//              "id": "ml",
+	//              "node_roles": [
+	//                "ml",
+	//                "remote_cluster_client"
+	//              ],
+	//              "zone_count": 1,
+	//              "elasticsearch": {
+	//                "system_settings": {
+	//                  "scripting": {
+	//                    "stored": {
+	//                      "enabled": true
+	//                    },
+	//                    "inline": {
+	//                      "enabled": true
+	//                    }
+	//                  },
+	//                  "http": {
+	//                    "compression": true,
+	//                    "cors_enabled": false,
+	//                    "cors_max_age": 1728000,
+	//                    "cors_allow_credentials": false
+	//                  },
+	//                  "reindex_whitelist": [],
+	//                  "auto_create_index": true,
+	//                  "enable_close_index": true,
+	//                  "destructive_requires_name": false,
+	//                  "monitoring_collection_interval": -1,
+	//                  "monitoring_history_duration": "3d"
+	//                }
+	//              },
+	//              "instance_configuration_id": "gcp.es.ml.n2.68x32x45",
+	//              "instance_configuration_version": 1,
+	//              "autoscaling_min": {
+	//                "value": 0,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_max": {
+	//                "value": 65536,
+	//                "resource": "memory"
+	//              },
+	//              "autoscaling_tier_override": true
+	//            }
+	//          ],
+	//          "elasticsearch": {
+	//            "version": "8.19.0-SNAPSHOT",
+	//            "enabled_built_in_plugins": [],
+	//            "user_bundles": [],
+	//            "user_plugins": []
+	//          },
+	//          "deployment_template": {
+	//            "id": "gcp-storage-optimized"
+	//          },
+	//          "transient": {
+	//            "strategy": {
+	//              "autodetect": {}
+	//            }
+	//          }
+	//        }
+	//      }
+	//    ],
+	//    "kibana": [
+	//      {
+	//        "region": "gcp-us-west2",
+	//        "ref_id": "main-kibana",
+	//        "elasticsearch_cluster_ref_id": "main-elasticsearch",
+	//        "plan": {
+	//          "cluster_topology": [
+	//            {
+	//              "instance_configuration_id": "gcp.kibana.n2.68x32x45",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 1024,
+	//                "resource": "memory"
+	//              },
+	//              "zone_count": 1,
+	//              "kibana": {
+	//                "system_settings": {}
+	//              }
+	//            }
+	//          ],
+	//          "kibana": {
+	//            "version": "8.19.0-SNAPSHOT"
+	//          },
+	//          "transient": {
+	//            "strategy": {
+	//              "autodetect": {}
+	//            }
+	//          }
+	//        }
+	//      }
+	//    ],
+	//    "apm": [],
+	//    "integrations_server": [
+	//      {
+	//        "region": "gcp-us-west2",
+	//        "ref_id": "main-integrations_server",
+	//        "elasticsearch_cluster_ref_id": "main-elasticsearch",
+	//        "plan": {
+	//          "cluster_topology": [
+	//            {
+	//              "instance_configuration_id": "gcp.integrationsserver.n2.68x32x45",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 1024,
+	//                "resource": "memory"
+	//              },
+	//              "zone_count": 1,
+	//              "integrations_server": {
+	//                "system_settings": {
+	//                  "debug_enabled": false
+	//                }
+	//              }
+	//            }
+	//          ],
+	//          "integrations_server": {
+	//            "version": "8.19.0-SNAPSHOT",
+	//            "system_settings": {
+	//              "secret_token": "TxdKLGthVCBamTXdCL"
+	//            }
+	//          },
+	//          "transient": {
+	//            "strategy": {
+	//              "autodetect": {}
+	//            }
+	//          }
+	//        }
+	//      }
+	//    ],
+	//    "appsearch": [],
+	//    "enterprise_search": [
+	//      {
+	//        "region": "gcp-us-west2",
+	//        "ref_id": "main-enterprise_search",
+	//        "elasticsearch_cluster_ref_id": "main-elasticsearch",
+	//        "plan": {
+	//          "cluster_topology": [
+	//            {
+	//              "node_type": {
+	//                "appserver": true,
+	//                "worker": true,
+	//                "connector": true
+	//              },
+	//              "instance_configuration_id": "gcp.enterprisesearch.n2.68x32x45",
+	//              "instance_configuration_version": 1,
+	//              "size": {
+	//                "value": 2048,
+	//                "resource": "memory"
+	//              },
+	//              "zone_count": 1,
+	//              "enterprise_search": {
+	//                "system_settings": {}
+	//              }
+	//            }
+	//          ],
+	//          "enterprise_search": {
+	//            "version": "8.19.0-SNAPSHOT",
+	//            "system_settings": {}
+	//          },
+	//          "transient": {
+	//            "strategy": {
+	//              "autodetect": {}
+	//            }
+	//          }
+	//        }
+	//      }
+	//    ]
+	//  }
+	//}
+
 }
 
 // ShutdownDeployment attempts to shut down the ESS deployment with the specified ID.
