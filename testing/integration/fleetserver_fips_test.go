85b58d1583dd24cf4b43c92da91e52b3f4812c3f
Shaunak Kashyap <ycombinator@gmail.com>
2025-05-21 09:09:53 -0700

    [FIPS][Integration Test] Ensure FIPS-capable Agent can connect to FIPS-capable Fleet Server in an ECH FRH environment (#8046)
    * WIP: scaffolding for test
    
    * WIP: Print out env vars
    
    * Update comment
    
    * Running mage fmt
    
    * Fix syntax errors
    
    * Update comments
    
    * Flesh out test
    
    * Reference PR from elastic-agent-libs
    
    * Removing comment
    
    * Remove comment that is no longer necessary
    
    * Export INTEGRATIONS_SERVER_HOST from ESS deployment's terraform output
    
    * Set up INTEGRATIONS_SERVER_HOST environment variable
    
    * Fixing API errors
    
    * Bump elastic-agent-libs
    
    (cherry picked from commit 83ece2c9450fe9993f880387d59d4053a3e3cbb0)

---
+++ testing/integration/fleetserver_fips_test.go
@@ -0,0 +1,86 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent-libs/kibana"
+	"github.com/elastic/elastic-agent/pkg/testing/define"
+)
+
+const cloudAgentPolicyID = "policy-elastic-agent-on-cloud"
+
+// TestFIPSAgentConnectingToFIPSFleetServerInECHFRH ensures that a FIPS-capable Elastic Agent
+// running in an ECH FRH (FedRamp High) environment is able to successfully connect to its
+// own local Fleet Server instance (which, by definition should also be FIPS-capable and
+// running in the ECH FRH environment).
+func TestFIPSAgentConnectingToFIPSFleetServerInECHFRH(t *testing.T) {
+	info := define.Require(t, define.Requirements{
+		Group: Fleet,
+		Stack: &define.Stack{},
+		OS: []define.OS{
+			{Type: define.Linux},
+		},
+		Sudo:  false,
+		Local: true,
+
+		// Ensures the test will run in a FIPS-configured environment against a
+		// deployment in ECH that's running a FIPS-capable integrations server.
+		FIPS: true,
+	})
+
+	// Check that the Fleet Server in the deployment is healthy
+	fleetServerHost := os.Getenv("INTEGRATIONS_SERVER_HOST")
+	statusUrl, err := url.JoinPath(fleetServerHost, "/api/status")
+	require.NoError(t, err)
+
+	resp, err := http.Get(statusUrl)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	var body struct {
+		Name   string `json:"name"`
+		Status string `json:"status"`
+	}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&body)
+	require.NoError(t, err)
+
+	require.Equal(t, "HEALTHY", body.Status)
+
+	// Get all Agents
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	agents, err := info.KibanaClient.ListAgents(ctx, kibana.ListAgentsRequest{})
+	require.NoError(t, err)
+
+	// Find Fleet Server's own Agent and get its status and whether it's
+	// FIPS-capable
+	var agentStatus string
+	var agentIsFIPS bool
+	for _, item := range agents.Items {
+		if item.PolicyID == cloudAgentPolicyID {
+			agentStatus = item.Status
+			agentIsFIPS = item.LocalMetadata.Elastic.Agent.FIPS
+		}
+	}
+
+	// Check that this Agent is online (i.e. healthy) and is FIPS-capable. This
+	// will prove that a FIPS-capable Agent is able to connect to a FIPS-capable
+	// Fleet Server, with both running in ECH.
+	require.Equal(t, "online", agentStatus)
+	require.Equal(t, true, agentIsFIPS)
+}
