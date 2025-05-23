57d0e78407f6f4bb2c9d7d985abd3961bfb3b57e
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-05-23 13:01:26 +0200

    Automatically update beats module versions (#8174)
    * Automatically update beats module versions
    
    * Correctly pass branch name
    
    (cherry picked from commit b3ac3f028bb8d3dd78fc2332e8d7143d5016ecdd)
    
    * Check the previous version explicitly
    
    (cherry picked from commit 964a0060b76a065d1738010e2cdb9267e2b2934c)
    
    * Add branch name to PR title
    
    (cherry picked from commit dadd46436eb86eb861a4ac50b75a78fbef64095f)
    
    * Submit the PR to the right branch
    
    (cherry picked from commit 476105b19034ceb45256270fbe5bcc5403bb088d)
    
    * Don't manually configure git in beats update job
    
    (cherry picked from commit 14dd4546bf2294cd56f4645d9af9e2c83411b07f)
    
    * Pin mage Github Action
    
    (cherry picked from commit b5df938745172a9c9fc2eb17c0a645223b7935b7)
    
    * Use elastic/oblt-actions/updatecli/run
    
    (cherry picked from commit 61844eb910a52c943e83e3ef3e185eddb21add93)
    
    * Use elastic/oblt-actions/slack/send@v1
    
    (cherry picked from commit 07cfa747ac4e8e3547873b0515db24e3492e8e30)
    
    * Add guaranteed output to beats update script
    
    (cherry picked from commit 39299203dc57a3f4832ccd5bbd468b9b8fa9fe5d)

--- dev-tools/mage/target/update/update.go
+++ dev-tools/mage/target/update/update.go
@@ -4,9 +4,28 @@
 
 package update
 
-import "github.com/magefile/mage/sh"
+import (
+	"fmt"
 
-// Update updates the generated files (aka make update).
-func Update() error {
-	return sh.Run("make", "update")
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+
+	"github.com/elastic/elastic-agent/dev-tools/mage/target/common"
+)
+
+const BeatsModulePath = "github.com/elastic/beats/v7"
+
+func Beats(targetVersion string) error {
+	mg.SerialDeps(mg.F(BeatsModule, targetVersion), common.Notice)
+
+	return nil
+}
+
+func BeatsModule(targetVersion string) error {
+	goArgs := []string{"get", fmt.Sprintf("%s@%s", BeatsModulePath, targetVersion)}
+	err := sh.RunV(mg.GoCmd(), goArgs...)
+	if err != nil {
+		return err
+	}
+	return sh.RunV(mg.GoCmd(), "mod", "tidy")
 }
