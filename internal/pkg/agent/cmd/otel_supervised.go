6d81c5ede69ea83485c3434fda4b5e3f48e6281b
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-05-28 13:08:26 +0300

    feat: add edot-supervised agent sub-command

---
+++ internal/pkg/agent/cmd/otel_supervised.go
@@ -0,0 +1,40 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/elastic/elastic-agent/internal/pkg/cli"
+	"github.com/elastic/elastic-agent/internal/pkg/otel"
+)
+
+func newOtelSupervisedCommandWithArgs(_ []string, streams *cli.IOStreams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "edot-supervised",
+		Short: "Run EDOT collector in supervised mode",
+		Long:  "This command starts the EDOT collector in supervised mode.",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			if err := prepareEnv(); err != nil {
+				return err
+			}
+			return otel.RunSupervisedCollector(cmd.Context(), streams.In)
+		},
+		PreRun: func(c *cobra.Command, args []string) {
+			// hide inherited flags not to bloat help with flags not related to otel
+			hideInheritedFlags(c)
+		},
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Hidden:        true,
+	}
+
+	cmd.SetHelpFunc(func(c *cobra.Command, s []string) {
+		hideInheritedFlags(c)
+		c.Root().HelpFunc()(c, s)
+	})
+
+	return cmd
+}
