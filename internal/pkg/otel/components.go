882e1b8a1587765d9b323876a8466d511254730d
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-05-28 13:08:26 +0300

    feat: enable support for healthcheckv2 extension

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -58,6 +58,7 @@ import (
 
 	// Extensions
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
+	healthcheckv2extension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckv2extension"
 	k8sobserver "github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/k8sobserver"
 	pprofextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/pprofextension"
 	filestorage "github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/filestorage"
@@ -152,6 +153,7 @@ func components(extensionFactories ...extension.Factory) func() (otelcol.Factori
 		}
 
 		extensions := []extension.Factory{
+			healthcheckv2extension.NewFactory(),
 			memorylimiterextension.NewFactory(),
 			filestorage.NewFactory(),
 			healthcheckextension.NewFactory(),
