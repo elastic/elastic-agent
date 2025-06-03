3e2d2cafef636904dd960e07535a54dfb681a67c
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-06-03 06:47:05 +0300

    feat: introduce new mage tidy target (#8279)
    * fix: introduce new mage tidy target
    
    * fix: G204: Subprocess launched with a potential tainted input or cmd arguments
    
    * doc: add documentation for mage tidy
    
    (cherry picked from commit 5b7ee26ec5dc24a070e8c945578e2f468242363a)
    
    # Conflicts:
    #	README.md
    #	dev-tools/mage/target/common/notice.go
    #	dev-tools/mage/target/update/update.go

--- dev-tools/mage/target/update/update.go
+++ dev-tools/mage/target/update/update.go
@@ -6,7 +6,34 @@ package update
 
 import "github.com/magefile/mage/sh"
 
+<<<<<<< HEAD
 // Update updates the generated files (aka make update).
 func Update() error {
 	return sh.Run("make", "update")
+=======
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+
+	"github.com/elastic/elastic-agent/dev-tools/mage/target/common"
+)
+
+const BeatsModulePath = "github.com/elastic/beats/v7"
+
+func Beats(targetVersion string) error {
+	mg.SerialDeps(mg.F(BeatsModule, targetVersion), common.Notice)
+
+	return nil
+}
+
+func BeatsModule(targetVersion string) error {
+	goArgs := []string{"get", fmt.Sprintf("%s@%s", BeatsModulePath, targetVersion)}
+	err := sh.RunV(mg.GoCmd(), goArgs...)
+	if err != nil {
+		return err
+	}
+	// NOTE: this is not invoked through mg.Deps because
+	// we want to always invoke it and guarantee that it runs
+	// as mg.Deps does memoization
+	return common.Tidy()
+>>>>>>> 5b7ee26ec (feat: introduce new mage tidy target (#8279))
 }
