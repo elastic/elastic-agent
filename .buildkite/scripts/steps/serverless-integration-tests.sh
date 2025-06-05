95b5b36cf6caf695becf53ecc311c5357d4a7390
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-06-05 20:57:36 +0200

    no need

--- .buildkite/scripts/steps/serverless-integration-tests.sh
+++ .buildkite/scripts/steps/serverless-integration-tests.sh
@@ -10,12 +10,6 @@ asdf install
 echo "~~~ Building test binaries"
 mage build:testBinaries
 
-# TODO: move to common.sh when it's refactored
-# BK analytics
-echo "--- Prepare BK test analytics token :vault:"
-BUILDKITE_ANALYTICS_TOKEN=$(vault kv get -field token kv/ci-shared/platform-ingest/buildkite_analytics_token)
-export BUILDKITE_ANALYTICS_TOKEN
-
 # Run integration tests
 echo "~~~ Running serverless integration tests"
 sudo -E .buildkite/scripts/buildkite-integration-tests.sh fleet true TestLogIngestionFleetManaged
