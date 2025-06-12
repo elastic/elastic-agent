f62da9b32bacbcab4c9c5360763e4e7bdf0864bb
Pavel Zorin <pavel.zorin@elastic.co>
2025-06-12 17:56:06 +0200

    Fix WD

--- .buildkite/scripts/macos_install_asdf.sh
+++ .buildkite/scripts/macos_install_asdf.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install_asdf() {
-  echo "$SHELL"
+echo $SHELL
 
-  AGENT_USER="${AGENT_USER:-"$USER"}"
-  AGENT_HOME="${AGENT_HOME:-"/Users/$AGENT_USER"}"
-  ASDF_DIR="${ASDF_DIR:-"/Users/$AGENT_USER/.asdf"}"
+AGENT_USER="${AGENT_USER:-"$USER"}"
+AGENT_HOME="${AGENT_HOME:-"/Users/$AGENT_USER"}"
+ASDF_DIR="${ASDF_DIR:-"/Users/$AGENT_USER/.asdf"}"
 
-  echo "~~~ Installing ASDF in ${ASDF_DIR} for user ${AGENT_USER}"
-  ASDF_VERSION="v0.14.0"
+echo "~~~ Installing ASDF in ${ASDF_DIR} for user ${AGENT_USER}"
+# Installation instructions from https://asdf-vm.com/guide/getting-started.html
+ASDF_VERSION="v0.14.0"
 
-  pushd "$AGENT_HOME" > /dev/null
-  trap 'popd > /dev/null' RETURN
+pushd $AGENT_HOME
 
-  # todo retry
-  git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR" --branch "$ASDF_VERSION"
-  echo 'source $ASDF_DIR/asdf.sh' >> "$AGENT_HOME/.bashrc"
-  source "$ASDF_DIR/asdf.sh"
-  asdf plugin update --all
+# todo retry
+git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch ${ASDF_VERSION} 
+echo 'source $ASDF_DIR/asdf.sh' >> $AGENT_HOME/.bashrc 
+source $ASDF_DIR/asdf.sh 
+asdf plugin update --all
 
-  asdf plugin add terraform
-  asdf plugin add golang
-  asdf plugin add mage
-}
+asdf plugin add terraform
+asdf plugin add golang
+asdf plugin add mage
 
-install_asdf
+popd
+
+asdf install
+go version
