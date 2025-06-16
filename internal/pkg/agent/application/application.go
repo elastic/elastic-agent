b2a14af3b857129b3c9eadfec083945c9ed4d369
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-06-16 16:02:51 +0200

    Update coordinator to use the new manager

--- internal/pkg/agent/application/application.go
+++ internal/pkg/agent/application/application.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/elastic/elastic-agent/internal/pkg/otel/componentmanager"
+
 	"go.elastic.co/apm/v2"
 
 	"github.com/elastic/go-ucfg"
@@ -230,7 +232,8 @@ func New(
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("failed to create otel manager: %w", err)
 	}
-	coord := coordinator.New(log, cfg, logLevel, agentInfo, specs, reexec, upgrader, runtime, configMgr, varsManager, caps, monitor, isManaged, otelManager, actionAcker, compModifiers...)
+	otelComponentManager := componentmanager.NewOtelComponentManager(log.Named("otel_component_manager"), otelManager, agentInfo, monitor.ComponentMonitoringConfig)
+	coord := coordinator.New(log, cfg, logLevel, agentInfo, specs, reexec, upgrader, runtime, configMgr, varsManager, caps, monitor, isManaged, otelComponentManager, actionAcker, compModifiers...)
 	if managed != nil {
 		// the coordinator requires the config manager as well as in managed-mode the config manager requires the
 		// coordinator, so it must be set here once the coordinator is created
