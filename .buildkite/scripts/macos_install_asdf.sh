9f1ae986613abf37b1fe4c537edb6026a294a78d
Pavel Zorin <pavel.zorin@elastic.co>
2025-06-22 00:00:29 +0200

    Add GOROOT

--- .buildkite/scripts/macos_install_asdf.sh
+++ .buildkite/scripts/macos_install_asdf.sh
@@ -9,9 +9,7 @@ ASDF_DIR="${ASDF_DIR:-"/Users/$AGENT_USER/.asdf"}"
 
 echo "~~~ Installing ASDF in ${ASDF_DIR} for user ${AGENT_USER}"
 # Installation instructions from https://asdf-vm.com/guide/getting-started.html
-ASDF_VERSION="v0.16.7"
-
-pushd $AGENT_HOME
+ASDF_VERSION="v0.14.1"
 
 # todo retry
 git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch ${ASDF_VERSION} 
@@ -23,12 +21,12 @@ asdf plugin add terraform
 asdf plugin add golang
 asdf plugin add mage
 
-popd
 
 echo "~~~ Installing golang $(cat .go-version) using ASDF"
-asdf set golang "$(cat .go-version)"
+asdf local golang "$(cat .go-version)"
 asdf install
 
 export GOROOT="$(asdf where golang)/go/"
 export GOPATH=$(go env GOPATH)
 export PATH="$GOPATH/bin:$PATH"
+go version
