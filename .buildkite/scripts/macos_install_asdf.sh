3647a5335e530504ba2661b635fde18e4f5c7482
Pavel Zorin <pavel.zorin@elastic.co>
2025-06-22 22:36:42 +0200

    Update ASDF

--- .buildkite/scripts/macos_install_asdf.sh
+++ .buildkite/scripts/macos_install_asdf.sh
@@ -9,14 +9,22 @@ echo "~~~ Installing ASDF in ${ASDF_DIR} for user ${AGENT_USER}"
 # Installation instructions from https://asdf-vm.com/guide/getting-started.html
 ASDF_VERSION="v0.14.1"
 
+# rm -rf "${ASDF_DIR}" || true
+
 # todo retry
 git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch ${ASDF_VERSION} 
 echo 'source $ASDF_DIR/asdf.sh' >> $AGENT_HOME/.bashrc 
 source $ASDF_DIR/asdf.sh
 asdf version
+
 asdf plugin update --all
+asdf plugin add terraform
+asdf plugin add golang
+asdf plugin add mage
 
 echo "~~~ Installing golang $(cat .go-version) using ASDF"
+asdf install
+
 export GOROOT="$(asdf where golang)/go/"
 export GOPATH=$(go env GOPATH)
 export PATH="$GOPATH/bin:$PATH"
