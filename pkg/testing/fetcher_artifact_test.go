97e23818732107d159009ebd3cb8f714578336c6
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-06-25 08:48:57 +0300

    feat: validate artifact hashes in artifact fetcher

--- pkg/testing/fetcher_artifact_test.go
+++ pkg/testing/fetcher_artifact_test.go
@@ -110,16 +110,17 @@ func (c *fakeHttpClient) Do(req *http.Request) (*http.Response, error) {
 
 func newFakeHttpClient(t *testing.T) *fakeHttpClient {
 	releaseResponse, err := os.ReadFile("./testdata/release-response.json")
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to read release response")
 
 	snapshotResponse, err := os.ReadFile("./testdata/snapshot-response.json")
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to read snapshot response")
 
 	manifestResponse, err := os.ReadFile("./testdata/build-manifest.json")
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to read manifest response")
 
-	binaryResponse := "not valid data; but its very fast to download something this small"
-	hashResponse := "c2f59774022b79b61a7e6bbe28f3388d00a5bc2c7416a5c8fda79042af491d335f9b87adf905d1b154abdd2e31b200e4b1bb23cb472297596b25edef0a3b8d59"
+	binaryResponse, err := os.ReadFile("./testdata/data.tar.gz")
+	require.NoError(t, err, "failed to read binary response")
+	hashResponse := "cc52f8aa1106857dae8d380f6c2cf789d5d52730df0b0e6aba908a5e1f3cb947fda63bb4bc0301a3bc3329ef4b2f3c5fa9be9d8975a4d0f8f43076cfd5a5ec8a"
 	ascResponse := `-----BEGIN PGP SIGNATURE-----
 
         wsBcBAABCAAQBQJlTLh5CRD2Vuvax5DnywAAzNcIADKuYov0CMeK938JQEzR4mXP
@@ -153,7 +154,7 @@ func newFakeHttpClient(t *testing.T) *fakeHttpClient {
 		// 8.12 release
 		"https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.0-linux-x86_64.tar.gz": {
 			StatusCode: 200,
-			Body:       io.NopCloser(bytes.NewReader([]byte(binaryResponse))),
+			Body:       io.NopCloser(bytes.NewReader(binaryResponse)),
 		},
 		"https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.0-linux-x86_64.tar.gz.sha512": {
 			StatusCode: 200,
@@ -166,7 +167,7 @@ func newFakeHttpClient(t *testing.T) *fakeHttpClient {
 		// 8.13 SNAPSHOT
 		"https://snapshots.elastic.co/8.13.0-yil7wib0/downloads/beats/elastic-agent/elastic-agent-8.13.0-SNAPSHOT-linux-x86_64.tar.gz": {
 			StatusCode: 200,
-			Body:       io.NopCloser(bytes.NewReader([]byte(binaryResponse))),
+			Body:       io.NopCloser(bytes.NewReader(binaryResponse)),
 		},
 		"https://snapshots.elastic.co/8.13.0-yil7wib0/downloads/beats/elastic-agent/elastic-agent-8.13.0-SNAPSHOT-linux-x86_64.tar.gz.sha512": {
 			StatusCode: 200,
@@ -184,7 +185,7 @@ func newFakeHttpClient(t *testing.T) *fakeHttpClient {
 		// 8.13 build l5snflwr
 		"https://snapshots.elastic.co/8.13.0-l5snflwr/downloads/beats/elastic-agent/elastic-agent-8.13.0-SNAPSHOT-linux-x86_64.tar.gz": {
 			StatusCode: 200,
-			Body:       io.NopCloser(bytes.NewReader([]byte(binaryResponse))),
+			Body:       io.NopCloser(bytes.NewReader(binaryResponse)),
 		},
 		"https://snapshots.elastic.co/8.13.0-l5snflwr/downloads/beats/elastic-agent/elastic-agent-8.13.0-SNAPSHOT-linux-x86_64.tar.gz.sha512": {
 			StatusCode: 200,
