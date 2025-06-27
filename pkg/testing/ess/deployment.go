963c14567f874b3c4d3e7471e15ec345fa9bf7dc
Shaunak Kashyap <ycombinator@gmail.com>
2025-06-27 13:42:05 -0700

    Integration Test: upgrade Integrations Server in ECH (#8417)
    * WIP: Implementing UpgradeDeployment method
    
    * Adding AvailableVersions method to StatefulProvisioner
    
    * WIP: adding test for upgrading integration server in ECH
    
    * Ensure test runs against FRH ECH region
    
    * WIP
    
    * WIP
    
    * Add test for generating upgrade deployment request
    
    * Flesh out test some more
    
    * Fixing filename in reference
    
    * Make DeploymentStatus contain status and health
    
    * Cleanup deployment after test
    
    * Add error handling
    
    * Make linter happy
    
    * Fix typo
    
    * Fix check
    
    * Adding logging
    
    * Filter by min/max versions
    
    * Fix call
    
    * Running non-sudo upgrade group tests
    
    * Running upgrade FIPS tests
    
    * Remove non-sudo upgrade group from regular integration tests
    
    * Fix env var for ECH region and default it to gcp-us-west2
    
    * Use env var for ECH API key
    
    * Run ECH deployment upgrade test if PR label is present
    
    * Fix missing key check
    
    * Add label on PR that bumps integration test versions
    
    * Export EC_API_KEY
    
    * WIP: debugging
    
    * Append query string to URL after joining paths
    
    * Fix up logic in doGet to construct URL without escaping ?
    
    * WIP: Remove some debug logging
    
    * Add bounds check
    
    * Fix start index math
    
    * Removing debug logs
    
    * WIP: don't clean up deployment so we can investigate
    
    * Add doPut method on client
    
    * Remove debugging statements
    
    * Fix build tag
    
    * Change group for testing in PR
    
    * Cleanup
    
    * Add comment
    
    * Cleanup
    
    * Update deployment version after upgrade
    
    * Test group in PR
    
    * Fix unit test
    
    * Undo unintentional changes
    
    (cherry picked from commit 4344a11785aba745e155c1bdc2b12815956cb7b1)
    
    # Conflicts:
    #	.ci/updatecli/updatecli-bump-stack-version.yml
    #	.mergify.yml
    #	test_infra/ess/readme.md

--- pkg/testing/ess/deployment.go
+++ pkg/testing/ess/deployment.go
@@ -59,32 +59,15 @@ type GetDeploymentResponse struct {
 	}
 }
 
-type DeploymentStatus string
-
-func (d *DeploymentStatus) UnmarshalJSON(data []byte) error {
-	var status string
-	if err := json.Unmarshal(data, &status); err != nil {
-		return err
-	}
-
-	switch status {
-	case string(DeploymentStatusInitializing), string(DeploymentStatusReconfiguring), string(DeploymentStatusStarted):
-		*d = DeploymentStatus(status)
-	default:
-		return fmt.Errorf("unknown status: [%s]", status)
-	}
-
-	return nil
-}
-
-func (d *DeploymentStatus) String() string {
-	return string(*d)
+type DeploymentStatus struct {
+	Status  string
+	Healthy bool
 }
 
 const (
-	DeploymentStatusInitializing  DeploymentStatus = "initializing"
-	DeploymentStatusReconfiguring DeploymentStatus = "reconfiguring"
-	DeploymentStatusStarted       DeploymentStatus = "started"
+	DeploymentStatusInitializing  = "initializing"
+	DeploymentStatusReconfiguring = "reconfiguring"
+	DeploymentStatusStarted       = "started"
 )
 
 type DeploymentStatusResponse struct {
@@ -192,6 +175,82 @@ func (c *Client) CreateDeployment(ctx context.Context, req CreateDeploymentReque
 	return &r, nil
 }
 
+//go:embed upgrade_deployment_request.tmpl.json
+var upgradeDeploymentRequestTemplate string
+
+// UpgradeDeployment upgrades the specified deployment to the specified version.
+func (c *Client) UpgradeDeployment(ctx context.Context, deploymentID string, version string) error {
+	u, err := url.JoinPath("deployments", deploymentID)
+	if err != nil {
+		return fmt.Errorf("unable to create deployment update API URL: %w", err)
+	}
+
+	// Get deployment
+	resp, err := c.doGet(ctx, u)
+	if err != nil {
+		return fmt.Errorf("unable to GET deployment: %w", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read body of GET deployment API response: %w", err)
+	}
+
+	// Parse out values from body that will be needed in the upgrade request body
+	var bodyObj struct {
+		Resources struct {
+			Elasticsearch []struct {
+				Region string
+			} `json:"elasticsearch"`
+		} `json:"resources"`
+	}
+	err = json.Unmarshal(data, &bodyObj)
+	if err != nil {
+		return fmt.Errorf("unable to parse current deployment version from GET deployment API response: %w", err)
+	}
+
+	region := bodyObj.Resources.Elasticsearch[0].Region
+	reqBodyBytes, err := generateUpgradeDeploymentRequestBody(region, version)
+	if err != nil {
+		return fmt.Errorf("unable to generate upgrade request body: %w", err)
+	}
+
+	upgradeResp, err := c.doPut(
+		ctx,
+		u,
+		"application/json",
+		bytes.NewReader(reqBodyBytes),
+	)
+	if err != nil {
+		return fmt.Errorf("error calling deployment update API: %w", err)
+	}
+	defer upgradeResp.Body.Close()
+
+	if upgradeResp.StatusCode != http.StatusOK {
+		resBytes, _ := io.ReadAll(upgradeResp.Body)
+		c.logger.Logf("Response body: %s", string(resBytes))
+		return fmt.Errorf("got unexpected response code [%d] from deployment update API", upgradeResp.StatusCode)
+	}
+
+	var upgradeRespBody struct {
+		Errors []struct {
+			Code    string `json:"code"`
+			Message string `json:"message"`
+		} `json:"errors"`
+	}
+
+	if err = json.NewDecoder(upgradeResp.Body).Decode(&upgradeRespBody); err != nil {
+		return fmt.Errorf("error parsing deployment update API response: %w", err)
+	}
+
+	if len(upgradeRespBody.Errors) > 0 {
+		return fmt.Errorf("failed to upgrade: (%s) %s", upgradeRespBody.Errors[0].Code, upgradeRespBody.Errors[0].Message)
+	}
+
+	return nil
+}
+
 // ShutdownDeployment attempts to shut down the ESS deployment with the specified ID.
 func (c *Client) ShutdownDeployment(ctx context.Context, deploymentID string) error {
 	u, err := url.JoinPath("deployments", deploymentID, "_shutdown")
@@ -222,20 +281,24 @@ func (c *Client) DeploymentStatus(ctx context.Context, deploymentID string) (*De
 	defer getResp.Body.Close()
 
 	var getRespBody struct {
+		Healthy   bool `json:"healthy"`
 		Resources struct {
 			Elasticsearch []struct {
 				Info struct {
-					Status DeploymentStatus `json:"status"`
+					Healthy bool   `json:"healthy"`
+					Status  string `json:"status"`
 				} `json:"info"`
 			} `json:"elasticsearch"`
 			Kibana []struct {
 				Info struct {
-					Status DeploymentStatus `json:"status"`
+					Healthy bool   `json:"healthy"`
+					Status  string `json:"status"`
 				} `json:"info"`
 			} `json:"kibana"`
 			IntegrationsServer []struct {
 				Info struct {
-					Status DeploymentStatus `json:"status"`
+					Healthy bool   `json:"healthy"`
+					Status  string `json:"status"`
 				} `json:"info"`
 			} `json:"integrations_server"`
 		} `json:"resources"`
@@ -246,16 +309,28 @@ func (c *Client) DeploymentStatus(ctx context.Context, deploymentID string) (*De
 	}
 
 	s := DeploymentStatusResponse{
-		Elasticsearch:      getRespBody.Resources.Elasticsearch[0].Info.Status,
-		Kibana:             getRespBody.Resources.Kibana[0].Info.Status,
-		IntegrationsServer: getRespBody.Resources.IntegrationsServer[0].Info.Status,
+		Elasticsearch: DeploymentStatus{
+			Status:  getRespBody.Resources.Elasticsearch[0].Info.Status,
+			Healthy: getRespBody.Resources.Elasticsearch[0].Info.Healthy,
+		},
+		Kibana: DeploymentStatus{
+			Status:  getRespBody.Resources.Kibana[0].Info.Status,
+			Healthy: getRespBody.Resources.Kibana[0].Info.Healthy,
+		},
+		IntegrationsServer: DeploymentStatus{
+			Status:  getRespBody.Resources.IntegrationsServer[0].Info.Status,
+			Healthy: getRespBody.Resources.IntegrationsServer[0].Info.Healthy,
+		},
 	}
-	s.Overall = overallStatus(s.Elasticsearch, s.Kibana, s.IntegrationsServer)
+	s.Overall = DeploymentStatus{
+		Status:  overallStatus(s.Elasticsearch.Status, s.Kibana.Status, s.IntegrationsServer.Status),
+		Healthy: getRespBody.Healthy,
+	}
 
 	return &s, nil
 }
 
-// DeploymentIsReady returns true when the deployment is ready, checking its status
+// DeploymentIsReady returns true when the deployment is ready and healthy, checking its status
 // every `tick` until `waitFor` duration.
 func (c *Client) DeploymentIsReady(ctx context.Context, deploymentID string, tick time.Duration) (bool, error) {
 	ticker := time.NewTicker(tick)
@@ -279,7 +354,7 @@ func (c *Client) DeploymentIsReady(ctx context.Context, deploymentID string, tic
 				statusCh <- status.Overall
 			}()
 		case status := <-statusCh:
-			if status == DeploymentStatusStarted {
+			if status.Status == DeploymentStatusStarted && status.Healthy {
 				return true, nil
 			}
 		}
@@ -295,11 +370,11 @@ func (c *Client) getDeployment(ctx context.Context, deploymentID string) (*http.
 	return c.doGet(ctx, u)
 }
 
-func overallStatus(statuses ...DeploymentStatus) DeploymentStatus {
+func overallStatus(statuses ...string) string {
 	// The overall status is started if every component's status is started. Otherwise,
 	// we take the non-started components' statuses and pick the first one as the overall
 	// status.
-	statusMap := map[DeploymentStatus]struct{}{}
+	statusMap := map[string]struct{}{}
 	for _, status := range statuses {
 		statusMap[status] = struct{}{}
 	}
@@ -310,7 +385,7 @@ func overallStatus(statuses ...DeploymentStatus) DeploymentStatus {
 		}
 	}
 
-	var overallStatus DeploymentStatus
+	var overallStatus string
 	for _, status := range statuses {
 		if status != DeploymentStatusStarted {
 			overallStatus = status
@@ -324,23 +399,15 @@ func overallStatus(statuses ...DeploymentStatus) DeploymentStatus {
 //go:embed create_deployment_request.tmpl.json
 var createDeploymentRequestTemplate string
 
-//go:embed create_deployment_csp_configuration.yaml
+//go:embed deployment_csp_configuration.yaml
 var cloudProviderSpecificValues []byte
 
 func generateCreateDeploymentRequestBody(req CreateDeploymentRequest) ([]byte, error) {
-	var csp string
-	// Special case: AWS us-east-1 region is just called
-	// us-east-1 (instead of aws-us-east-1)!
-	if req.Region == "us-east-1" {
-		csp = "aws"
-	} else {
-		regionParts := strings.Split(req.Region, "-")
-		if len(regionParts) < 2 {
-			return nil, fmt.Errorf("unable to parse CSP out of region [%s]", req.Region)
-		}
-
-		csp = regionParts[0]
+	csp, err := parseCSPFromRegion(req.Region)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse CSP from region [%s]: %w", req.Region, err)
 	}
+
 	templateContext, err := createDeploymentTemplateContext(csp, req)
 	if err != nil {
 		return nil, fmt.Errorf("creating request template context: %w", err)
@@ -361,6 +428,21 @@ func generateCreateDeploymentRequestBody(req CreateDeploymentRequest) ([]byte, e
 	return bBuf.Bytes(), nil
 }
 
+func parseCSPFromRegion(region string) (string, error) {
+	// Special case: AWS us-east-1 region is just called
+	// us-east-1 (instead of aws-us-east-1)!
+	if region == "us-east-1" {
+		return "aws", nil
+	}
+
+	regionParts := strings.Split(region, "-")
+	if len(regionParts) < 2 {
+		return "", fmt.Errorf("unable to parse CSP out of region [%s]", region)
+	}
+
+	return regionParts[0], nil
+}
+
 func jsonMarshal(in any) (string, error) {
 	jsonBytes, err := json.Marshal(in)
 	if err != nil {
@@ -370,7 +452,7 @@ func jsonMarshal(in any) (string, error) {
 	return string(jsonBytes), nil
 }
 
-func createDeploymentTemplateContext(csp string, req CreateDeploymentRequest) (map[string]any, error) {
+func createDeploymentTemplateContext(csp string, req any) (map[string]any, error) {
 	cspSpecificContext, err := loadCspValues(csp)
 	if err != nil {
 		return nil, fmt.Errorf("loading csp-specific values for %q: %w", csp, err)
@@ -399,4 +481,35 @@ func loadCspValues(csp string) (map[string]any, error) {
 	}
 
 	return values, nil
+}
+
+func generateUpgradeDeploymentRequestBody(region, version string) ([]byte, error) {
+	csp, err := parseCSPFromRegion(region)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse CSP from region [%s]: %w", region, err)
+	}
+
+	req := map[string]string{
+		"region":  region,
+		"version": version,
+	}
+
+	templateContext, err := createDeploymentTemplateContext(csp, req)
+	if err != nil {
+		return nil, fmt.Errorf("creating request template context: %w", err)
+	}
+
+	tpl, err := template.New("upgrade_deployment_request").
+		Funcs(template.FuncMap{"json": jsonMarshal}).
+		Parse(upgradeDeploymentRequestTemplate)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse deployment upgrade template: %w", err)
+	}
+
+	var bBuf bytes.Buffer
+	err = tpl.Execute(&bBuf, templateContext)
+	if err != nil {
+		return nil, fmt.Errorf("rendering upgrade deployment request template with context %v: %w", templateContext, err)
+	}
+	return bBuf.Bytes(), nil
 }
