25932cdd98e2209d78e62fd50fd76b10ee311fed
girodav <1390902+girodav@users.noreply.github.com>
2025-07-07 12:43:36 +0100

    Add elasticapmintakereceiver

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -13,6 +13,7 @@ import (
 	"go.opentelemetry.io/collector/receiver"
 
 	// Receivers:
+	elasticapmintakereceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/elasticapmintakereceiver" // for collecting APM data from Elastic APM agents
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
 	hostmetricsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
 	httpcheckreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/httpcheckreceiver"
@@ -83,6 +84,7 @@ func components(extensionFactories ...extension.Factory) func() (otelcol.Factori
 
 		// Receivers
 		receivers := []receiver.Factory{
+			elasticapmintakereceiver.NewFactory(),
 			otlpreceiver.NewFactory(),
 			filelogreceiver.NewFactory(),
 			kubeletstatsreceiver.NewFactory(),
