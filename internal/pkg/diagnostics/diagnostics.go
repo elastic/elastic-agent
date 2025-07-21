fbc6db14ce7ee3b16181545a578f6eb6348ec93c
Paolo Chila <paolo.chila@elastic.co>
2025-07-21 12:38:50 +0200

    Add pod description to k8s diagnostics

--- internal/pkg/diagnostics/diagnostics.go
+++ internal/pkg/diagnostics/diagnostics.go
@@ -19,6 +19,9 @@ import (
 	"strings"
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/elastic/elastic-agent-autodiscover/kubernetes"
 	"github.com/elastic/elastic-agent/pkg/control/v2/client"
 	"github.com/elastic/go-ucfg"
 
@@ -136,7 +139,76 @@ func GlobalHooks() Hooks {
 			ContentType: "application/octet-stream",
 			Hook:        pprofDiag("mutex", 0),
 		},
+		{
+			Name:        "k8s diagnostics",
+			Filename:    "elastic-agent-k8s.zip",
+			Description: " kubernetes pod describe for elastic-agent",
+			ContentType: "application/octet-stream",
+			Hook:        k8sDescribePod(),
+		},
 	}
+}
+
+func k8sDescribePod() func(ctx context.Context) []byte {
+	return func(ctx context.Context) []byte {
+		if _, ok := os.LookupEnv("KUBERNETES_SERVICE_HOST"); !ok {
+			return nil
+		}
+
+		// TODO create a temp dir here and an errors file where to dump all the errors ?
+
+		kubernetesClient, err := kubernetes.GetKubernetesClient("", kubernetes.KubeClientOptions{})
+		if err != nil {
+			return []byte(fmt.Sprintf("error instantiating k8s client: %q", err))
+		}
+		const namespaceFile = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+		namespaceBytes, err := os.ReadFile(namespaceFile)
+		if err != nil {
+			return []byte(fmt.Sprintf("error reading namespace from %q: %q", namespaceFile, err))
+		}
+
+		const podNameFile = "/etc/podinfo/name"
+		podName, err := os.ReadFile(podNameFile)
+		if err != nil {
+			return []byte(fmt.Sprintf("error reading podName from %q: %q", podNameFile, err))
+		}
+
+		namespace := strings.TrimSpace(string(namespaceBytes))
+		pod, err := kubernetesClient.CoreV1().Pods(namespace).Get(ctx, string(podName), metav1.GetOptions{})
+		if err != nil {
+			return []byte(fmt.Sprintf("error reading podName from %q: %q", podNameFile, err))
+		}
+
+		tmpDir, err := os.MkdirTemp("", "elastic-agent-k8s-diag-*")
+		if err != nil {
+			return []byte(fmt.Sprintf("error creating k8s diag temp directory: %q", err))
+		}
+		defer os.RemoveAll(tmpDir)
+
+		podMashalledBytes, err := pod.Marshal()
+		if err != nil {
+			return []byte(fmt.Sprintf("error marshalling pod: %q", err))
+		}
+
+		err = os.WriteFile(filepath.Join(tmpDir, "pod.yaml"), podMashalledBytes, 0644)
+		if err != nil {
+			return []byte(fmt.Sprintf("error writing pod.yaml: %q", err))
+		}
+
+		buf := new(bytes.Buffer)
+		err = writeZipFileFromDir(buf, tmpDir)
+		if err != nil {
+			return []byte(fmt.Sprintf("error creating zipped elastic-agent-k8s-*.zip: %q", err))
+		}
+
+		return buf.Bytes()
+	}
+}
+
+func writeZipFileFromDir(baseWriter io.Writer, dir string) error {
+	writer := zip.NewWriter(baseWriter)
+	defer writer.Close()
+	return writer.AddFS(os.DirFS(dir))
 }
 
 func pprofDiag(name string, debug int) func(context.Context) []byte {
