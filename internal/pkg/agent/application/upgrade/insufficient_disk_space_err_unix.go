61b5387a4422e0e56b36e267ec677977a1a9a164
kaanyalti <kaan.yalti@elastic.co>
2025-07-25 18:10:14 +0300

    enhancement(5235): created disk space error type, wrapping backoff permanent error type

--- internal/pkg/agent/application/upgrade/insufficient_disk_space_err_unix.go
+++ internal/pkg/agent/application/upgrade/insufficient_disk_space_err_unix.go
@@ -10,7 +10,7 @@ import (
 // ToDiskSpaceError returns a generic disk space error if the error is a disk space error
 func ToDiskSpaceError(err error) error {
 	if errors.Is(err, syscall.ENOSPC) || errors.Is(err, syscall.EDQUOT) {
-		return errors.New(insufficientDiskSpaceErrorStr)
+		return insufficientDiskSpaceErr
 	}
 
 	return err
