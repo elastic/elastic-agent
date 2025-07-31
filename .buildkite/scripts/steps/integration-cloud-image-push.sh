a1a493722319191daed231c2f27e64fd41a70469
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-07-31 14:44:29 +0300

    feat: rework fabrication of CI_ELASTIC_AGENT_DOCKER_IMAGE

--- .buildkite/scripts/steps/integration-cloud-image-push.sh
+++ .buildkite/scripts/steps/integration-cloud-image-push.sh
@@ -5,11 +5,12 @@ source .buildkite/scripts/common.sh
 
 echo "~~~ Pushing cloud image"
 
+suffix=""
 if [ "${FIPS:-false}" == "true" ]; then
-  CI_ELASTIC_AGENT_DOCKER_IMAGE="docker.elastic.co/beats-ci/elastic-agent-cloud-fips"
-else
-  CI_ELASTIC_AGENT_DOCKER_IMAGE="docker.elastic.co/beats-ci/elastic-agent-cloud"
+  suffix="-fips"
 fi
+
+CI_ELASTIC_AGENT_DOCKER_IMAGE="docker.elastic.co/beats-ci/elastic-agent-cloud${suffix}"
 export CI_ELASTIC_AGENT_DOCKER_IMAGE
 echo "CI_ELASTIC_AGENT_DOCKER_IMAGE: ${CI_ELASTIC_AGENT_DOCKER_IMAGE}"
 
