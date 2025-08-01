ad7820ebcf72826c4a6f4773eac24eae1fa2df85
Shaunak Kashyap <ycombinator@gmail.com>
2025-08-01 06:57:57 -0700

    Two implementations

---
+++ internal/pkg/agent/application/actions/handlers/handler_helpers_windows.go
@@ -0,0 +1,46 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+//go:build windows
+
+package handlers
+
+import (
+	"errors"
+	"io"
+	"syscall"
+	"time"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/storage"
+)
+
+const saveRetryFrequency = 50 * time.Millisecond
+const saveRetryDuration = 2 * time.Second
+
+// On Windows platforms, save operations are retried if the error is an
+// ACCESS_DENIED error, which can happen if the file is locked by another process.
+func checkSaveErrorAndRetry(err error, store storage.Store, reader io.Reader) bool {
+	if !errors.Is(err, syscall.ERROR_ACCESS_DENIED) {
+		return false
+	}
+
+	for start := time.Now(); time.Since(start) < saveRetryDuration; time.Sleep(saveRetryFrequency) {
+		err := store.Save(reader)
+		if err == nil {
+			// Save succeeded
+			return true
+		}
+
+		if !errors.Is(err, syscall.ERROR_ACCESS_DENIED) {
+			// Save failed due to an error that is not ACCESS_DENIED. Immediately
+			// indicate failure without retrying further.
+			return false
+		}
+
+		// Keep retrying...
+	}
+
+	// We exhausted retries without succeeding, so indicate failure.
+	return false
+}
