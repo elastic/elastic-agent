282d62f99f751ae6175eb76840bf1482639d1a42
Shaunak Kashyap <ycombinator@gmail.com>
2025-08-12 17:25:45 -0700

    Adding a mock binary

--- pkg/component/runtime/testdata/exitcode/doc.go
+++ pkg/component/runtime/testdata/exitcode/doc.go
@@ -1,1 +1,8 @@
-package exitcode
+package main
+
+// This package contains a go program that exits with an exit code.
+// The desired exit code must be set at build time using
+// go build -ldflags='-X main.ExitCode=<code>'.
+// The resulting binary can be used in tests to simulate an
+// Agent-managed component, e.g. Endpoint, that exits with a specific
+// exit code.
--- pkg/component/runtime/testdata/exitcode/main.go
+++ pkg/component/runtime/testdata/exitcode/main.go
@@ -1,1 +1,16 @@
-package exitcode
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+var ExitCode = "0" // string so it can be set at build time
+
+func main() {
+	exitCode, err := strconv.Atoi(ExitCode)
+	if err != nil {
+		exitCode = -1
+	}
+	os.Exit(exitCode)
+}
