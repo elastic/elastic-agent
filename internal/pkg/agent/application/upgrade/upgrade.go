c6a9fa78a28a5fcc1c8fe0160cba4a018ca2313f
Kaan Yalti <kaan.yalti@elastic.co>
2025-08-13 17:56:24 +0300

    enhancement(5235): added package var to abstract third party package function copy.Copy

--- internal/pkg/agent/application/upgrade/upgrade.go
+++ internal/pkg/agent/application/upgrade/upgrade.go
@@ -667,6 +667,8 @@ func readDirs(dir string) ([]string, error) {
 	return dirs, nil
 }
 
+var fileDirCopyFunc = copy.Copy // abstraction for testability
+
 func copyDir(l *logger.Logger, from, to string, ignoreErrs bool) error {
 	var onErr func(src, dst string, err error) error
 
@@ -693,7 +695,7 @@ func copyDir(l *logger.Logger, from, to string, ignoreErrs bool) error {
 		copyConcurrency = runtime.NumCPU() * 4
 	}
 
-	return copy.Copy(from, to, copy.Options{
+	return fileDirCopyFunc(from, to, copy.Options{
 		OnSymlink: func(_ string) copy.SymlinkAction {
 			return copy.Shallow
 		},
