c9e9a084defc696c14384f184242842e8d4da632
Kaan Yalti <kaan.yalti@elastic.co>
2025-08-14 18:23:52 +0300

    enhancement(5235): added mkdirall wrapper in copyRunDir
    enhancement(5235): added common package import

--- internal/pkg/agent/application/upgrade/upgrade.go
+++ internal/pkg/agent/application/upgrade/upgrade.go
@@ -23,6 +23,7 @@ import (
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/reexec"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/artifact"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/common"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/details"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/configuration"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
@@ -547,7 +548,7 @@ var copyRunDirectoryFunc = copyRunDirectory // abstraction for testability
 func copyRunDirectory(log *logger.Logger, oldRunPath, newRunPath string) error {
 	log.Infow("Copying run directory", "new_run_path", newRunPath, "old_run_path", oldRunPath)
 
-	if err := os.MkdirAll(newRunPath, runDirMod); err != nil {
+	if err := common.MkdirAll(newRunPath, runDirMod); err != nil {
 		return fmt.Errorf("failed to create run directory: %w", err)
 	}
 
