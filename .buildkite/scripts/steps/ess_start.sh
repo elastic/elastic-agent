c8c25797c85493f4a9d67d8014bd23dafae1b409
Shaunak Kashyap <ycombinator@gmail.com>
2025-08-14 09:23:28 -0700

    Only use STACK_VERSION when FIPS=true

--- .buildkite/scripts/steps/ess_start.sh
+++ .buildkite/scripts/steps/ess_start.sh
@@ -5,7 +5,12 @@ source .buildkite/scripts/steps/ess.sh
 source .buildkite/scripts/steps/fleet.sh
 
 STACK_VERSION="$(jq -r '.version' .package-version)"
-STACK_BUILD_ID="${STACK_BUILD_ID:-$(jq -r '.stack_build_id' .package-version)}"
+STACK_BUILD_ID="$(jq -r '.stack_build_id' .package-version)"
+if [[ "${FIPS:-false}" == "true" ]]; then
+  # FRH testing environment does not have same stack build IDs as CFT environment so
+  # we just go with the STACK_VERSION.
+  STACK_BUILD_ID=""
+fi
 ESS_REGION="${ESS_REGION:-gcp-us-west2}"
 
 ess_up "$STACK_VERSION" "$STACK_BUILD_ID" "$ESS_REGION"
