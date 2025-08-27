b2f34bec98b6e7054b15ff5faefed3ee4546db79
Roger Coll <rogercoll@protonmail.com>
2025-08-27 13:13:17 +0200

    Add the dockerstats Otel receiver (#9364)
    * add the dockerstats Otel receiver
    
    * test: add component to testdata
    
    ---------
    
    Co-authored-by: Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
    (cherry picked from commit 77c5f511a8f56521c5c3a11230483ef79b28a50b)
    
    # Conflicts:
    #	internal/pkg/otel/README.md
    #	internal/pkg/otel/components.go
    #	internal/pkg/otel/testdata/all-components.yml

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -13,6 +13,7 @@ import (
 	"go.opentelemetry.io/collector/receiver"
 
 	// Receivers:
+	dockerstatsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dockerstatsreceiver"
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
 	hostmetricsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
 	httpcheckreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/httpcheckreceiver"
@@ -83,6 +84,11 @@ func components(extensionFactories ...extension.Factory) func() (otelcol.Factori
 
 		// Receivers
 		receivers := []receiver.Factory{
+<<<<<<< HEAD
+=======
+			dockerstatsreceiver.NewFactory(),
+			elasticapmintakereceiver.NewFactory(),
+>>>>>>> 77c5f511a (Add the dockerstats Otel receiver (#9364))
 			otlpreceiver.NewFactory(),
 			filelogreceiver.NewFactory(),
 			kubeletstatsreceiver.NewFactory(),
