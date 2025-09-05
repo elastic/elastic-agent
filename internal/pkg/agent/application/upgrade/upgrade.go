2ef0cd733b12d82855c2afb93d685928b3571244
Paolo Chila <paolo.chila@elastic.co>
2025-09-05 13:22:04 +0200

    Introduce install descriptor

--- internal/pkg/agent/application/upgrade/upgrade.go
+++ internal/pkg/agent/application/upgrade/upgrade.go
@@ -16,7 +16,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/otiai10/copy"
+	filecopy "github.com/otiai10/copy"
 	"go.elastic.co/apm/v2"
 
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/filelock"
@@ -35,6 +35,7 @@ import (
 	"github.com/elastic/elastic-agent/internal/pkg/fleetapi/acker"
 	fleetclient "github.com/elastic/elastic-agent/internal/pkg/fleetapi/client"
 	"github.com/elastic/elastic-agent/internal/pkg/release"
+	v1 "github.com/elastic/elastic-agent/pkg/api/v1"
 	"github.com/elastic/elastic-agent/pkg/control/v2/client"
 	"github.com/elastic/elastic-agent/pkg/control/v2/cproto"
 	"github.com/elastic/elastic-agent/pkg/core/logger"
@@ -89,7 +90,7 @@ type unpackHandler interface {
 // Types used to abstract copyActionStore, copyRunDirectory and github.com/otiai10/copy.Copy
 type copyActionStoreFunc func(log *logger.Logger, newHome string) error
 type copyRunDirectoryFunc func(log *logger.Logger, oldRunPath, newRunPath string) error
-type fileDirCopyFunc func(from, to string, opts ...copy.Options) error
+type fileDirCopyFunc func(from, to string, opts ...filecopy.Options) error
 type markUpgradeFunc func(log *logger.Logger, dataDirPath string, updatedOn time.Time, agent, previousAgent agentInstall, action *fleetapi.ActionUpgrade, upgradeDetails *details.Details, rollbackWindow time.Duration) error
 type changeSymlinkFunc func(log *logger.Logger, topDirPath, symlinkPath, newTarget string) error
 type rollbackInstallFunc func(ctx context.Context, log *logger.Logger, topDirPath, versionedHome, oldVersionedHome string) error
@@ -161,7 +162,7 @@ func NewUpgrader(log *logger.Logger, settings *artifact.Config, upgradeConfig *c
 		isDiskSpaceErrorFunc: upgradeErrors.IsDiskSpaceError,
 		extractAgentVersion:  extractAgentVersion,
 		copyActionStore:      copyActionStoreProvider(os.ReadFile, os.WriteFile),
-		copyRunDirectory:     copyRunDirectoryProvider(os.MkdirAll, copy.Copy),
+		copyRunDirectory:     copyRunDirectoryProvider(os.MkdirAll, filecopy.Copy),
 		markUpgrade:          markUpgradeProvider(UpdateActiveCommit, os.WriteFile),
 		changeSymlink:        changeSymlink,
 		rollbackInstall:      rollbackInstall,
@@ -400,6 +401,9 @@ func (u *Upgrader) Upgrade(ctx context.Context, version string, rollback bool, s
 		return nil, err
 	}
 
+	//FIXME make it nicer
+	err = addInstallDesc(version, unpackRes.VersionedHome, unpackRes.Hash, detectedFlavor)
+
 	newHash := unpackRes.Hash
 	if newHash == "" {
 		return nil, errors.New("unknown hash")
@@ -496,6 +500,65 @@ func (u *Upgrader) Upgrade(ctx context.Context, version string, rollback bool, s
 	}
 
 	return cb, nil
+}
+
+func addInstallDesc(version string, home string, hash string, flavor string) error {
+	installMarkerFilePath := filepath.Join(paths.Top(), paths.MarkerFileName)
+	installDescriptor, err := readInstallMarker(installMarkerFilePath)
+	if err != nil {
+		return err
+	}
+
+	if installDescriptor == nil {
+		return fmt.Errorf("no install descriptor found at %q")
+	}
+
+	existingInstalls := installDescriptor.AgentInstalls
+	installDescriptor.AgentInstalls = make([]v1.AgentInstallDesc, len(existingInstalls)+1)
+	newInstall := v1.AgentInstallDesc{
+		Version:       version,
+		Hash:          hash,
+		VersionedHome: home,
+		Flavor:        flavor,
+	}
+	installDescriptor.AgentInstalls[0] = newInstall
+	copied := copy(installDescriptor.AgentInstalls[1:], existingInstalls)
+	if copied != len(existingInstalls) {
+		return fmt.Errorf("error adding new install %v to existing installs %v", newInstall, existingInstalls)
+	}
+
+	err = writeInstallMarker(installMarkerFilePath, installDescriptor)
+	if err != nil {
+		return fmt.Errorf("writing updated install marker: %w", err)
+	}
+
+	return nil
+}
+
+func writeInstallMarker(markerFilePath string, descriptor *v1.InstallDescriptor) error {
+	installMarkerFile, err := os.Create(markerFilePath)
+	if err != nil {
+		return fmt.Errorf("opening install marker file: %w", err)
+	}
+	defer func(installMarkerFile *os.File) {
+		_ = installMarkerFile.Close()
+	}(installMarkerFile)
+	return v1.WriteInstallDescriptor(installMarkerFile, descriptor)
+}
+
+func readInstallMarker(markerFilePath string) (*v1.InstallDescriptor, error) {
+	installMarkerFile, err := os.Open(markerFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("opening install marker file: %w", err)
+	}
+	defer func(installMarkerFile *os.File) {
+		_ = installMarkerFile.Close()
+	}(installMarkerFile)
+	installDescriptor, err := v1.ParseInstallDescriptor(installMarkerFile)
+	if err != nil {
+		return nil, fmt.Errorf("parsing install marker file: %w", err)
+	}
+	return installDescriptor, nil
 }
 
 func (u *Upgrader) rollbackToPreviousVersion(ctx context.Context, topDir string, now time.Time, version string, action *fleetapi.ActionUpgrade) (reexec.ShutdownCallbackFn, error) {
@@ -783,7 +846,7 @@ func shutdownCallback(l *logger.Logger, homePath, prevVersion, newVersion, newHo
 			newRelPath = strings.ReplaceAll(newRelPath, oldHome, newHome)
 			newDir := filepath.Join(newHome, newRelPath)
 			l.Debugf("copying %q -> %q", processDir, newDir)
-			if err := copyDir(l, processDir, newDir, true, copy.Copy); err != nil {
+			if err := copyDir(l, processDir, newDir, true, filecopy.Copy); err != nil {
 				return err
 			}
 		}
@@ -855,9 +918,9 @@ func copyDir(l *logger.Logger, from, to string, ignoreErrs bool, fileDirCopy fil
 		copyConcurrency = runtime.NumCPU() * 4
 	}
 
-	return fileDirCopy(from, to, copy.Options{
-		OnSymlink: func(_ string) copy.SymlinkAction {
-			return copy.Shallow
+	return fileDirCopy(from, to, filecopy.Options{
+		OnSymlink: func(_ string) filecopy.SymlinkAction {
+			return filecopy.Shallow
 		},
 		Sync:         true,
 		OnError:      onErr,
