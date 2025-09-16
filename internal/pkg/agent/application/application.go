3e1fc2ba144cee08fcd5174a4d96d3102f2999a0
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-09-16 17:06:30 +0300

    fix: ensure EDOT subprocess shuts down gracefully on agent termination (#9886)
    * fix: ensure EDOT subprocess shuts down gracefully on agent termination
    
    * fix: reword returned error
    
    * doc: add comment to describe the functionality of testing.go
    
    * fix: re-structure shutdown delay in test binary
    
    * fix: utilise t.SetEnv in unit-tests
    
    * feat: derive otel manager wait to stop timeout from agent.process.stop_timeout

--- internal/pkg/agent/application/application.go
+++ internal/pkg/agent/application/application.go
@@ -240,7 +240,7 @@ func New(
 		return nil, nil, nil, errors.New(err, "failed to initialize composable controller")
 	}
 
-	otelManager, err := otelmanager.NewOTelManager(log.Named("otel_manager"), logLevel, baseLogger, otelmanager.EmbeddedExecutionMode, agentInfo, monitor.ComponentMonitoringConfig)
+	otelManager, err := otelmanager.NewOTelManager(log.Named("otel_manager"), logLevel, baseLogger, otelmanager.EmbeddedExecutionMode, agentInfo, monitor.ComponentMonitoringConfig, cfg.Settings.ProcessConfig.StopTimeout)
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("failed to create otel manager: %w", err)
 	}
