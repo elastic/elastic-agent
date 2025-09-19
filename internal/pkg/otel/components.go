5a9fdd2aa931bf9adc729aa483852396c0bf683f
Vihas Makwana <121151420+VihasMakwana@users.noreply.github.com>
2025-09-19 16:14:04 +0530

    [EDOT] - add header setter extension (#9903)
    * add header setter extensions
    
    * go.mod and notice
    
    * revert otel.yml
    
    * add header setter to test config
    
    * spelling
    
    * go.mod
    
    * doc
    
    (cherry picked from commit 593a8b9056e3e4c12c5b272da9f23bdbc7c8ba38)
    
    # Conflicts:
    #	go.mod
    #	internal/pkg/otel/components.go

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -13,6 +13,7 @@ import (
 	"go.opentelemetry.io/collector/receiver"
 
 	// Receivers:
+	headersetterextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/headerssetterextension"
 	dockerstatsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dockerstatsreceiver"
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
 	hostmetricsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
@@ -167,6 +168,11 @@ func components(extensionFactories ...extension.Factory) func() (otelcol.Factori
 			k8sobserver.NewFactory(),
 			apikeyauthextension.NewFactory(),
 			apmconfigextension.NewFactory(),
+<<<<<<< HEAD
+=======
+			headersetterextension.NewFactory(),
+			beatsauthextension.NewFactory(),
+>>>>>>> 593a8b905 ([EDOT] - add header setter extension (#9903))
 		}
 		extensions = append(extensions, extensionFactories...)
 		factories.Extensions, err = otelcol.MakeFactoryMap[extension.Factory](extensions...)
