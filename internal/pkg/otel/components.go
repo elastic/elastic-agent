d411a4e93e03510ee79962341db3ee425cbae0c1
Vihas <vihas.makwana@elastic.co>
2025-09-22 13:01:39 +0530

    notice and conflicts

--- internal/pkg/otel/components.go
+++ internal/pkg/otel/components.go
@@ -13,11 +13,6 @@ import (
 	"go.opentelemetry.io/collector/receiver"
 
 	// Receivers:
-<<<<<<< HEAD
-=======
-	headersetterextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/headerssetterextension"
-	dockerstatsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dockerstatsreceiver"
->>>>>>> 593a8b905 ([EDOT] - add header setter extension (#9903))
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
 	hostmetricsreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
 	httpcheckreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/httpcheckreceiver"
@@ -63,6 +58,7 @@ import (
 
 	// Extensions
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/bearertokenauthextension"
+	headersetterextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/headerssetterextension"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
 	healthcheckv2extension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckv2extension"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/k8sleaderelector"
@@ -172,11 +168,7 @@ func components(extensionFactories ...extension.Factory) func() (otelcol.Factori
 			k8sobserver.NewFactory(),
 			apikeyauthextension.NewFactory(),
 			apmconfigextension.NewFactory(),
-<<<<<<< HEAD
-=======
 			headersetterextension.NewFactory(),
-			beatsauthextension.NewFactory(),
->>>>>>> 593a8b905 ([EDOT] - add header setter extension (#9903))
 		}
 		extensions = append(extensions, extensionFactories...)
 		factories.Extensions, err = otelcol.MakeFactoryMap[extension.Factory](extensions...)
