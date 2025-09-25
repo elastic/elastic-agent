8e32595a0fbc7629791affb98905ebf4096a4224
Vihas <vihas.makwana@elastic.co>
2025-09-25 15:46:58 +0530

    test

--- internal/pkg/otel/extension/elasticdiagnostics/extension_test.go
+++ internal/pkg/otel/extension/elasticdiagnostics/extension_test.go
@@ -10,7 +10,6 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"path/filepath"
 	"testing"
 	"time"
 
@@ -23,63 +22,49 @@ import (
 
 	"github.com/elastic/elastic-agent/internal/pkg/otel/extension/elasticdiagnostics/internal/metadata"
 	"github.com/elastic/elastic-agent/pkg/control/v2/client"
+	"github.com/elastic/elastic-agent/pkg/utils"
 )
 
 func TestExtension(t *testing.T) {
 	temp := t.TempDir()
-	tests := []struct {
-		name     string
-		endpoint string
-	}{
-		{
-			name:     "default config",
-			endpoint: filepath.Join(temp, "edot.sock"),
+	config := createDefaultConfig().(*Config)
+	config.Endpoint = utils.SocketURLWithFallback("edot.sock", temp)
+
+	ext, err := NewFactory().Create(context.Background(), extension.Settings{
+		TelemetrySettings: component.TelemetrySettings{
+			Logger: zap.NewNop(),
 		},
-		{
-			name:     "config with subdirectory",
-			endpoint: filepath.Join(temp, "elastic-agent", "edot.sock"),
+		ID: component.NewID(metadata.Type),
+	}, config)
+	require.NoError(t, err)
+	require.NotNil(t, ext)
+
+	require.NoError(t, ext.Start(context.Background(), componenttest.NewNopHost()))
+	defer func() {
+		require.NoError(t, ext.Shutdown(context.Background()))
+	}()
+
+	tr := &http.Transport{
+		DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+			return client.Dialer(ctx, config.Endpoint)
 		},
 	}
-	for _, test := range tests {
-		config := createDefaultConfig().(*Config)
-		config.Endpoint = test.endpoint
+	client := &http.Client{Transport: tr}
 
-		ext, err := NewFactory().Create(context.Background(), extension.Settings{
-			TelemetrySettings: component.TelemetrySettings{
-				Logger: zap.NewNop(),
-			},
-			ID: component.NewID(metadata.Type),
-		}, config)
-		require.NoError(t, err)
-		require.NotNil(t, ext)
+	require.EventuallyWithT(t, func(collect *assert.CollectT) {
+		req, err := http.NewRequest(http.MethodGet, "http://localhost/diagnostics", nil)
+		require.NoError(collect, err)
+		resp, err := client.Do(req.WithContext(context.Background()))
+		require.NoError(collect, err)
+		require.Equal(collect, http.StatusOK, resp.StatusCode)
 
-		require.NoError(t, ext.Start(context.Background(), componenttest.NewNopHost()))
-		defer func() {
-			require.NoError(t, ext.Shutdown(context.Background()))
-		}()
+		b, err := io.ReadAll(resp.Body)
+		require.NoError(collect, err)
+		res := Response{}
 
-		tr := &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return client.Dialer(ctx, test.endpoint)
-			},
-		}
-		client := &http.Client{Transport: tr}
+		// test that the response can be unmarshalled
+		require.NoErrorf(collect, json.Unmarshal(b, &res), "failed to unmarshal response: %s", string(b))
 
-		require.EventuallyWithT(t, func(collect *assert.CollectT) {
-			req, err := http.NewRequest(http.MethodGet, "http://localhost/diagnostics", nil)
-			require.NoError(collect, err)
-			resp, err := client.Do(req.WithContext(context.Background()))
-			require.NoError(collect, err)
-			require.Equal(collect, http.StatusOK, resp.StatusCode)
-
-			b, err := io.ReadAll(resp.Body)
-			require.NoError(collect, err)
-			res := Response{}
-
-			// test that the response can be unmarshalled
-			require.NoErrorf(collect, json.Unmarshal(b, &res), "failed to unmarshal response: %s", string(b))
-
-			require.NoError(collect, resp.Body.Close())
-		}, 5*time.Second, 100*time.Millisecond, "extension did not start in time")
-	}
+		require.NoError(collect, resp.Body.Close())
+	}, 5*time.Second, 100*time.Millisecond, "extension did not start in time")
 }
