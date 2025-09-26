9ebe8bbabce32692fc5d47bbdf5f08e31f8d81dd
Shaunak Kashyap <ycombinator@gmail.com>
2025-09-26 06:19:24 -0700

    Append requirefips build tag for fips140=only unit tests

--- magefile.go
+++ magefile.go
@@ -537,6 +537,7 @@ func (Test) FIPSOnlyUnit(ctx context.Context) error {
 	params := devtools.DefaultGoTestUnitArgs()
 	params.Env["FIPS"] = "true"
 	params.Env["GODEBUG"] = "fips140=only"
+	params.Tags = append(params.Tags, "requirefips")
 	return devtools.GoTest(ctx, params)
 }
 
