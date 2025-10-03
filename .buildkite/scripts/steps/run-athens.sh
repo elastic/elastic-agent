6dbd4a6406349f651aa19cd9b8e0f63597fc689a
Michal Pristas <michal.pristas@elastic.co>
2025-10-03 10:43:18 +0200

    script

--- .buildkite/scripts/steps/run-athens.sh
+++ .buildkite/scripts/steps/run-athens.sh
@@ -10,3 +10,7 @@ docker run -d --rm \
   -e ATHENS_GOGET_STORAGE_TYPE=memory \
   --name athens-proxy \
   gomods/athens:latest
+
+ATHENS_IP=$(ip route get 1 | awk '{print $7; exit}')
+
+buildkite-agent meta-data env set GOPROXY "$ATHENS_IP:3000"
