f58f63d5045bc705b0137581326ec0968d20717e
Michal Pristas <michal.pristas@elastic.co>
2025-10-03 09:54:37 +0200

    script

---
+++ .buildkite/scripts/steps/run-athens.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common.sh
+
+docker run -d --rm \
+  -p 3000:3000 \
+  -e ATHENS_STORAGE_TYPE=memory \
+  -e ATHENS_GOGET_STORAGE_TYPE=memory \
+  gomods/athens:latest
