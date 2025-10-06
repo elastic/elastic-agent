f627db993c8e3318daa80e1e82cd6fae35c4a19e
Hayot <hayot@bisonai.com>
2025-10-06 11:28:53 +0900

    fix: handle error nil case

--- internal/pkg/agent/application/enroll/enroll.go
+++ internal/pkg/agent/application/enroll/enroll.go
@@ -95,17 +95,21 @@ func EnrollWithBackoff(
 
 RETRYLOOP:
 	for {
+		if err == nil {
+			break RETRYLOOP
+		}
+
 		switch {
 		case errors.Is(err, fleetapi.ErrTooManyRequests):
 			log.Warn("Too many requests on the remote server, will retry in a moment.")
 		case errors.Is(err, fleetapi.ErrConnRefused):
-			log.Warn("Remote server is not ready to accept connections(Connection Refused), will retry in a moment.")
+			log.Warn("Remote server is not ready to accept connections (Connection Refused), will retry in a moment.")
 		case errors.Is(err, fleetapi.ErrTemporaryServerError):
-			log.Warnf("Remote server failed to handle the request(%s), will retry in a moment.", err.Error())
-		case errors.Is(err, context.Canceled), errors.Is(err, context.DeadlineExceeded), err == nil:
+			log.Warnf("Remote server failed to handle the request (%s), will retry in a moment.", err)
+		case errors.Is(err, context.Canceled), errors.Is(err, context.DeadlineExceeded):
 			break RETRYLOOP
-		case err != nil:
-			log.Warnf("Error detected: %s, will retry in a moment.", err.Error())
+		default:
+			log.Warnf("Error detected: %s, will retry in a moment.", err)
 		}
 		if !backExp.Wait() {
 			break RETRYLOOP
