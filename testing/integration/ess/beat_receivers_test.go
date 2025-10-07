7a7bedb0cc502dede85274f5297ea671d0646855
Khushi Jain <khushi.jain@elastic.co>
2025-10-07 17:17:26 +0530

    make check-ci

--- testing/integration/ess/beat_receivers_test.go
+++ testing/integration/ess/beat_receivers_test.go
@@ -1,6 +1,7 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
+
 //go:build integration
 
 package ess
@@ -9,11 +10,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
 	"runtime"
 	"strings"
 	"testing"
