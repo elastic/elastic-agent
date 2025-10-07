527be2f06ce4d654d563e613af1f42924fde7ad0
Khushi Jain <khushi.jain@elastic.co>
2025-10-07 16:46:59 +0530

    fix conflicts

--- testing/integration/ess/beat_receivers_test.go
+++ testing/integration/ess/beat_receivers_test.go
@@ -34,7 +34,6 @@ import (
 	"github.com/elastic/elastic-agent/testing/integration"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/gofrs/uuid/v5"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
