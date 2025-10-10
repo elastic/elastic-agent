edabec47c8cb0f3bf0830675b15be645db3cb7a4
Lee E. Hinman <lee.e.hinman@elastic.co>
2025-10-09 21:21:34 -0500

    make linter happy

--- pkg/testing/multipass/provisioner.go
+++ pkg/testing/multipass/provisioner.go
@@ -170,7 +170,7 @@ func (p *provisioner) launch(ctx context.Context, cfg common.Config, batch commo
 	if err != nil {
 		return fmt.Errorf("failed to run multipass launch: %w", err)
 	}
-	_, err = proc.Stdin.Write([]byte(fmt.Sprintf("#cloud-config\n%s", cloudCfgData)))
+	_, err = fmt.Fprintf(proc.Stdin, "#cloud-config\n%s", cloudCfgData)
 	if err != nil {
 		_ = proc.Stdin.Close()
 		_ = proc.Kill()
