fb74ccfeff39089e6eeaa24ef0f74e591c43fb8d
Stefan Traistaru <stefan.traistaru@elastic.co>
2025-10-10 14:51:53 +0300

    Fix troubleshooting message URL for versions after 9.x (#10218)
    * Fix troubleshooting message URL for versions after 9.x
    
    * Add changelog fragment
    
    * Change troubleshoot message to a const variable
    
    * Fix build issue created by the merge
    
    * Fix linter issue
    
    * Revert linter fix and add nolint comment
    
    (cherry picked from commit b74942add0a8066bd6939a1a7fe374201f207058)

--- internal/pkg/agent/cmd/run.go
+++ internal/pkg/agent/cmd/run.go
@@ -88,7 +88,7 @@ func newRunCommandWithArgs(_ []string, streams *cli.IOStreams) *cobra.Command {
 			fleetInitTimeout, _ := cmd.Flags().GetDuration("fleet-init-timeout")
 			testingMode, _ := cmd.Flags().GetBool("testing-mode")
 			if err := run(nil, testingMode, fleetInitTimeout); err != nil && !errors.Is(err, context.Canceled) {
-				fmt.Fprintf(streams.Err, "Error: %v\n%s\n", err, troubleshootMessage())
+				fmt.Fprintf(streams.Err, "Error: %v\n%s\n", err, troubleshootMessage)
 				return err
 			}
 			return nil
@@ -356,7 +356,7 @@ func runElasticAgent(
 		return err
 	}
 
-	monitoringServer, err := setupMetrics(l, cfg.Settings.DownloadConfig.OS(), cfg.Settings.MonitoringConfig, tracer, coord)
+	monitoringServer, err := setupMetrics(l, cfg.Settings.MonitoringConfig, tracer, coord)
 	if err != nil {
 		return err
 	}
@@ -715,12 +715,11 @@ func initTracer(agentName, version string, mcfg *monitoringCfg.MonitoringConfig)
 
 func setupMetrics(
 	logger *logger.Logger,
-	operatingSystem string,
 	cfg *monitoringCfg.MonitoringConfig,
 	tracer *apm.Tracer,
 	coord *coordinator.Coordinator,
 ) (*reload.ServerReloader, error) {
-	if err := report.SetupMetrics(logger, agentName, version.GetDefaultVersion()); err != nil {
+	if err := report.SetupMetrics(logger, agentName, version.GetDefaultVersion()); err != nil { //nolint:staticcheck // ignore deprecation
 		return nil, err
 	}
 
