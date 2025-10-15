a3703f95b5a13a7048da76bf03de27575a63c5a5
Njal Karevoll <njal@karevoll.no>
2025-10-15 11:29:46 +0200

    add some output to make it a bit easier to follow the build

--- .buildkite/scripts/steps/ecp-internal-release.sh
+++ .buildkite/scripts/steps/ecp-internal-release.sh
@@ -32,34 +32,50 @@ write_annotation() {
     cat $BUILDKITE_ANNOTATE_FILE | buildkite-agent annotate --style info
 }
 
+echo "--- :package: Preparing build information"
 BUILD_VERSION="$(jq -r '.version' .package-version)"
 DOCKER_TAG="git-${VERSION}"
 PRIVATE_REPO="docker.elastic.co/observability-ci/ecp-elastic-agent-service"
 PRIVATE_IMAGE="${PRIVATE_REPO}:${DOCKER_TAG}"
 
-# download the amd64 and arm64 builds of the image from the previous steps
+echo "Build version: ${BUILD_VERSION}"
+echo "Docker tag: ${DOCKER_TAG}"
+echo "Target image: ${PRIVATE_IMAGE}"
+
+echo "--- :arrow_down: Downloading build artifacts"
+echo "Downloading AMD64 build artifacts..."
 buildkite-agent artifact download "build/distributions/**" . --step "packaging-service-container-amd64"
+echo "Downloading ARM64 build artifacts..."
 buildkite-agent artifact download "build/distributions/**" . --step "packaging-service-container-arm64"
 
-# AMD64
+echo "--- :docker: Processing AMD64 image"
+echo "Loading AMD64 image..."
 docker load -i ./build/distributions/elastic-agent-service-$DOCKER_TAG-$BUILD_VERSION-linux-amd64.docker.tar.gz
+echo "Tagging AMD64 image as ${PRIVATE_IMAGE}..."
 docker image tag "elastic-agent-service:$DOCKER_TAG" "$PRIVATE_IMAGE"
+echo "Pushing AMD64 image..."
 docker push "$PRIVATE_IMAGE"
 AMD64_DIGEST=$(docker image inspect --format "{{index .RepoDigests 0}}" "$PRIVATE_IMAGE")
+echo "AMD64 digest: ${AMD64_DIGEST}"
 
-# ARM64 (overwrites AMD64 tags)
+echo "--- :docker: Processing ARM64 image"
+echo "Loading ARM64 image..."
 docker load -i ./build/distributions/elastic-agent-service-$DOCKER_TAG-$BUILD_VERSION-linux-arm64.docker.tar.gz
+echo "Tagging ARM64 image as ${PRIVATE_IMAGE}..."
 docker image tag "elastic-agent-service:$DOCKER_TAG" "$PRIVATE_IMAGE"
+echo "Pushing ARM64 image..."
 docker push "$PRIVATE_IMAGE"
 ARM64_DIGEST=$(docker image inspect --format "{{index .RepoDigests 0}}" "$PRIVATE_IMAGE")
+echo "ARM64 digest: ${ARM64_DIGEST}"
 
-# at this point the $PRIVATE_IMAGE is pointing to only the arm64 based image, we need the image to
-# be a multi-architecture based image so we create an image from the digests and tag it the same and
-# push it to the registry (aka. make the tag now a multi-architecture based image)
+echo "--- :rocket: Creating multi-architecture manifest"
+echo "Creating multi-arch image from digests..."
 docker buildx imagetools create -t "$PRIVATE_IMAGE" \
   "$AMD64_DIGEST" \
   "$ARM64_DIGEST"
+echo "Multi-architecture image created and pushed successfully"
 
+echo "--- :memo: Creating build annotation and metadata"
 annotate "* Image: $PRIVATE_IMAGE"
 annotate "* Short commit: $VERSION"
 annotate "* Commit: https://github.com/elastic/elastic-agent/commit/$VERSION"
