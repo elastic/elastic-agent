a9e6548b0361a16df0e7a3b1dcb28143daea112d
Olga Naydyonock <olga.naidjonoka@elastic.co>
2025-10-16 17:47:01 +0300

    PR adjustments according to coomments

--- pkg/testing/tools/git/git_test.go
+++ pkg/testing/tools/git/git_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Test require full git history, otherwise it will fail on the CI
 func TestGetReleaseBranches(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
