f950f1ace3930ce086f1dcbb7828d7c11990be2e
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-10-21 16:38:12 +0200

    Make otel runtime component updates synchronous (#10675)
    * Make otel runtime component updates synchronous
    
    * Make the regression test runnable on main
    
    * Add comments on final status in collector executions
    
    * Add some more comments

--- internal/pkg/otel/manager/execution.go
+++ internal/pkg/otel/manager/execution.go
@@ -15,6 +15,11 @@ import (
 )
 
 type collectorExecution interface {
+	// startCollector starts the otel collector with the given arguments, returning a handle allowing it to be stopped.
+	// Cancelling the context will stop all goroutines involved in the execution.
+	// The collector will report status events in the statusCh channel and errors on errCh in a non-blocking fashion,
+	// draining the channel before writing to it.
+	// After the collector exits, it will emit an error describing the exit status (nil if successful) and a nil status.
 	startCollector(ctx context.Context, logger *logger.Logger, cfg *confmap.Conf, errCh chan error, statusCh chan *status.AggregateStatus) (collectorHandle, error)
 }
 
