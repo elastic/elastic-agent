eba6529ca2cccb9e121dab9a7428bc7a746280db
Vihas <vihas.makwana@elastic.co>
2025-10-28 18:50:37 +0530

    use observer and t.Setenv

--- internal/pkg/otel/samples_test.go
+++ internal/pkg/otel/samples_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/elastic/elastic-agent/pkg/core/logger/loggertest"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/otelcol"
 	"go.uber.org/zap"
@@ -21,25 +22,15 @@ import (
 func TestSamples(t *testing.T) {
 	// Set environment variables required by the sample configurations.
 	// These are just sample values to allow the collector to start.
-	os.Setenv("STORAGE_DIR", t.TempDir())
-	os.Setenv("ELASTIC_ENDPOINT", "http://localhost:9200")
-	os.Setenv("ELASTIC_API_KEY", "test_api_key")
-	os.Setenv("ELASTIC_OTLP_ENDPOINT", "http://localhost:4318")
-	os.Setenv("AUTOOPS_ES_URL", "http://localhost:9200")
-	os.Setenv("AUTOOPS_TOKEN", "token")
-	os.Setenv("AUTOOPS_TEMP_RESOURCE_ID", "temp")
-	os.Setenv("AUTOOPS_OTEL_URL", "http://localhost:4318")
+	t.Setenv("STORAGE_DIR", t.TempDir())
+	t.Setenv("ELASTIC_ENDPOINT", "http://localhost:9200")
+	t.Setenv("ELASTIC_API_KEY", "test_api_key")
+	t.Setenv("ELASTIC_OTLP_ENDPOINT", "http://localhost:4318")
+	t.Setenv("AUTOOPS_ES_URL", "http://localhost:9200")
+	t.Setenv("AUTOOPS_TOKEN", "token")
+	t.Setenv("AUTOOPS_TEMP_RESOURCE_ID", "temp")
+	t.Setenv("AUTOOPS_OTEL_URL", "http://localhost:4318")
 
-	defer func() {
-		os.Unsetenv("STORAGE_DIR")
-		os.Unsetenv("ELASTIC_ENDPOINT")
-		os.Unsetenv("ELASTIC_API_KEY")
-		os.Unsetenv("ELASTIC_OTLP_ENDPOINT")
-		os.Unsetenv("AUTOOPS_ES_URL")
-		os.Unsetenv("AUTOOPS_TOKEN")
-		os.Unsetenv("AUTOOPS_TEMP_RESOURCE_ID")
-		os.Unsetenv("AUTOOPS_OTEL_URL")
-	}()
 	err := filepath.WalkDir(filepath.Join(".", "samples", runtime.GOOS), func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -55,9 +46,17 @@ func TestSamples(t *testing.T) {
 }
 
 func testSample(t *testing.T, configFile string) {
+	base, obs := loggertest.New("otel")
+	t.Cleanup(func() {
+		if t.Failed() {
+			for _, log := range obs.All() {
+				t.Logf("%+v", log)
+			}
+		}
+	})
 	settings := NewSettings("test", []string{configFile})
 	settings.LoggingOptions = []zap.Option{zap.WrapCore(func(zapcore.Core) zapcore.Core {
-		return zapcore.NewNopCore()
+		return base.Core()
 	})}
 	collector, err := otelcol.NewCollector(*settings)
 	assert.NoError(t, err)
