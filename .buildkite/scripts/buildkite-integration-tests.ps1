979bb839021316bef3287624452a47a430f596b2
Paolo Chila <paolo.chila@elastic.co>
2025-10-30 21:05:08 +0100

    fixup! Fix check for PR vs branch run

--- .buildkite/scripts/buildkite-integration-tests.ps1
+++ .buildkite/scripts/buildkite-integration-tests.ps1
@@ -71,7 +71,7 @@ try
     # while still enforcing a reasonable upper limit on total execution time.
     # See: https://pkg.go.dev/cmd/go#hdr-Testing_flags
     $gotestFlags = @("-test.shuffle=on", "-test.timeout=2h0m0s")
-    if(-not ([string]::IsNullOrEmpty($env:BUILDKITE_PULL_REQUEST)))
+    if(-not $env:BUILDKITE_PULL_REQUEST -eq "false")
     {
         $gotestFlags += "-test.short"
     }
