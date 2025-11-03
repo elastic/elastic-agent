4ab6cf6dfb9724afb300788b93d620a631bcbc1c
Vihas Makwana <121151420+VihasMakwana@users.noreply.github.com>
2025-11-04 01:32:37 +0530

    [beatsreceivers] add option to mute exporter status (#10890)
    * add option to skip output status
    
    * fix test
    
    * status reporting
    
    * test case fix
    
    * skip yaml
    
    * test
    
    * fix test
    
    * pipeline status and review comments
    
    * review comments and testing
    
    * comment
    
    * lint
    
    * rename channel parameter to ensure consitency
    
    * integration test
    
    * remove os arch hardcoded
    
    * review comments
    
    * test fix
    
    * move force fetch if config hasn't changed and fix test
    
    (cherry picked from commit 83880d318a6ad9d01d63e613cf2fdc2df3584be3)

--- testing/integration/ess/otel_test.go
+++ testing/integration/ess/otel_test.go
@@ -2218,3 +2218,128 @@ service:
 
 	cancel()
 }
+
+func TestOutputStatusReporting(t *testing.T) {
+	define.Require(t, define.Requirements{
+		Sudo:  true,
+		Group: integration.Default,
+		Local: false,
+		Stack: nil,
+		OS: []define.OS{
+			{Type: define.Windows},
+			{Type: define.Linux},
+			{Type: define.Darwin},
+		},
+	})
+
+	fixture, err := define.NewFixtureFromLocalBuild(t, define.Version())
+	require.NoError(t, err)
+
+	// Create the otel configuration file
+	type otelConfigOptions struct {
+		StatusReportingEnabled bool
+	}
+	configTemplate := `
+inputs:
+  - type: system/metrics
+    id: http-metrics-test
+    use_output: default
+    _runtime_experimental: otel
+    streams:
+    - metricsets:
+       - cpu
+      period: 1s
+      data_stream:
+        dataset: e2e
+      namespace: "json_namespace"
+agent.reload:
+  period: 1s
+outputs:
+  default:
+    type: elasticsearch
+    hosts: [http://localhost:9200]
+    api_key: placeholder
+    preset: "balanced"
+    status_reporting:
+      enabled: {{.StatusReportingEnabled}}
+agent.monitoring:
+  metrics: false
+  logs: false
+  http:
+    enabled: true
+    port: 6792
+agent.grpc:
+    port: 6790
+`
+
+	var configBuffer bytes.Buffer
+	template.Must(template.New("config").Parse(configTemplate)).Execute(&configBuffer,
+		otelConfigOptions{
+			StatusReportingEnabled: true,
+		})
+	ctx, cancel := testcontext.WithDeadline(t, context.Background(), time.Now().Add(5*time.Minute))
+	defer cancel()
+
+	installOpts := aTesting.InstallOpts{
+		NonInteractive: true,
+		Privileged:     true,
+		Force:          true,
+		Develop:        true,
+	}
+
+	err = fixture.Prepare(ctx)
+	require.NoError(t, err)
+
+	err = fixture.Configure(ctx, configBuffer.Bytes())
+
+	output, err := fixture.InstallWithoutEnroll(ctx, &installOpts)
+	require.NoErrorf(t, err, "error install withouth enroll: %s\ncombinedoutput:\n%s", err, string(output))
+
+	require.Eventually(t, func() bool {
+		status, err := fixture.ExecStatus(ctx)
+		if err != nil {
+			t.Logf("waiting for agent degraded: %s", err.Error())
+			return false
+		}
+		return status.State == int(cproto.State_DEGRADED)
+	}, 30*time.Second, 1*time.Second)
+
+	// Disable status reporting.
+	// This should result in HEALTHY state
+	configBuffer.Reset()
+	template.Must(template.New("config").Parse(configTemplate)).Execute(&configBuffer,
+		otelConfigOptions{
+			StatusReportingEnabled: false,
+		})
+	err = fixture.Configure(ctx, configBuffer.Bytes())
+	require.NoError(t, err)
+	require.Eventually(t, func() bool {
+		err = fixture.IsHealthy(ctx)
+		if err != nil {
+			t.Logf("waiting for agent healthy: %s", err.Error())
+			return false
+		}
+		return true
+	}, 1*time.Minute, 1*time.Second)
+
+	// Enabled status reporting and keep using localhost.
+	// This should result in DEGRADED state
+	configBuffer.Reset()
+	template.Must(template.New("config").Parse(configTemplate)).Execute(&configBuffer,
+		otelConfigOptions{
+			StatusReportingEnabled: true,
+		})
+	err = fixture.Configure(ctx, configBuffer.Bytes())
+	require.NoError(t, err)
+	require.Eventually(t, func() bool {
+		status, err := fixture.ExecStatus(ctx)
+		if err != nil {
+			t.Logf("waiting for agent degraded: %s", err.Error())
+			return false
+		}
+		return status.State == int(cproto.State_DEGRADED)
+	}, 30*time.Second, 1*time.Second)
+
+	combinedOutput, err := fixture.Uninstall(ctx, &aTesting.UninstallOpts{Force: true})
+	require.NoErrorf(t, err, "error uninstalling classic agent monitoring, err: %s, combined output: %s", err, string(combinedOutput))
+}
