3eec160fee5504707e63882bafd1213fd34d4a36
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-11-06 17:40:11 +0100

    refreshenv in Windows bootstrap

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,3 +1,4 @@
 # Shorten BUILDKITE_MESSAGE if needed to avoid filling the Windows env var buffer
 $env:BUILDKITE_MESSAGE = $env:BUILDKITE_MESSAGE.Substring(0, [System.Math]::Min(2048, $env:BUILDKITE_MESSAGE.Length))
 choco install awscli jq -y
+refreshenv
