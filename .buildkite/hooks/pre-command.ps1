852655161eb5d7bc499a36c362869f43b4c60d03
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-11-12 20:27:18 +0100

    Troubleshooting

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,6 +1,8 @@
 # Shorten BUILDKITE_MESSAGE if needed to avoid filling the Windows env var buffer
 $env:BUILDKITE_MESSAGE = $env:BUILDKITE_MESSAGE.Substring(0, [System.Math]::Min(2048, $env:BUILDKITE_MESSAGE.Length))
 choco install awscli jq -y
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Output "Env Path Before: $env:Path"
+$env:Path += ";C:\Program Files\Amazon\AWSCLIV2\"
+Write-Output "Env Path After: $env:Path"
 Write-Output "AWS CLI installed. Version:"
 aws --version
