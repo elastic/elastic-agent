6924117e6521e13a506f6ae08542ea494959b8a2
Tiago Queiroz <tiago.queiroz@elastic.co>
2025-11-12 17:56:42 -0500

    Refactor

--- testing/integration/ess/otel_log_as_filestream_test.go
+++ testing/integration/ess/otel_log_as_filestream_test.go
@@ -9,21 +9,21 @@ package ess
 import (
 	"bytes"
 	"context"
-	"encoding/json"
-	"errors"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 	"text/template"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	libbeatinteg "github.com/elastic/beats/v7/libbeat/tests/integration"
 	"github.com/elastic/elastic-agent-libs/testing/estools"
-	aTesting "github.com/elastic/elastic-agent/pkg/testing"
+	atesting "github.com/elastic/elastic-agent/pkg/testing"
 	"github.com/elastic/elastic-agent/pkg/testing/define"
 	"github.com/elastic/elastic-agent/pkg/testing/tools/testcontext"
 	"github.com/elastic/elastic-agent/testing/integration"
-	"github.com/stretchr/testify/require"
 )
 
 func TestFilebeatReceiverLogAsFilestream(t *testing.T) {
@@ -38,17 +38,6 @@ func TestFilebeatReceiverLogAsFilestream(t *testing.T) {
 		},
 	})
 
-	type otelConfigOptions struct {
-		Message      string
-		Output       string
-		HomeDir      string
-		LogFilepath  string
-		AsFilestream bool
-		ESApiKey     string
-		ESEndpoint   string
-		Namespace    string
-	}
-
 	tmpDir := t.TempDir()
 	tmpDir = os.TempDir()
 	logFilepath := filepath.Join(tmpDir, "log.log")
@@ -56,17 +45,17 @@ func TestFilebeatReceiverLogAsFilestream(t *testing.T) {
 	libbeatinteg.WriteLogFile(t, logFilepath, numEvents, false)
 
 	exporterOutputPath := filepath.Join(tmpDir, "output.json")
-	// t.Cleanup(func() {
-	// 	if t.Failed() {
-	// 		contents, err := os.ReadFile(exporterOutputPath)
-	// 		if err != nil {
-	// 			t.Logf("No exporter output file")
-	// 			return
-	// 		}
-	// 		t.Logf("Otel output file path: %s", exporterOutputPath)
-	// 		t.Logf("Contents of exporter output file:\n%s\n", string(contents))
-	// 	}
-	// })
+	t.Cleanup(func() {
+		if t.Failed() {
+			contents, err := os.ReadFile(exporterOutputPath)
+			if err != nil {
+				t.Logf("No exporter output file")
+				return
+			}
+			t.Logf("Otel output file path: %s", exporterOutputPath)
+			t.Logf("Contents of exporter output file:\n%s\n", string(contents))
+		}
+	})
 
 	otelConfigPath := filepath.Join(tmpDir, "otel.yml")
 	otelConfigTemplate := `receivers:
@@ -116,60 +105,28 @@ service:
 	require.NoError(t, err, "failed to get ES host")
 	require.True(t, len(esHost) > 0)
 
-	var otelConfigBuffer bytes.Buffer
-	require.NoError(t,
-		template.Must(
-			template.New("otelConfig").
-				Parse(otelConfigTemplate)).
-			Execute(
-				&otelConfigBuffer,
-				otelConfigOptions{
-					Output:      exporterOutputPath,
-					HomeDir:     tmpDir,
-					LogFilepath: logFilepath,
-					ESApiKey:    esApiKey.Encoded,
-					ESEndpoint:  esHost,
-					Namespace:   info.Namespace,
-				}))
-	require.NoError(t, os.WriteFile(otelConfigPath, otelConfigBuffer.Bytes(), 0o600))
-	// t.Cleanup(func() {
-	// 	if t.Failed() {
-	// 		contents, err := os.ReadFile(otelConfigPath)
-	// 		if err != nil {
-	// 			t.Logf("no otel config file")
-	// 			return
-	// 		}
-	// 		t.Logf("Contents of otel config file:\n%s\n", string(contents))
-	// 	}
-	// })
+	ctx, cancel := testcontext.WithDeadline(t, context.Background(), time.Now().Add(1*time.Minute))
+	defer cancel()
 
 	fixture, err := define.NewFixtureFromLocalBuild(
 		t,
-		define.Version(),
-		aTesting.WithAdditionalArgs([]string{"--config", otelConfigPath}))
-	require.NoError(t, err)
-
-	ctx, cancel := testcontext.WithDeadline(t, context.Background(), time.Now().Add(1*time.Minute))
-	defer cancel()
-	err = fixture.Prepare(ctx, fakeComponent)
-	require.NoError(t, err)
-
-	cmd, err := fixture.PrepareAgentCommand(
-		ctx,
-		[]string{"otel", "--config", otelConfigPath, "-e"},
-	)
+		define.Version())
 	require.NoError(t, err)
 
 	f := NewLogFile(t)
 	f.KeepLogFile = true
 
-	cmd.Stderr = f
-	cmd.Stdout = f
-
-	if err := cmd.Start(); err != nil {
-		t.Errorf("cannot start Elastic Agent in OTel mode: %s", err)
+	cfg := map[string]any{
+		"Output":       exporterOutputPath,
+		"HomeDir":      tmpDir,
+		"LogFilepath":  logFilepath,
+		"ESApiKey":     esApiKey.Encoded,
+		"ESEndpoint":   esHost,
+		"Namespace":    info.Namespace,
+		"AsFilestream": false,
 	}
 
+	cmd := start(t, ctx, otelConfigTemplate, otelConfigPath, cfg, fixture, f.File)
 	f.WaitLogsContains(
 		t,
 		"Log input running as Log input",
@@ -196,6 +153,93 @@ service:
 		30*time.Second, 1*time.Second,
 		"Expected %v logs", numEvents)
 
+	stop(t, cmd, f)
+
+	//================================================== Run again
+	t.Log("================================================== RUNNING AGAIN")
+	cmd = start(t, ctx, otelConfigTemplate, otelConfigPath, cfg, fixture, f.File)
+
+	t.Log("==================== Waiting for the second start")
+	f.WaitLogsContains(
+		t,
+		"Log input running as Log input",
+		20*time.Second,
+		"Log input did not start as Log input",
+	)
+
+	// Ensure no new data has been added
+	f.WaitLogsContains(
+		t,
+		"File didn't change: /tmp/log.log",
+		20*time.Second,
+		"did not reach EOF")
+
+	stop(t, cmd, f)
+
+	cfg["AsFilestream"] = true
+	cmd = start(t, ctx, otelConfigTemplate, otelConfigPath, cfg, fixture, f.File)
+	t.Log("==================== Waiting for the third start")
+	f.WaitLogsContains(
+		t,
+		"Log input running as Filestream input",
+		20*time.Second,
+		"Log input did not start as Filestream input",
+	)
+
+	f.WaitLogsContains(
+		t,
+		"Input 'filestream' starting",
+		20*time.Second,
+		"Filestream did not start",
+	)
+
+	f.WaitLogsContains(
+		t,
+		"End of file reached: /tmp/log.log; Backoff now.",
+		20*time.Second,
+		"Filestream did not reach EOF")
+
+	stop(t, cmd, f)
+}
+
+func start(
+	t *testing.T,
+	ctx context.Context,
+	otelConfigTemplate string,
+	otelConfigPath string,
+	cfg map[string]any,
+	fixture *atesting.Fixture,
+	f *os.File) *exec.Cmd {
+
+	otelConfigBuffer := bytes.Buffer{}
+	require.NoError(t,
+		template.Must(
+			template.New("otelConfig").
+				Parse(otelConfigTemplate)).
+			Execute(
+				&otelConfigBuffer,
+				cfg))
+	require.NoError(t, os.WriteFile(otelConfigPath, otelConfigBuffer.Bytes(), 0o600))
+
+	t.Log("================================================== Start starting")
+
+	cmd, err := fixture.PrepareAgentCommand(
+		ctx,
+		[]string{"otel", "--config", otelConfigPath},
+	)
+	require.NoError(t, err)
+
+	cmd.Stderr = f
+	cmd.Stdout = f
+
+	if err := cmd.Start(); err != nil {
+		t.Errorf("cannot start Elastic Agent in OTel mode: %s", err)
+	}
+
+	return cmd
+}
+
+func stop(t *testing.T, cmd *exec.Cmd, f *LogFile) {
 	t.Log("==================== Sending Interrupt signal")
 	if err := cmd.Process.Signal(os.Interrupt); err != nil {
 		t.Fatalf("cannot send interrupt signal to Elastic Agent: %s", err)
@@ -206,73 +250,5 @@ service:
 		t.Fatalf("Elastic Agent exited with an error: %s", err)
 	}
 
-	//================================================== Run again
-	// t.Log("================================================== RUNNING AGAIN")
-
-}
-
-func countOtelEvents(t *testing.T, path string) int {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return 0
-		}
-
-		// t.Logf("cannot read output file: %s", err)
-		return -1
-	}
-
-	out := otelOutput{}
-	if err := json.Unmarshal(data, &out); err != nil {
-		// t.Logf("cannot unmarshal data: %s", err)
-		return -1
-	}
-
-	count := 0
-	for _, rlogs := range out.ResourceLogs {
-		for _, slogs := range rlogs.ScopeLogs {
-			count += len(slogs.LogRecords)
-			// for _, _ := range slogs.LogRecords {
-			// 	count++
-			// 	// t.Logf("data: %s", lr.Body.KvlistValue.Values)
-			// }
-		}
-	}
-
-	return count
-}
-
-type otelOutput struct {
-	ResourceLogs []ResourceLogs `json:"resourceLogs"`
-}
-
-type ResourceLogs struct {
-	ScopeLogs []ScopeLogs `json:"scopeLogs"`
-}
-
-type ScopeLogs struct {
-	LogRecords []LogRecords `json:"logRecords"`
-}
-
-type LogRecords struct {
-	TimeUnixNano         string `json:"timeUnixNano"`
-	ObservedTimeUnixNano string `json:"observedTimeUnixNano"`
-	Body                 Body   `json:"body"`
-}
-
-type Body struct {
-	KvlistValue KvlistValue `json:"kvlistValue"`
-}
-
-type KvlistValue struct {
-	Values []Values `json:"values"`
-}
-
-type Values struct {
-	Key   string `json:"key"`
-	Value Value  `json:"value"`
-}
-
-type Value struct {
-	StringValue string `json:"stringValue"`
+	f.WaitLogsContains(t, "Shutdown complete.", time.Second, "Filebeat Receiver didn't shutdown")
 }
