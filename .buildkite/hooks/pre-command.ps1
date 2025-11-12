8bcfa312f6a87257b410d3ad5cb67f466c60f381
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-11-12 20:12:59 +0100

    Fix path reloading

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,4 +1,6 @@
 # Shorten BUILDKITE_MESSAGE if needed to avoid filling the Windows env var buffer
 $env:BUILDKITE_MESSAGE = $env:BUILDKITE_MESSAGE.Substring(0, [System.Math]::Min(2048, $env:BUILDKITE_MESSAGE.Length))
 choco install awscli jq -y
-$env:Path += "c:\Program Files\Amazon\AWSCLIV2\aws.exe"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Output "AWS CLI installed. Version:"
+aws --version
