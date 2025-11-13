1d6b429363a899187fb404c38734324d5ace52f6
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-11-13 15:07:34 +0100

    Install zstandard on Windows

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,6 +1,6 @@
 # Shorten BUILDKITE_MESSAGE if needed to avoid filling the Windows env var buffer
 $env:BUILDKITE_MESSAGE = $env:BUILDKITE_MESSAGE.Substring(0, [System.Math]::Min(2048, $env:BUILDKITE_MESSAGE.Length))
-choco install awscli jq -y
+choco install awscli jq zstandard -y
 Write-Output "Env Path Before: $env:Path"
 $env:Path += ";C:\Program Files\Amazon\AWSCLIV2\"
 Write-Output "Env Path After: $env:Path"
