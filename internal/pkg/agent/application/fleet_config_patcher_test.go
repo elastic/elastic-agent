165155810cab1d4c1e1885b0d81b3a2d19ac4119
Michal <michal.pristas@elastic.co>
2025-12-03 16:17:11 +0100

    added missing headers

--- internal/pkg/agent/application/fleet_config_patcher_test.go
+++ internal/pkg/agent/application/fleet_config_patcher_test.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 package application
 
 import (
@@ -5,12 +9,13 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/elastic/elastic-agent/internal/pkg/agent/configuration"
-	"github.com/elastic/elastic-agent/internal/pkg/config"
-	"github.com/elastic/elastic-agent/pkg/core/logger/loggertest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent/internal/pkg/agent/configuration"
+	"github.com/elastic/elastic-agent/internal/pkg/config"
+	"github.com/elastic/elastic-agent/pkg/core/logger/loggertest"
 )
 
 func Test_FleetPatcher(t *testing.T) {
