f31716a43c51fd215b04e0470ee7fdbb76339edf
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-12-08 18:39:30 +0100

    Use patched healthcheckv2 extension
    # Conflicts:
    #	go.mod
    #	go.sum
    #	internal/edot/go.sum

--- internal/pkg/otel/manager/healthcheck.go
+++ internal/pkg/otel/manager/healthcheck.go
@@ -77,8 +77,9 @@ func injectHealthCheckV2Extension(conf *confmap.Conf, healthCheckExtensionID str
 				"http": map[string]interface{}{
 					"endpoint": fmt.Sprintf("localhost:%d", httpHealthCheckPort),
 					"status": map[string]interface{}{
-						"enabled": healthCheckHealthStatusEnabled,
-						"path":    healthCheckHealthStatusPath,
+						"enabled":            healthCheckHealthStatusEnabled,
+						"path":               healthCheckHealthStatusPath,
+						"include_attributes": true,
 					},
 					"config": map[string]interface{}{
 						"enabled": healthCheckHealthConfigEnabled,
