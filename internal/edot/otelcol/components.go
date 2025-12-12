230dc8a2412250fd25a6b8c884ee479806c8c9d0
Damien Mathieu <42@dmathieu.com>
2025-12-12 19:05:50 +0100

    Upgrade otel to 0.141.0 and 1.37.0 (#11671)
    * upgrade otel to 0.141.0 and 1.37.0
    
    * upgrade components
    
    * upgrade custom collector manifest too
    
    * upgrade more docs
    
    * restore the docs upgrade
    
    * set telemetry factory
    
    * upgrade profiling metrics
    
    * remove now defunct profilingmetrics metrics
    
    (cherry picked from commit ee439f134c2315862f6705a538ed1d167c3123ec)
    
    # Conflicts:
    #	NOTICE-fips.txt
    #	NOTICE.txt
    #	go.mod
    #	go.sum
    #	internal/edot/go.mod
    #	internal/edot/go.sum
    #	internal/pkg/otel/README.md
    #	internal/pkg/otel/samples/linux/profiles_metrics.yml

--- internal/edot/otelcol/components.go
+++ internal/edot/otelcol/components.go
@@ -11,6 +11,7 @@ import (
 	"go.opentelemetry.io/collector/otelcol"
 	"go.opentelemetry.io/collector/processor"
 	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/service/telemetry/otelconftelemetry"
 
 	// Receivers:
 	filelogreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver" // for collecting log files
@@ -85,7 +86,9 @@ import (
 func components(extensionFactories ...extension.Factory) func() (otelcol.Factories, error) {
 	return func() (otelcol.Factories, error) {
 		var err error
-		factories := otelcol.Factories{}
+		factories := otelcol.Factories{
+			Telemetry: otelconftelemetry.NewFactory(),
+		}
 
 		// Receivers
 		receivers := []receiver.Factory{
