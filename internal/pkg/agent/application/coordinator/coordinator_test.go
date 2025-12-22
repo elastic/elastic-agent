d5da222cb7f99db09b969152844585e0c8296b69
Shaunak Kashyap <ycombinator@gmail.com>
2025-12-22 10:46:15 +0530

    Add assertions on lengths of components added, removed, updated

--- internal/pkg/agent/application/coordinator/coordinator_test.go
+++ internal/pkg/agent/application/coordinator/coordinator_test.go
@@ -201,6 +201,9 @@ func TestComponentUpdateDiff(t *testing.T) {
 				},
 			},
 			logtest: func(t *testing.T, logs UpdateStats) {
+				require.Len(t, logs.Components.Added, 0)
+				require.Len(t, logs.Components.Removed, 0)
+				require.Len(t, logs.Components.Updated, 1)
 				require.Contains(t, logs.Components.Updated[0], "unit-three: added")
 				require.Contains(t, logs.Components.Updated[0], "unit-x: removed")
 			},
@@ -220,6 +223,9 @@ func TestComponentUpdateDiff(t *testing.T) {
 				},
 			},
 			logtest: func(t *testing.T, logs UpdateStats) {
+				require.Len(t, logs.Components.Added, 0)
+				require.Len(t, logs.Components.Removed, 0)
+				require.Len(t, logs.Components.Updated, 0)
 				require.Equal(t, []string{"elasticsearch"}, logs.Outputs.Removed)
 				require.Equal(t, []string{"logstash"}, logs.Outputs.Added)
 			},
