702f69c31f619775617a384ee7f6d39253e6b06a
Khushi Jain <khushi.jain@elastic.co>
2026-01-08 23:54:31 +0530

    (fix) Flaky test TestLogReloading (#12159)
    * (fix) Flaky test TestLogReloading
    
    * add back build tag
    
    (cherry picked from commit 023091862ceccd2d6ef5fb3eca030709915cfece)
    
    # Conflicts:
    #	testing/integration/ess/otel_test.go

--- testing/integration/ess/otel_test.go
+++ testing/integration/ess/otel_test.go
@@ -2135,9 +2135,6 @@ agent.reload:
 		return (zapLogs.FilterMessageSnippet("otelcol.component.kind").FilterMessageSnippet(`"log.level":"debug"`).Len() > 1)
 	}, 1*time.Minute, 10*time.Second, "could not find debug logs")
 
-	// reset logs
-	zapLogs.TakeAll()
-
 	// set agent.logging.level: info
 	cfg = fmt.Sprintf(logConfig, esURL, "info")
 	require.NoError(t, fixture.Configure(ctx, []byte(cfg)))
@@ -2149,11 +2146,19 @@ agent.reload:
 			t.Logf("waiting for agent healthy: %s", err.Error())
 			return false
 		}
+<<<<<<< HEAD
 		return zapLogs.FilterMessageSnippet("Everything is ready. Begin running and processing data").Len() > 0
 	}, 1*time.Minute, 10*time.Second, "elastic-agent was not healthy after log level changed to info")
 
 	// if debug level was enabled, we would fine this message
 	require.Zero(t, zapLogs.FilterMessageSnippet(`Starting health check extension V2`).Len())
+=======
+		return zapLogs.FilterMessageSnippet("Everything is ready. Begin running and processing data").Len() > 1
+	}, 90*time.Second, 10*time.Second, "elastic-agent was not healthy after log level changed to info")
+
+	// this debug log should not be present again after re-loading
+	require.Equal(t, 1, zapLogs.FilterMessageSnippet(`Starting health check extension V2`).Len())
+>>>>>>> 023091862 ((fix) Flaky test TestLogReloading (#12159))
 
 	// set collector logs to debug
 	logConfig = logConfig + `
