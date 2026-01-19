42cfeb0e5fd33c9d9649dbaae568ab8caa03d16f
VihasMakwana <121151420+VihasMakwana@users.noreply.github.com>
2026-01-19 15:11:52 +0530

    test fix

--- testing/integration/ess/container_cmd_test.go
+++ testing/integration/ess/container_cmd_test.go
@@ -886,73 +886,68 @@ func TestContainerCMDEnrollByPolicyName(t *testing.T) {
 	)
 }
 
-func TestContainerCMDReadOnly(t *testing.T) {
+func TestContainerCMDDiagnosticsSocket(t *testing.T) {
 	info := define.Require(t, define.Requirements{
 		Stack: &define.Stack{},
 		Local: false,
 		Sudo:  true,
 		OS: []define.OS{
-			{Type: define.Darwin},
+			{Type: define.Linux},
 		},
 		Group: "container",
 	})
 
-	ctx, cancel := context.WithTimeout(t.Context(), 10*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
 
 	agentFixture, err := define.NewFixtureFromLocalBuild(t, define.Version())
 	require.NoError(t, err)
+
+	// prepare must be called otherwise `agentFixture.WorkDir()` will be empty
+	// and it must be set so the `STATE_PATH` below gets a valid path.
 	err = agentFixture.Prepare(ctx)
 	require.NoError(t, err)
 
 	fleetURL, err := fleettools.DefaultURL(ctx, info.KibanaClient)
-	require.NoError(t, err)
-
-	// Populate fleet with a lot of policies to test retrieval by name
-	// Kibana's default page size is 20
-	// Include the special characters that must be escapted \():<>"* in KQL in the name
-	// See https://www.elastic.co/docs/reference/query-languages/kql
-	t.Log("Populate fleet with extra policies")
-	policyID := ""
-	for i := 0; i < 30; i++ {
-		policyID, _ = createPolicy(
-			t,
-			ctx,
-			agentFixture,
-			info,
-			fmt.Sprintf("%s \\():<>\"* %s", t.Name(), uuid.Must(uuid.NewV4()).String()),
-			"")
+	if err != nil {
+		t.Fatalf("could not get Fleet URL: %s", err)
 	}
-	// Use the last ID to get the policy, we want the name
-	resp, err := info.KibanaClient.GetPolicy(ctx, policyID)
-	require.NoError(t, err)
 
+	_, enrollmentToken := createPolicy(
+		t,
+		ctx,
+		agentFixture,
+		info,
+		fmt.Sprintf("%s-%s", t.Name(), uuid.Must(uuid.NewV4()).String()),
+		"")
+
+	// create a new state directory, as the tempDir might exceed socket path character limit (104 characters)
+	stateDir, err := os.MkdirTemp("/tmp", "state*")
+	t.Cleanup(func() {
+		_ = os.RemoveAll(stateDir)
+	})
+	require.NoError(t, err)
 	env := []string{
 		"FLEET_ENROLL=1",
 		"FLEET_URL=" + fleetURL,
-		"KIBANA_FLEET_HOST=" + info.KibanaClient.Connection.URL,
-		"FLEET_TOKEN_POLICY_NAME=" + resp.Name,
-		"KIBANA_FLEET_USERNAME=" + info.KibanaClient.Connection.Username,
-		"KIBANA_FLEET_PASSWORD=" + info.KibanaClient.Connection.Password,
-		"STATE_PATH=" + agentFixture.WorkDir(),
+		"FLEET_ENROLLMENT_TOKEN=" + enrollmentToken,
+		// As the agent isn't built for a container, it's upgradable, triggering
+		// the start of the upgrade watcher. If `STATE_PATH` isn't set, the
+		// upgrade watcher will commence from a different path within the
+		// container, distinct from the current execution path.
+		"STATE_PATH=" + stateDir,
 	}
-	require.NoError(t, os.Chmod(agentFixture.WorkDir(), 0555))
+
 	cmd, agentOutput := prepareAgentCMD(t, ctx, agentFixture, []string{"container"}, env)
 	t.Logf(">> running binary with: %v", cmd.Args)
-	err = cmd.Start()
-	require.NoError(t, err)
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("error running container cmd: %s", err)
+	}
 
-	require.EventuallyWithTf(t, func(c *assert.CollectT) {
-		// This will return errors until it connects to the agent,
-		// they're mostly noise because until the agent starts running
-		// we will get connection errors. If the test fails
-		// the agent logs will be present in the error message
-		// which should help to explain why the agent was not
-		// healthy.
-		status, err := agentFixture.ExecStatus(ctx, atesting.WithCmdOptions(withEnv(env)))
-		require.NoError(c, err)
-		require.Equal(c, int(cproto.State_HEALTHY), status.State, "agent status is not healthy")
-		require.Equal(c, int(cproto.State_HEALTHY), status.FleetState, "fleet state is not healthy")
+	require.EventuallyWithT(t, func(tt *assert.CollectT) {
+		require.Contains(tt, agentOutput.String(), "Diagnostics extension started")
+		_, err := os.Stat(filepath.Join(stateDir, "data", "edot-diagnostics-extension.sock"))
+		require.NoError(tt, err)
 	},
 		5*time.Minute, time.Second,
 		"Elastic-Agent did not report healthy. Agent status error: \"%v\", Agent logs\n%s",
