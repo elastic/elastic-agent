0c2cee8f367cbdc06ba4d5a57cf4b0918ca35608
Andrzej Stencel <andrzej.stencel@elastic.co>
2026-01-23 09:28:19 +0100

    feat: use the feature flag in Beat receivers

--- internal/pkg/otel/translate/otelconfig_test.go
+++ internal/pkg/otel/translate/otelconfig_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/mapstr"
+	internalConfig "github.com/elastic/elastic-agent/internal/pkg/config"
 
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
@@ -28,6 +29,7 @@ import (
 	"go.opentelemetry.io/collector/pipeline"
 
 	"github.com/elastic/elastic-agent/pkg/component"
+	"github.com/elastic/elastic-agent/pkg/features"
 )
 
 func TestBeatNameToDefaultDatastreamType(t *testing.T) {
@@ -393,8 +395,8 @@ func TestGetOtelConfig(t *testing.T) {
 	}
 
 	// expects input id
-	expectedFilestreamConfig := func(id string) map[string]any {
-		return map[string]any{
+	expectedFilestreamConfig := func(id string, opts ...filestreamConfigOption) (config map[string]any) {
+		config = map[string]any{
 			"filebeat": map[string]any{
 				"inputs": []map[string]any{
 					{
@@ -456,6 +458,10 @@ func TestGetOtelConfig(t *testing.T) {
 			},
 			"management.otel.enabled": true,
 		}
+		for _, opt := range opts {
+			opt.apply(config)
+		}
+		return config
 	}
 
 	getBeatMonitoringConfig := func(_, _ string) map[string]any {
@@ -468,10 +474,11 @@ func TestGetOtelConfig(t *testing.T) {
 	}
 
 	tests := []struct {
-		name           string
-		model          *component.Model
-		expectedConfig *confmap.Conf
-		expectedError  error
+		name              string
+		model             *component.Model
+		expectedConfig    *confmap.Conf
+		expectedError     error
+		defaultProcessors *bool // value of the default_processors feature flag
 	}{
 		{
 			name: "no supported components",
@@ -527,6 +534,60 @@ func TestGetOtelConfig(t *testing.T) {
 				},
 				"receivers": map[string]any{
 					"filebeatreceiver/_agent-component/filestream-default": expectedFilestreamConfig("filestream-default"),
+				},
+				"service": map[string]any{
+					"extensions": []any{"beatsauth/_agent-component/default"},
+					"pipelines": map[string]any{
+						"logs/_agent-component/filestream-default": map[string][]string{
+							"exporters": {"elasticsearch/_agent-component/default"},
+							"receivers": {"filebeatreceiver/_agent-component/filestream-default"},
+						},
+					},
+				},
+			}),
+		},
+		{
+			name:              "filestream with default processors disabled",
+			defaultProcessors: func() *bool { b := false; return &b }(), // Looking forward to Go v1.26 and its `new(false)` notation
+			model: &component.Model{
+				Components: []component.Component{
+					{
+						ID:         "filestream-default",
+						InputType:  "filestream",
+						OutputType: "elasticsearch",
+						OutputName: "default",
+						InputSpec: &component.InputRuntimeSpec{
+							BinaryName: "elastic-otel-collector",
+							Spec: component.InputSpec{
+								Command: &component.CommandSpec{
+									Args: []string{"filebeat"},
+								},
+							},
+						},
+						Units: []component.Unit{
+							{
+								ID:     "filestream-unit",
+								Type:   client.UnitTypeInput,
+								Config: component.MustExpectedConfig(fileStreamConfig),
+							},
+							{
+								ID:     "filestream-default",
+								Type:   client.UnitTypeOutput,
+								Config: component.MustExpectedConfig(esOutputConfig()),
+							},
+						},
+					},
+				},
+			},
+			expectedConfig: confmap.NewFromStringMap(map[string]any{
+				"exporters": map[string]any{
+					"elasticsearch/_agent-component/default": expectedESConfig("default"),
+				},
+				"extensions": map[string]any{
+					"beatsauth/_agent-component/default": expectedExtensionConfig(),
+				},
+				"receivers": map[string]any{
+					"filebeatreceiver/_agent-component/filestream-default": expectedFilestreamConfig("filestream-default", withDefaultProcessors(false)),
 				},
 				"service": map[string]any{
 					"extensions": []any{"beatsauth/_agent-component/default"},
@@ -837,6 +898,19 @@ func TestGetOtelConfig(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.defaultProcessors != nil {
+				originalValue := features.DefaultProcessors()
+				defer func() {
+					err := features.Apply(internalConfig.MustNewConfigFrom(map[string]any{
+						"agent.features.default_processors.enabled": originalValue,
+					}))
+					require.NoError(t, err)
+				}()
+				err := features.Apply(internalConfig.MustNewConfigFrom(map[string]any{
+					"agent.features.default_processors.enabled": *tt.defaultProcessors,
+				}))
+				require.NoError(t, err)
+			}
 			actualConf, actualError := GetOtelConfig(tt.model, agentInfo, getBeatMonitoringConfig, logp.NewNopLogger())
 			if actualConf == nil || tt.expectedConfig == nil {
 				assert.Equal(t, tt.expectedConfig, actualConf)
@@ -852,6 +926,27 @@ func TestGetOtelConfig(t *testing.T) {
 			}
 		})
 	}
+}
+
+type filestreamConfigOption interface {
+	apply(config map[string]any)
+}
+
+type withDefaultProcessorsOption struct {
+	value bool
+}
+
+func (w withDefaultProcessorsOption) apply(config map[string]any) {
+	if config == nil {
+		return
+	}
+	if !w.value {
+		delete(config, "processors")
+	}
+}
+
+func withDefaultProcessors(value bool) filestreamConfigOption {
+	return withDefaultProcessorsOption{value: value}
 }
 
 func TestGetReceiversConfigForComponent(t *testing.T) {
