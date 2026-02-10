e58aa1619fd6520683ddafdc9d7d7eed87ec6c3a
Mikołaj Świątek <mail@mikolajswiatek.com>
2026-02-10 11:34:20 +0000

    Use agent version from .package-version in PR package step (#12660)
    (cherry picked from commit 1d8207cca85cf48ed2016fd97c0ab1a0222bdcf9)
    
    # Conflicts:
    #	.buildkite/scripts/steps/package.sh

--- .buildkite/scripts/steps/package.sh
+++ .buildkite/scripts/steps/package.sh
@@ -5,9 +5,37 @@ set -euo pipefail
 _SELF=$(dirname $0)
 source "${_SELF}/../common.sh"
 
+<<<<<<< HEAD
 if test -z "${MANIFEST_URL=:""}"; then
   echo "Missing variable MANIFEST_URL, export it before use."
   exit 2
+=======
+mage clean
+
+if test -z "${MANIFEST_URL:-}"; then
+  echo "No MANIFEST_URL building core packages"
+
+  # Repo manifest is always SNAPSHOT components, force
+  # building in SNAPSHOT mode.
+  export SNAPSHOT=true
+
+  # We want to use the version from .package-version.
+  # If the version defined in version/version.go is different,
+  # the packaging step will expect artifacts with different names
+  # than what the manifest contains
+  export USE_PACKAGE_VERSION=true
+
+  # No manifest URL build the the core packages.
+  mage packageAgentCore
+
+  # Set manifest to version in repo so downloadManifest target
+  # can download the needed components. This gets unset before
+  # calling packageUsingDRA, so it uses the core built packages.
+  export MANIFEST_URL=$(jq -r .manifest_url .package-version)
+  _UNSET_MANIFEST_URL=true
+
+  echo "Using MANIFEST_URL from .package-version"
+>>>>>>> 1d8207cca (Use agent version from .package-version in PR package step (#12660))
 fi
 
 export AGENT_DROP_PATH=build/elastic-agent-drop
