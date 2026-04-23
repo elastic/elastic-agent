8483b03e35bc6365e17cd68b2c398d2f2a9602ab
Mikołaj Świątek <mail@mikolajswiatek.com>
2026-04-23 14:43:29 +0200

    Only build the necessary test binaries (#12987)
    (cherry picked from commit 216e1d26c1cfc6048b22034e30dac0c90e5ecaba)
    
    # Conflicts:
    #	magefile.go

--- magefile.go
+++ magefile.go
@@ -352,14 +352,21 @@ func (Build) Clean() error {
 		return fmt.Errorf("cannot remove build dir '%s': %w", absBuildDir, err)
 	}
 
-	testBinariesPath, err := getTestBinariesPath()
+	unitBinariesPath, err := getUnitTestBinariesPath()
 	if err != nil {
-		return fmt.Errorf("cannot remove test binaries: %w", err)
+		return fmt.Errorf("cannot remove unit test binaries: %w", err)
+	}
+	integrationBinariesPath, err := getIntegrationTestBinariesPath()
+	if err != nil {
+		return fmt.Errorf("cannot remove integration test binaries: %w", err)
 	}
 
 	if mg.Verbose() {
 		fmt.Println("removed", absBuildDir)
-		for _, b := range testBinariesPath {
+		for _, b := range unitBinariesPath {
+			fmt.Println("removed", b)
+		}
+		for _, b := range integrationBinariesPath {
 			fmt.Println("removed", b)
 		}
 	}
@@ -367,7 +374,40 @@ func (Build) Clean() error {
 	return nil
 }
 
+<<<<<<< HEAD
 func getTestBinariesPath() ([]string, error) {
+=======
+// TestBinaries build the required binaries for the test suite.
+func (Build) TestBinaries() error {
+	mg.SerialDeps(Build.UnitTestBinaries, Build.IntegrationTestBinaries)
+	return nil
+}
+
+// UnitTestBinaries builds only the binaries needed for unit tests.
+func (Build) UnitTestBinaries() error {
+	testBinaryPkgs, err := getUnitTestBinariesPath()
+	if err != nil {
+		return fmt.Errorf("cannot build unit test binaries: %w", err)
+	}
+	return buildTestBinaries(testBinaryPkgs)
+}
+
+// IntegrationTestBinaries builds only the binaries needed for integration tests.
+func (Build) IntegrationTestBinaries() error {
+	testBinaryPkgs, err := getIntegrationTestBinariesPath()
+	if err != nil {
+		return fmt.Errorf("cannot build integration test binaries: %w", err)
+	}
+	return buildTestBinaries(testBinaryPkgs)
+}
+
+// TestFakeComponent build just the test fake component.
+func (Build) TestFakeComponent() error {
+	return Build{}.IntegrationTestBinaries()
+}
+
+func getUnitTestBinariesPath() ([]string, error) {
+>>>>>>> 216e1d26c (Only build the necessary test binaries (#12987))
 	wd, err := os.Getwd()
 	if err != nil {
 		return nil, fmt.Errorf("could not get working directory: %w", err)
@@ -382,9 +422,26 @@ func getTestBinariesPath() ([]string, error) {
 	return testBinaryPkgs, nil
 }
 
+<<<<<<< HEAD
 // TestBinaries build the required binaries for the test suite.
 func (Build) TestBinaries() error {
 	testBinaryPkgs, err := getTestBinariesPath()
+=======
+func getIntegrationTestBinariesPath() ([]string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("could not get working directory: %w", err)
+	}
+
+	testBinaryPkgs := []string{
+		filepath.Join(wd, "pkg", "component", "fake", "component"),
+	}
+	return testBinaryPkgs, nil
+}
+
+func buildTestBinaries(testBinaryPkgs []string) error {
+	wd, err := os.Getwd()
+>>>>>>> 216e1d26c (Only build the necessary test binaries (#12987))
 	if err != nil {
 		fmt.Errorf("cannot build test binaries: %w", err)
 	}
@@ -473,7 +530,7 @@ func (Test) All() {
 
 // Unit runs all the unit tests.
 func (Test) Unit(ctx context.Context) error {
-	mg.Deps(Prepare.Env, Build.TestBinaries)
+	mg.Deps(Prepare.Env, Build.UnitTestBinaries)
 	cfg := devtools.SettingsFromContext(ctx)
 	params := devtools.DefaultGoTestUnitArgs(cfg)
 	return devtools.GoTest(ctx, params)
@@ -481,7 +538,7 @@ func (Test) Unit(ctx context.Context) error {
 
 // FIPSOnlyUnit runs all the unit tests with GODEBUG=fips140=only.
 func (Test) FIPSOnlyUnit(ctx context.Context) error {
-	mg.Deps(Prepare.Env, Build.TestBinaries)
+	mg.Deps(Prepare.Env, Build.UnitTestBinaries)
 
 	cfg := devtools.SettingsFromContext(ctx)
 	params := devtools.DefaultGoTestUnitArgs(cfg)
@@ -499,7 +556,7 @@ func (Test) FIPSOnlyUnit(ctx context.Context) error {
 
 // Coverage takes the coverages report from running all the tests and display the results in the browser.
 func (Test) Coverage() error {
-	mg.Deps(Prepare.Env, Build.TestBinaries)
+	mg.Deps(Prepare.Env, Build.UnitTestBinaries)
 	return RunGo("tool", "cover", "-html="+filepath.Join(buildDir, "coverage.out"))
 }
 
