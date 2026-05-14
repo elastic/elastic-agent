fc910d7a03906530ec1860098b5ba154614a283b
Samuel Veloso <samuel.veloso@elastic.co>
2026-05-14 12:00:54 +0200

    fix unit tests

--- internal/pkg/agent/application/upgrade/step_mark_test.go
+++ internal/pkg/agent/application/upgrade/step_mark_test.go
@@ -7,7 +7,9 @@ package upgrade
 import (
 	"errors"
 	"os"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -48,64 +50,118 @@ func TestTryLoadMarker_MissingFile(t *testing.T) {
 }
 
 func TestMarkUpgrade(t *testing.T) {
-	log, _ := loggertest.New("test")
-	agent := agentInstall{
-		version:       "8.5.0",
-		hash:          "abc123",
-		versionedHome: "home/v8.5.0",
+	type args struct {
+		currentAgent  agentInstall
+		previousAgent agentInstall
+		action        *fleetapi.ActionUpgrade
+		details       *details.Details
 	}
-	previousAgent := agentInstall{
-		version:       "8.4.0",
-		hash:          "xyz789",
-		versionedHome: "home/v8.4.0",
-	}
-	action := &fleetapi.ActionUpgrade{
-		ActionID:   "action-123",
-		ActionType: "UPGRADE",
-		Data: fleetapi.ActionUpgradeData{
-			Version:   "8.5.0",
-			SourceURI: "https://example.com/upgrade",
+	type workingDirHook func(t *testing.T, dataDir string)
+
+	testcases := []struct {
+		name            string
+		setupBeforeMark workingDirHook
+		args            args
+		wantErr         require.ErrorAssertionFunc
+		assertAfterMark workingDirHook
+	}{
+		{
+			name: "marker is written with all fields",
+			args: args{
+				currentAgent: agentInstall{
+					version:       "8.5.0",
+					hash:          "abc123",
+					versionedHome: "home/v8.5.0",
+				},
+				previousAgent: agentInstall{
+					version:       "8.4.0",
+					hash:          "xyz789",
+					versionedHome: "home/v8.4.0",
+				},
+				action:  nil,
+				details: details.NewDetails("8.5.0", details.StateScheduled, ""),
+			},
+			wantErr: require.NoError,
+			assertAfterMark: func(t *testing.T, dataDir string) {
+				actualMarker, err := LoadMarker(dataDir)
+				require.NoError(t, err, "error reading actualMarker content after writing")
+
+				require.WithinDuration(t, time.Now(), actualMarker.UpdatedOn, 5*time.Second, "UpdatedOn mismatch")
+
+				// Zero out UpdatedOn before full comparison: we cannot inject a
+				// fixed timestamp in this branch (no updatedOn param on markUpgradeFunc).
+				actualCopy := *actualMarker
+				actualCopy.UpdatedOn = time.Time{}
+
+				require.Equal(t, UpdateMarker{
+					Version:           "8.5.0",
+					Hash:              "abc123",
+					VersionedHome:     "home/v8.5.0",
+					PrevVersion:       "8.4.0",
+					PrevHash:          "xyz789",
+					PrevVersionedHome: "home/v8.4.0",
+					Acked:             false,
+					Action:            nil,
+					Details: &details.Details{
+						TargetVersion: "8.5.0",
+						State:         "UPG_SCHEDULED",
+						ActionID:      "",
+						Metadata:      details.Metadata{},
+					},
+				}, actualCopy)
+			},
+		},
+		{
+			name: "error writing update marker - check error",
+			setupBeforeMark: func(t *testing.T, dataDir string) {
+				// read-only permissions on directories don't work on windows, skip
+				if runtime.GOOS == "windows" {
+					t.Skip("skipping test on windows since readonly permissions on directory don't work")
+				}
+				err := os.Chmod(dataDir, 0555)
+				require.NoError(t, err, "error setting dataDir read-only")
+			},
+			args: args{
+				currentAgent: agentInstall{
+					version:       "8.5.0",
+					hash:          "abc123",
+					versionedHome: "home/v8.5.0",
+				},
+				previousAgent: agentInstall{
+					version:       "8.4.0",
+					hash:          "xyz789",
+					versionedHome: "home/v8.4.0",
+				},
+				action:  nil,
+				details: details.NewDetails("8.5.0", details.StateScheduled, ""),
+			},
+			wantErr: require.Error,
 		},
 	}
-	upgradeDetails := details.NewDetails("8.5.0", details.StateScheduled, "action-123")
 
-	testError := errors.New("test error")
-
-	type testCase struct {
-		fileName      string
-		expectedError error
-		markUpgrade   markUpgradeFunc
-	}
-
-	testCases := map[string]testCase{
-		"should return error if it fails updating the active commit file": {
-			fileName:      "commit",
-			expectedError: testError,
-			markUpgrade: markUpgradeProvider(func(log *logger.Logger, topDirPath, hash string, writeFile writeFileFunc) error {
-				return testError
-			}, func(name string, data []byte, perm os.FileMode) error {
-				return nil
-			}),
+	// Use the regular markUpgrade function, disabling the updateActiveCommit
+	// function that is bundled together (covered separately by TestUpdateActiveCommit).
+	markUpgrade := markUpgradeProvider(
+		func(log *logger.Logger, topDirPath, hash string, writeFile writeFileFunc) error {
+			return nil
 		},
-		"should return error if it fails writing to marker file": {
-			fileName:      "marker",
-			expectedError: testError,
-			markUpgrade: markUpgradeProvider(func(log *logger.Logger, topDirPath, hash string, writeFile writeFileFunc) error {
-				return nil
-			}, func(name string, data []byte, perm os.FileMode) error {
-				return testError
-			}),
-		},
-	}
+		os.WriteFile,
+	)
 
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			baseDir := t.TempDir()
-			paths.SetTop(baseDir)
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			dataDir := t.TempDir()
+			log, _ := loggertest.New(t.Name())
 
-			err := tc.markUpgrade(log, paths.Data(), agent, previousAgent, action, upgradeDetails)
-			require.Error(t, err)
-			require.ErrorIs(t, err, tc.expectedError)
+			if tc.setupBeforeMark != nil {
+				tc.setupBeforeMark(t, dataDir)
+			}
+
+			err := markUpgrade(log, dataDir, tc.args.currentAgent, tc.args.previousAgent, tc.args.action, tc.args.details)
+			tc.wantErr(t, err)
+			if tc.assertAfterMark != nil {
+				tc.assertAfterMark(t, dataDir)
+			}
 		})
 	}
 }
