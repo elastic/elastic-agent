7fa295ad9e73e240818ed6cf76ceb1f1177c3263
Victor Martinez <VictorMartinezRubio@gmail.com>
2026-05-14 17:30:52 +0200

    debug

--- .buildkite/hooks/environment.ps1
+++ .buildkite/hooks/environment.ps1
@@ -6,3 +6,6 @@ $env:BUILDKITE_GS_ACL="private"
 
 # Optional: Authenticated access in UI
 $env:BUILDKITE_GCS_ACCESS_HOST="storage.cloud.google.com"
+
+# Optional: Debug
+$env:BUILDKITE_AGENT_LOG_LEVEL="debug"
