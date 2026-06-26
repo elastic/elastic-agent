a8560798b18511cc0f1959bca9988b85b582800c
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2026-06-26 13:17:54 -0300

    otel(packetbeat): enable otel as default runtime (#15151)
    * otel(packetbeat): enable OTel receiver as default runtime
    
    Set Packetbeat.Default to otel in DefaultRuntimeConfig so that
    packetbeat uses the pbreceiver path by default
    
    Remove the skip in TestNetworkTraffic/otel to compare documents
    ingested by process and otel runtimes.
    
    * add changelog

--- pkg/component/component.go
+++ pkg/component/component.go
@@ -80,6 +80,7 @@ func DefaultRuntimeConfig() *RuntimeConfig {
 			InputType: make(map[string]string),
 		},
 		Packetbeat: BeatRuntimeConfig{
+			Default: string(OtelRuntimeManager),
 			// go-ucfg sets this while unpacking, having it in the default makes testing easier
 			InputType: make(map[string]string),
 		},
