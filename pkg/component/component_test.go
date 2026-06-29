884d3a696dca7619b9deb9e513d779d2cf1dc2f8
Craig MacKenzie <craig.mackenzie@elastic.co>
2026-06-29 12:02:15 -0400

    Switch auditbeat to run as a receiver by default. (#15188)
    * Switch auditbeat to run as a receiver by default.
    
    * Add changelog.

--- pkg/component/component_test.go
+++ pkg/component/component_test.go
@@ -4279,7 +4279,7 @@ func TestDefaultRuntimeConfig(t *testing.T) {
 	require.NotNil(t, config)
 	assert.Equal(t, string(DefaultRuntimeManager), config.Default)
 	assert.Equal(t, string(ProcessRuntimeManager), config.DynamicInputs)
-	assert.Equal(t, "", config.Auditbeat.Default)
+	assert.Equal(t, "otel", config.Auditbeat.Default)
 	assert.Empty(t, config.Auditbeat.InputType)
 	assert.Equal(t, "otel", config.Filebeat.Default)
 	assert.Empty(t, config.Filebeat.InputType)
