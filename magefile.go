56080ec24d56fce30a17e925e0db1fbd153b693b
Mikołaj Świątek <mail@mikolajswiatek.com>
2026-07-10 18:29:25 +0200

    Update magefile.go

--- magefile.go
+++ magefile.go
@@ -4228,7 +4228,7 @@ func (h Helm) Package(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed downloading manifest: %w", err)
 	}
-	
+
 	agentPackageVersion := cfg.AgentPackageVersion()
 	agentImageTag := agentPackageVersion + devtools.MaybeSnapshotSuffix(cfg)
 	agentChartVersion := agentPackageVersion + devtools.MaybeSnapshotSuffix(cfg)
