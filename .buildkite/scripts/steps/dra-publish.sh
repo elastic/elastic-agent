089eb1ee448cd29cea19aaca879722df254fc578
Doug Weyrauch <douglas.weyrauch@elastic.co>
2026-07-13 17:01:41 -0400

    Add temp testing branch

--- .buildkite/scripts/steps/dra-publish.sh
+++ .buildkite/scripts/steps/dra-publish.sh
@@ -21,6 +21,9 @@ if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" ]]; then
     echo "+++ Running in PR and setting branch main and --dry-run"
 fi
 
+# XXX: Temp for testing IAR Staging workflow end-to-end
+BRANCH="9.4"
+
 if [[ -z "${WORKFLOW}" ]]; then
   echo "+++ Missing DRA workflow";
   exit 1
