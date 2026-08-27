43c8d377db8227b8b1422457222ce26038ae17d7
Lorien Hu <lorien.hu@elastic.co>
2026-08-26 23:49:19 -0700

    Return RecoverableError in CheckRemote

--- internal/pkg/fleetapi/client/client.go
+++ internal/pkg/fleetapi/client/client.go
@@ -145,9 +145,16 @@ func CheckRemote(ctx context.Context, c Sender) error {
 	}()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("fleet server ping returned a bad status code: %d", resp.StatusCode)
+		err := fmt.Errorf("fleet server ping returned a bad status code: %d", resp.StatusCode)
+		if resp.StatusCode == http.StatusTooManyRequests || resp.StatusCode == http.StatusServiceUnavailable {
+			// treat 429, 503 as potentially recoverable so they don't cause a failed state
+			// 502 (Bad Gateway) and 504 (Gateway Timeout) are intentionally
+			// not included for this check as they may indicate a
+			// misconfiguration issue despite being considered retryable
+			return errors.NewRecoverable(err)
+		}
+		return err
 	}
-
 
 	return nil
 }
